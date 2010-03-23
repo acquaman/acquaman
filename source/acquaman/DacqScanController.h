c5dc5579a60c4dbacfb206dc1e5fe9f6a8fd7759
David Chevrier <david.k.chevrier@gmail.com>
2010-03-23 15:56:33 -0600

    Added first draft for dacq library controller

---
+++ source/acquaman/DacqScanController.h
@@ -0,0 +1,39 @@
+#ifndef DACQSCANCONTROLLER_H
+#define DACQSCANCONTROLLER_H
+
+#include "ScanController.h"
+#include "dacq3_2/acquisitionLib.h"
+#include "dacq3_2/qepicsacqlocal.h"
+#include "dacq3_2/OutputHandler/acqFactory.h"
+#include "dacq3_2/qepicsadvacq.h"
+//#include "dacq/acquisitionLib.h"
+//#include "dacq/qepicsacqlocal.h"
+
+class DacqScanController : public ScanController
+{
+public:
+    DacqScanController(QObject *parent = 0);
+
+public slots:
+    /// Sets a new scan configuration
+    virtual void newConfigurationLoad(ScanConfiguration &cfg);
+    /// Start scan running if not currently running or paused
+    virtual void start(){ advAcq_->Start();}
+    /// Cancel scan if currently running or paused
+    virtual void cancel();
+    /// Pause scan if currently running
+    virtual void pause() {advAcq_->Pause(0);}
+    /// Resume scan if currently paused
+    virtual void resume(){ advAcq_->Pause(1);}
+
+protected:
+    QEpicsAdvAcq *advAcq_;
+    bool cancelled_;
+
+protected slots:
+    void onStart();
+    void onStop();
+    void onPause(int mode);
+};
+
+#endif // DACQSCANCONTROLLER_H
