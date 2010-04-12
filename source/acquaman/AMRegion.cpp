acb0a5f4d9f0f35fc265190fb6f15e58446af781
David Chevrier <david.k.chevrier@gmail.com>
2010-04-12 15:39:46 -0600

    Doing documentation for AMRegion

--- source/acquaman/AMRegion.cpp
+++ source/acquaman/AMRegion.cpp
@@ -1,6 +1,120 @@
 #include "AMRegion.h"
 
 AMRegion::AMRegion(QObject *parent) :
-    QObject(parent)
+	QObject(parent)
 {
+}
+
+/// Sets the delta value from the double passed in. Makes sure the value is non-zero, otherwise it returns false.
+bool AMRegion::setDelta(double delta) {
+	if(delta == 0)
+		return false;
+	delta_ = delta;
+	return true;
+}
+
+/// Sets the start value from the double passed in. Makes sure the energy is within the allowable range, otherwise returns false.
+bool AMXASRegion::setStart(double start) {
+	if(ctrl_->valueOutOfRange(start))
+		return FALSE;
+	start_ = start;
+	return TRUE;
+}
+
+/// Sets the end value from the double passed in. Makes sure the energy is within the allowable range, otherwise returns false.
+bool AMXASRegion::setEnd(double end) {
+	if(ctrl_->valueOutOfRange(end))
+		return FALSE;
+	end_ = end;
+	return TRUE;
+}
+
+/// Returns the data at the index from the list of AMXASRegion available. For invalid requests an empty QVariant is returned.
+QVariant AMXASRegionModel::data(const QModelIndex &index, int role) const {
+
+	// Invalid index:
+	if(!index.isValid())
+		return QVariant();
+
+	// We only answer to Qt::DisplayRole right now
+	if(role != Qt::DisplayRole)
+		return QVariant();
+
+	// Out of range: (Just checking for too big.  isValid() checked for < 0)
+	if(index.row() >= regions_->count())
+		return QVariant();
+	// Return Start:
+	if(index.column() == 0)
+		return regions_->at(index.row())->start() ;
+	// Return Delta:
+	if(index.column() == 1)
+		return regions_->at(index.row())->delta() ;
+	if(index.column() == 2)
+		return regions_->at(index.row())->end() ;
+
+	// Anything else:
+	return QVariant();
+}
+
+/// Returns the header data at the row or column requested. For invalid requests an empty QVariant is returned.
+QVariant AMXASRegionModel::headerData(int section, Qt::Orientation orientation, int role) const {
+
+	if (role != Qt::DisplayRole) return QVariant();
+
+	// Vertical headers:
+	if(orientation == Qt::Vertical) {
+		return section;
+	}
+
+	// Horizontal Headers: (Column labels)
+	else {
+		if(section == 0)
+			return "Start";
+		if(section == 1)
+			return "Delta";
+		if(section == 2)
+			return "End";
+	}
+	return QVariant();
+}
+
+/// Sets the data at the requested index from a QVariant, which is converted to a double in all cases. Returns false if no value set or if the QVariant cannot be converted to a double.
+bool AMXASRegionModel::setData(const QModelIndex &index, const QVariant &value, int role) {
+
+	if (index.isValid()  && index.row() < regions_->count() && role == Qt::EditRole) {
+
+		bool conversionOK;
+		double dval = value.toDouble(&conversionOK);
+		if(!conversionOK)
+			return false;
+
+		// Setting a start value?
+		if(index.column() == 0) {
+			regions_->at(index.row())->setStart(dval);
+			emit dataChanged(index, index);
+			return true;
+		}
+		// Setting a delta value?
+		if(index.column() == 1) {
+			regions_->at(index.row())->setDelta(dval);
+			emit dataChanged(index, index);
+			return true;
+		}
+		// Setting an end value?
+		if(index.column() == 1) {
+			regions_->at(index.row())->setEnd(dval);
+			emit dataChanged(index, index);
+			return true;
+		}
+	}
+	return false;	// no value set
+}
+
+/// Returns the appropriate flags for an index, which are always the same if the index is valid.
+Qt::ItemFlags AMXASRegionModel::flags(const QModelIndex &index) const {
+
+	Qt::ItemFlags flags;
+	if (index.isValid() && index.row() < regions_->count() && index.column()<3)
+		flags = Qt::ItemIsEditable | Qt::ItemIsSelectable | Qt::ItemIsEnabled;
+	return flags;
 }
--- source/acquaman/AMRegion.h
+++ source/acquaman/AMRegion.h
@@ -11,12 +11,21 @@
 class ContinuousRegion
 {
 public:
-    ContinuousRegion(){};
-    double startVal_;
-    QList<double> values_;
+	ContinuousRegion(){};
+	double startVal_;
+	QList<double> values_;
 };
 */
 
+/// An AMRegion is the generalized parent of all "regions" one can imagine scanning, it can be subclasses to achieve particular functionality.
+/*!
+  The AMRegion class provides the basic interface to a particular region in a scan. Most scans are made up of a list of back to back regions.
+  As such, an AMRegion holds only a pointer to an AMControl (the device being scanned) as well as start, delta, and end values.
+  The region should begin at the "start" value and move in discrete steps of "delta" until it hits the "end" value.
+  The actual implementation is up to the scan library being used.
+  Checks like making sure that the delta moves in the right direction (positive if end > start OR negative if end < start) is NOT taken care of here.
+  Responsibility for such checks is passed onto higher level classes (like AMScanConfiguration).
+  */
 class AMRegion: public QObject
 {
 Q_OBJECT
@@ -25,155 +34,93 @@ Q_PROPERTY(double delta READ delta WRITE setDelta)
 Q_PROPERTY(double end READ end WRITE setEnd)
 
 public:
-AMRegion(QObject *parent = 0);
-    virtual double start() const { return start_;}
-    virtual double delta() const { return delta_;}
-    virtual double end() const { return end_;}
-    virtual AMControl* control() const { return ctrl_;}
+	/// Constructor, only requires a QObject for a parent
+	AMRegion(QObject *parent = 0);
+	/// Returns the stored start value as a double
+	virtual double start() const { return start_;}
+	/// Returns the stored delta value as a double
+	virtual double delta() const { return delta_;}
+	/// Returns the stored end value as a double
+	virtual double end() const { return end_;}
+	/// Returns a pointer the AMControl this region is operating on
+	virtual AMControl* control() const { return ctrl_;}
 
 public slots:
-    virtual bool setStart(double start) { start_ = start; return TRUE;}
-    virtual bool setDelta(double delta) {
-        if(delta == 0)
-            return false;
-        delta_ = delta;
-        return true;
-    }
-    virtual bool setEnd(double end) { end_ = end; return TRUE;}
-    virtual bool setControl(AMControl* ctrl) { ctrl_ = ctrl; return true;}
+	/// Sets the start value from the double passed in. Does not affect the AMControl directly.
+	/// \todo A check on the limits of the AMControl?
+	virtual bool setStart(double start) { start_ = start; return TRUE;}
+	/// Sets the delta value from the double passed in. The value MUST BE non-zero, or function will return false. Does not affect the AMControl direclty.
+	virtual bool setDelta(double delta);
+	/// Sets the end value from the double passed in. Does not affect the AMControl directly.
+	/// \todo A check on the limits of the AMControl?
+	virtual bool setEnd(double end) { end_ = end; return TRUE;}
+	/// Sets the AMControl for the region.
+	virtual bool setControl(AMControl* ctrl) { ctrl_ = ctrl; return true;}
 
 protected:
-    double start_;
-    double delta_;
-    double end_;
-    AMControl *ctrl_;
+	/// Value for the beginning of the region.
+	double start_;
+	/// Stepping value for the region.
+	double delta_;
+	/// Value for the end of the region.
+	double end_;
+	/// AMControl for the region to step through.
+	AMControl *ctrl_;
 };
 
+/// AMXASRegion is an impementation of AMRegion designed to scan energy regions; therefore, the AMControl is passed into the constructor and must be a beamline energy control.
+/*!
+  Simply pass an AMControl pointer into the constructor, and this child class will always refer to the beamline energy.
+  The setStart() and setEnd() are reimplemented to ensure that the energy passed into the region is within the energy limits of the beamline.
+  The setControl(AMControl*) function has been castrated, it will always return false and no change will be made.
+  */
 class AMXASRegion : public AMRegion
 {
 Q_OBJECT
 
 public:
-AMXASRegion(AMControl* beamlineEnergy, QObject *parent = 0) : AMRegion(parent) {ctrl_ = beamlineEnergy;}
+	/// Constructor, takes an AMControl to act as the perminant control for this region, must be the beamline energy control.
+	AMXASRegion(AMControl* beamlineEnergy, QObject *parent = 0) : AMRegion(parent) {ctrl_ = beamlineEnergy;}
 
 public slots:
-    bool setStart(double start) {
-        if(ctrl_->valueOutOfRange(start))
-            return FALSE;
-        start_ = start;
-        return TRUE;
-    }
-    bool setEnd(double end) {
-        if(ctrl_->valueOutOfRange(end))
-            return FALSE;
-        end_ = end;
-        return TRUE;
-    }
-    bool setControl(AMControl *ctrl){Q_UNUSED(ctrl); return false;}
+	/// Sets the start value from the double passed in. Checks for the validity of the input. Does not affect the beamline energy directly.
+	bool setStart(double start);
+	/// Sets the end value from the double passed in. Checks for the validity of the input. Does not affect the beamline energy directly.
+	bool setEnd(double end);
+	/// Castrated function, does nothing and returns false.
+	bool setControl(AMControl *ctrl){Q_UNUSED(ctrl); return false;}
 };
 
+/// An AMXASRegionModel is used as an interface between any default model viewer in Qt and a list of AMXASRegion.
+/*!
+  Simply pass a pointer to a list of AMXASRegion into the constructor and the model will be automatically set up.
+  \todo Move this up one level to ALL AMRegions and/or combine with the AMScanConfiguration classes (so they can be the model direclty).
+  */
 class AMXASRegionModel : public QAbstractTableModel
 {
 	Q_OBJECT
 
 public:
+	/// Constructor, pass a pointer to a list of AMXASRegions to set up the model. Such a list can be easily returned with regionsPtr() function in AMXASScanConfiguration class.
 	AMXASRegionModel(QList<AMXASRegion*> *regions, QObject *parent = 0) : QAbstractTableModel(parent) {
 		regions_ = regions;
 	}
 
+	/// Returns the number of regions in the list to generate the number of rows in a table or list
 	int rowCount(const QModelIndex & /*parent*/) const { return regions_->count(); }
+	/// Returns "3" statically. There are always three fields in the region: start, delta, and end.
 	int columnCount(const QModelIndex & /*parent*/) const { return 3; }
-
-	QVariant data(const QModelIndex &index, int role) const {
-
-		// Invalid index:
-		if(!index.isValid())
-			return QVariant();
-
-		// We only answer to Qt::DisplayRole right now
-		if(role != Qt::DisplayRole)
-			return QVariant();
-
-		// Out of range: (Just checking for too big.  isValid() checked for < 0)
-		if(index.row() >= regions_->count())
-			return QVariant();
-		// Return Start:
-		if(index.column() == 0)
-			return regions_->at(index.row())->start() ;
-		// Return Delta:
-		if(index.column() == 1)
-			return regions_->at(index.row())->delta() ;
-		if(index.column() == 2)
-			return regions_->at(index.row())->end() ;
-
-		// Anything else:
-		return QVariant();
-	}
-
-	QVariant headerData(int section, Qt::Orientation orientation, int role) const {
-
-		if (role != Qt::DisplayRole) return QVariant();
-
-		// Vertical headers:
-		if(orientation == Qt::Vertical) {
-			return section;
-		}
-
-		// Horizontal Headers: (Column labels)
-		else {
-			if(section == 0)
-				return "Start";
-			if(section == 1)
-				return "Delta";
-			if(section == 2)
-				return "End";
-		}
-		return QVariant();
-	}
-
-	bool setData(const QModelIndex &index, const QVariant &value, int role) {
-
-		if (index.isValid()  && index.row() < regions_->count() && role == Qt::EditRole) {
-
-			bool conversionOK;
-			double dval = value.toDouble(&conversionOK);
-			if(!conversionOK)
-				return false;
-
-			// Setting a start value?
-			if(index.column() == 0) {
-				regions_->at(index.row())->setStart(dval);
-				emit dataChanged(index, index);
-				return true;
-			}
-			// Setting a delta value?
-			if(index.column() == 1) {
-				regions_->at(index.row())->setDelta(dval);
-				emit dataChanged(index, index);
-				return true;
-			}
-			// Setting an end value?
-			if(index.column() == 1) {
-				regions_->at(index.row())->setEnd(dval);
-				emit dataChanged(index, index);
-				return true;
-			}
-		}
-		return false;	// no value set
-	}
-
-	// This allows editing of values within range (for ex: in a QTableView)
-	Qt::ItemFlags flags(const QModelIndex &index) const {
-
-		Qt::ItemFlags flags;
-		if (index.isValid() && index.row() < regions_->count() && index.column()<3)
-			flags = Qt::ItemIsEditable | Qt::ItemIsSelectable | Qt::ItemIsEnabled;
-		return flags;
-	}
+	/// Retrieves the data from an index (row and column) and returns as a QVariant. Only valid role is Qt::DisplayRole right now.
+	QVariant data(const QModelIndex &index, int role) const;
+	/// Retrieves the header data for a column or row and returns as a QVariant. Only valid role is Qt::DisplayRole right now.
+	QVariant headerData(int section, Qt::Orientation orientation, int role) const;
+	/// Sets the data value at an index (row and column). Only valid role is Qt::DisplayRole right now.
+	bool setData(const QModelIndex &index, const QVariant &value, int role);
+	/// This allows editing of values within range (for ex: in a QTableView)
+	Qt::ItemFlags flags(const QModelIndex &index) const;
 
 private:
-//	void setupModelData(const QStringList &lines, TreeItem *parent);
-
+	/// Internal pointer to the list of AMXASRegion.
 	QList<AMXASRegion*> *regions_;
 };
 
