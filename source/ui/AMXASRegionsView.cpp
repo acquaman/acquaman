0ee2eba3dd83c933de06aeb119749e0ef3e0ec41
David Chevrier <david.k.chevrier@gmail.com>
2010-04-12 11:00:08 -0600

    Documenting AMXASRegionsView

--- source/ui/AMXASRegionsView.cpp
+++ source/ui/AMXASRegionsView.cpp
@@ -1,26 +1,23 @@
 #include "AMXASRegionsView.h"
 
+/// Creates buttons for add and delete region. \todo Connect add and delete to something.
+/// Defines a new model and views using table view.
+/// Adds all items to form layout.
 AMXASRegionsView::AMXASRegionsView(QList<AMXASRegion*> *regions, QWidget *parent) :
     QWidget(parent)
 {
-//	setFixedSize(300, 300);
 	this->setMaximumSize(400, 400);
 	addButton_ = new QPushButton("Add Region", this);
 	deleteButton_ = new QPushButton("Delete Region", this);
-	hl_ = new QHBoxLayout();
+	QHBoxLayout *hl_ = new QHBoxLayout();
 	hl_->addWidget(addButton_);
 	hl_->addWidget(deleteButton_);
 	rm_ = new AMXASRegionModel(regions, this);
 	tv_ = new QTableView(this);
 	tv_->setModel(rm_);
 	tv_->resize(tv_->sizeHint());
-//	tv_->setFixedSize(300, 250);
-//	vl_ = new QVBoxLayout(this);
-//	vl_->addLayout(hl_);
-//	vl_->addWidget(tv_, 0);
-	fl_ = new QFormLayout(this);
+	QFormLayout *fl_ = new QFormLayout(this);
 	fl_->addRow(tv_);
-//	fl_->addRow(addButton_, deleteButton_);
 	fl_->addRow(hl_);
 	setLayout(fl_);
 }
--- source/ui/AMXASRegionsView.h
+++ source/ui/AMXASRegionsView.h
@@ -8,21 +8,33 @@
 #include <QFormLayout>
 #include "acquaman/AMRegion.h"
 
+/// An AMXASRegionsView is a means of generating a default view for a group of AMXASRegion
+/*!
+  This default view utilizes a QTableView to interface with a group of AMXASRegion.
+  By calling the constructor while passing a QList of AMXASRegion, the AMXASRegionsView:
+  - creates add region and delete region buttons; and, \todo Hook these buttons up to something
+  - uses the QList of AMXASRegion to make an AMXASRegionModel (the programmer need not do this nor understand how it works); and,
+  - creates a table view on the newly created model.
+  \todo This should most likely be an AMRegionsView ... there's no real need to have it at the AMXASRegion level. I'll fix this, I just needed to figure out if it would work in the first place.
+  */
 class AMXASRegionsView : public QWidget
 {
 Q_OBJECT
 public:
+	/// Constructor
+	/*!
+	  \param regions Pointer to a QList of AMXASRegions. Can be easily retreived using regionsPtr() function on an AMXASScanConfiguration or child.
+	  */
 	explicit AMXASRegionsView(QList<AMXASRegion*> *regions, QWidget *parent = 0);
 
 signals:
 
 public slots:
+
 protected:
 	QTableView *tv_;
 	QPushButton *addButton_;
 	QPushButton *deleteButton_;
-	QHBoxLayout *hl_;
-	QFormLayout *fl_;
 	AMXASRegionModel *rm_;
 };
 
