8459a2cad6e0fb8e7652c0def6fd5c0aff5e81ef
David Chevrier <david.k.chevrier@gmail.com>
2010-04-14 15:25:49 -0600

    Doing documentation for AMXASScanConfiguration

--- source/acquaman/AMXASScanConfiguration.cpp
+++ source/acquaman/AMXASScanConfiguration.cpp
@@ -1,118 +1,119 @@
 #include "AMXASScanConfiguration.h"
 
+/// Constructor simply passes arguments up to AMScanConfiguration constructor.
 AMXASScanConfiguration::AMXASScanConfiguration(QObject *parent) : AMScanConfiguration(parent)
 {
 
-    /*
-    QList<AMControl*> fluxresList;
-    fluxresList << SGMBeamline::sgm()->exitSlitGap() << SGMBeamline::sgm()->grating() << SGMBeamline::sgm()->harmonic();
-    */
+	/*
+	QList<AMControl*> fluxresList;
+	fluxresList << SGMBeamline::sgm()->exitSlitGap() << SGMBeamline::sgm()->grating() << SGMBeamline::sgm()->harmonic();
+	*/
 
-    /*
-    setExitSlitGap(SGMBeamline::sgm()->exitSlitGap()->value());
-    setGrating((SGMBeamline::sgm()->grating()->value() < 1) ? SGMBeamline::lowGrating : ((SGMBeamline::sgm()->grating()->value() < 2) ? SGMBeamline::mediumGrating : SGMBeamline::highGrating));
-    setUndulatorTracking(SGMBeamline::sgm()->undulatorTracking()->value());
-    setMonoTracking(SGMBeamline::sgm()->monoTracking()->value());
-    setExitSlitTracking(SGMBeamline::sgm()->exitSlitTracking()->value());
-    */
+	/*
+	setExitSlitGap(SGMBeamline::sgm()->exitSlitGap()->value());
+	setGrating((SGMBeamline::sgm()->grating()->value() < 1) ? SGMBeamline::lowGrating : ((SGMBeamline::sgm()->grating()->value() < 2) ? SGMBeamline::mediumGrating : SGMBeamline::highGrating));
+	setUndulatorTracking(SGMBeamline::sgm()->undulatorTracking()->value());
+	setMonoTracking(SGMBeamline::sgm()->monoTracking()->value());
+	setExitSlitTracking(SGMBeamline::sgm()->exitSlitTracking()->value());
+	*/
 }
 
 double AMXASScanConfiguration::start(size_t index) const
 {
-    AMXASRegion *xasr = region(index);
-    if(xasr)
-        return xasr->start();
-    return -1;
+	AMXASRegion *xasr = region(index);
+	if(xasr)
+		return xasr->start();
+	return -1;
 }
 
 double AMXASScanConfiguration::delta(size_t index) const
 {
-    AMXASRegion *xasr = region(index);
-    if(xasr)
-        return xasr->delta();
-    return -1;
+	AMXASRegion *xasr = region(index);
+	if(xasr)
+		return xasr->delta();
+	return 0;
 }
 
 double AMXASScanConfiguration::end(size_t index) const
 {
-    AMXASRegion *xasr = region(index);
-    if(xasr)
-        return xasr->end();
-    return -1;
+	AMXASRegion *xasr = region(index);
+	if(xasr)
+		return xasr->end();
+	return -1;
 }
 
 AMXASRegion* AMXASScanConfiguration::region(size_t index) const
 {
-    if(index < (size_t)regions_.count())
-        return regions_.at(index);
-    return NULL;
+	if(index < (size_t)regions_.count())
+		return regions_.at(index);
+	return NULL;
 }
 
 bool AMXASScanConfiguration::setStart(size_t index, double start)
 {
-    if(index < (size_t)regions_.count()){
-        AMXASRegion* xasr = regions_.at(index);
-        xasr->setStart(start);
-        return TRUE;
-    }
-    return FALSE;
+	if(index < (size_t)regions_.count()){
+		AMXASRegion* xasr = regions_.at(index);
+		xasr->setStart(start);
+		return TRUE;
+	}
+	return FALSE;
 }
 
 bool AMXASScanConfiguration::setDelta(size_t index, double delta)
 {
-    if(index < (size_t)regions_.count()){
-        AMXASRegion* xasr = regions_.at(index);
-        xasr->setDelta(delta);
-        return TRUE;
-    }
-    return FALSE;
+	if(index < (size_t)regions_.count()){
+		AMXASRegion* xasr = regions_.at(index);
+		xasr->setDelta(delta);
+		return TRUE;
+	}
+	return FALSE;
 }
 
 bool AMXASScanConfiguration::setEnd(size_t index, double end)
 {
-    if(index < (size_t)regions_.count()){
-        AMXASRegion* xasr = regions_.at(index);
-        xasr->setEnd(end);
-        return TRUE;
-    }
-    return FALSE;
+	if(index < (size_t)regions_.count()){
+		AMXASRegion* xasr = regions_.at(index);
+		xasr->setEnd(end);
+		return TRUE;
+	}
+	return FALSE;
 }
 
 bool AMXASScanConfiguration::setRegion(size_t index, AMXASRegion *region)
 {
-    if(index < (size_t)regions_.count()){
-        AMXASRegion *xasr = regions_.at(index);
-        xasr = region;
-        return TRUE;
-    }
-    return FALSE;
+	if(region && index < (size_t)regions_.count()){
+		AMXASRegion *xasr = regions_.at(index);
+		xasr = region;
+		return TRUE;
+	}
+	return FALSE;
 }
 
 bool AMXASScanConfiguration::addRegion(size_t index, AMXASRegion *region)
 {
-    if(index <= (size_t)regions_.count()){
-        regions_.insert(index, region);
-        return TRUE;
-    }
-    return FALSE;
+	if(index <= (size_t)regions_.count()){
+		regions_.insert(index, region);
+		return TRUE;
+	}
+	return FALSE;
 }
 
 /*
 bool AMXASScanConfiguration::addRegion(size_t index, double start, double delta, double end)
 {
-    AMXASRegion *xasr = new AMXASRegion(SGMBeamline::sgm()->energy(), this);
-    xasr->setStart(start);
-    xasr->setDelta(delta);
-    xasr->setEnd(end);
-    return addRegion(index, xasr);
+	AMXASRegion *xasr = new AMXASRegion(SGMBeamline::sgm()->energy(), this);
+	xasr->setStart(start);
+	xasr->setDelta(delta);
+	xasr->setEnd(end);
+	return addRegion(index, xasr);
 }
 */
 
 bool AMXASScanConfiguration::deleteRegion(size_t index)
 {
-    if(index < (size_t)regions_.count()){
-        regions_.removeAt(index);
-        return TRUE;
-    }
-    return FALSE;
+	if(index < (size_t)regions_.count()){
+		regions_.removeAt(index);
+		return TRUE;
+	}
+	return FALSE;
 }
--- source/acquaman/AMXASScanConfiguration.h
+++ source/acquaman/AMXASScanConfiguration.h
@@ -3,31 +3,54 @@
 
 #include <QObject>
 #include "AMScanConfiguration.h"
-//#include "beamline/SGMBeamline.h"
 
+/// An AMXASScanConfiguration is the parent class for any beamline that wants to implement a simple XAS (X-Ray Absorption Spectroscopy) scan.
+/*!
+  The AMXASScanConfiguration class is designed to allow beamline specific implementation classes to inherit it, but take advantage of a common method for configuring the energy regions.
+  To this end, the class holds a QList of pointers to AMXASRegion.
+  The AMXASRegion class inherits from AMRegion and optimizes the class to explicitly control beamline energy.
+  Functions for adding and deleting regions, as well as editing regions are available.
+  \todo Make a class on top of this (AMRegionScanConfiguration?) that uses just AMRegions? Then reimplement some of this...
+  */
 class AMXASScanConfiguration : public AMScanConfiguration
 {
-    Q_OBJECT
-public:    
-    AMXASScanConfiguration(QObject *parent = 0);
-    double start(size_t index) const;
-    double delta(size_t index) const;
-    double end(size_t index) const;
-    AMXASRegion* region(size_t index) const;
-    QList<AMXASRegion*> regions() { return regions_;}
+	Q_OBJECT
+public:
+	/// Constructor, needs only a pointer to a QObject to act as a parent.
+	AMXASScanConfiguration(QObject *parent = 0);
+	/// Returns the start value of the region refered to by index. If an invalid index is given, returns -1 (not a valid energy value).
+	double start(size_t index) const;
+	/// Returns the delta value of the region refered to by index. If an invalid index is given, returns 0 (not a valid delta value).
+	double delta(size_t index) const;
+	/// Returns the end value of the region refered to by index. If an invalid index is given, returns -1 (not a valid energy value).
+	double end(size_t index) const;
+	/// Returns a pointer to the region refered to by index. If an invalid index is given, returns NULL.
+	AMXASRegion* region(size_t index) const;
+	/// Returns a copy of the QList of regions.
+	QList<AMXASRegion*> regions() { return regions_;}
+	/// Returns a pointer to the QList of regions.
 	QList<AMXASRegion*>* regionsPtr() { return &regions_;}
 
 public slots:
-    bool setStart(size_t index, double start);
-    bool setDelta(size_t index, double delta);
-    bool setEnd(size_t index, double end);
-    bool setRegion(size_t index, AMXASRegion *region);
-    virtual bool addRegion(size_t index, AMXASRegion *region);
-    virtual bool addRegion(size_t index, double start, double delta, double end) = 0;
-    bool deleteRegion(size_t index);
+	/// Sets the start value of the region refered to by index. Returns true if sucessful, returns false if the index is invalid or the energy is out of range.
+	bool setStart(size_t index, double start);
+	/// Sets the delta value of the region refered to by index. Returns true if sucessful, return false if the index is invalid or the delta is 0.
+	bool setDelta(size_t index, double delta);
+	/// Sets the end value of the region refered to by index. Returns true if succesful, returns false if the index is invalid or the energy is out of range.
+	bool setEnd(size_t index, double end);
+	/// Sets the region refered to by index. Returns true if successful, returns false if the index is invalid or the region pointer is NULL.
+	bool setRegion(size_t index, AMXASRegion *region);
+	/// Adds a region into position refered to by index and renumbers subsequent regions accordingly. Returns true if successful, returns false if the index is invalid.
+	virtual bool addRegion(size_t index, AMXASRegion *region);
+	/// Pure virtual function. Should be implemented in beamline specific subclasses as a convenience function for above.
+	/// Creates a new region using start, delta, and end values then calls addRegion(index, *region).
+	virtual bool addRegion(size_t index, double start, double delta, double end) = 0;
+	/// Deletes the region refered to by index and renumbers subsequent regions accordingly. Returns true if successful, return false if index is invalid.
+	bool deleteRegion(size_t index);
 
 protected:
-    QList<AMXASRegion*> regions_;
+	/// Holds the list of AMXASRegion pointers.
+	QList<AMXASRegion*> regions_;
 //    QList<AMControlSet*> groups_;
 };
 
