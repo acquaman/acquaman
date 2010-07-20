6a58aa26f24abc152fbd38f8eaec373481cd731e
Mark Boots <mark.boots@usask.ca>
2010-07-19 22:03:37 -0600

    added AMSample

---
+++ source/dataman/AMSample.cpp
@@ -0,0 +1,34 @@
+#include "AMSample.h"
+
+AMSample::AMSample(QObject *parent) :
+    AMDbObject(parent)
+{
+
+}
+
+/// This constructor initializes a sample with a given name.
+AMSample::AMSample(const QString& sampleName, QObject* parent)
+	: AMDbObject(parent)
+{
+	/// Initialize our unique pieces of meta-data. The values don't matter much, but we want to make sure that they're present in the metaData_ hash.
+	metaData_["dateTime"] = QDateTime::currentDateTime();
+	metaData_["notes"] = QString();
+	metaData_["image"] = QVariant();
+
+	this->setName(sampleName);
+}
+
+/// This constructor immediately loads a stored sample from the database.
+AMSample::AMSample(int databaseId, AMDatabase* database, QObject* parent)
+	: AMDbObject(parent)
+{
+	/// Initialize our unique pieces of meta-data. The values don't matter much, but we want to make sure that they're present in the metaData_ hash.
+	metaData_["dateTime"] = QDateTime::currentDateTime();
+	metaData_["notes"] = QString();
+	metaData_["image"] = QVariant();
+
+	loadFromDb(database, databaseId);
+}
+
+
+///
