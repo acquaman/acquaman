57c699f6fcd2992d5f60bbee108579350a0a99bb
Rachel Si <rsi@ualberta.ca>
2010-07-23 13:50:04 -0600

    updating...

--- source/ui/ComboBox.cpp
+++ source/ui/ComboBox.cpp
@@ -1,1 +1,28 @@
+#include <QGui>
+
+#include "ComboBox.h"
+#include <QSqlQuery>
+
+ComboBox:: ComboBox(QWidget *parent)
+	: QComboBox(parent)
+{
+/// Getting required information from database
+	/* will need from database:
+	   * run name
+	   * run date
+	   * run id
+	*/
+
+	// acquiring run name (will be in String format)
+	QSqlQuery q = database()->query();
+	q.prepare ("SELECT runName FROM runs WHERE ... ");
+	q.bindValue(0, runName());
+
+
+
+
+
+
+}
+
 
---
+++ source/ui/ComboBox.h
@@ -0,0 +1,17 @@
+#ifndef COMBOBOX_H
+#define COMBOBOX_H
+
+#include <QComboBox>
+
+class ComboBox: public QComboBox
+{
+	Q_Object
+
+public:
+	ComboBox (QWidget *parent = 0);
+
+private slots:
+
+};
+
+#endif // COMBOBOX_H
