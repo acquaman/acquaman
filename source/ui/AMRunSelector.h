21f4815869500ed1bba15e85343abc0946536e41
Rachel Si <rsi@ualberta.ca>
2010-08-24 21:30:48 -0500

    added AMRunSelector

---
+++ source/ui/AMRunSelector.h
@@ -0,0 +1,45 @@
+#ifndef COMBOBOX_H
+#define COMBOBOX_H
+
+#include <QComboBox>
+#include <QString>
+#include <QList>
+#include <QVBoxLayout>
+#include <dataman/AMRun.h>
+#include "dataman/AMDatabase.h"
+#include "ui/AMNewRunDialog.h"
+
+
+class AMRunSelector: public QComboBox
+{
+	Q_OBJECT
+
+
+public:
+	AMRunSelector (QWidget *parent=0); //Constructor
+	virtual ~AMRunSelector();            //Destructor
+
+	int currentRunId() const ;
+
+public slots:
+	void setCurrentRunId(int runId);
+
+protected slots:
+	void autoAddRuns();
+	void onComboBoxActivated(int index);
+	void newRunBox();
+	void addNewRun();
+	void onDatabaseUpdate(const QString&, int);
+
+signals:
+	void currentRunIdChanged(int runId);
+
+protected:
+
+	AMDatabase* database_;
+	AMNewRunDialog *box;
+	bool runUpdateScheduled_;
+
+};
+
+#endif // COMBOBOX_H
