0d65b441d7e198bd63f3c948663c8f67bd2a48b8
David Chevrier <david.k.chevrier@gmail.com>
2011-05-24 02:51:56 -0600

    Added new fileLoader for SGM DacqLib files collected with acquaman. Tightened the reins a little so I can be certain of what is and isn't a spectral column. Now the new loader can handle 0, 1, 2 and (maybe) n different 1D spectal columns placed into the spectra.dat file. First working version, need to test on the OPIs.

---
+++ source/dataman/SGM2011XASFileLoader.h
@@ -0,0 +1,53 @@
+#ifndef SGM2011XASFILELOADER_H
+#define SGM2011XASFILELOADER_H
+
+#include "dataman/AMAbstractFileLoader.h"
+#include "util/AMBiHash.h"
+#include "dataman/AMDetectorInfo.h"
+
+class AMXASScan;
+
+/// This class encapsulates loading from SGM DacqLibrary data files (XAS scans made with Acquaman from 2011 to ?) to an AMXASScan. It can only be used for loading files that we already have in the database which happen to store their data in this file format. This loader is far more restrictive than the 2004FileLoader as it needs to be able to convert all the PV name it encounters to column names; however, it can also load data with more than one spectral component.
+class SGM2011XASFileLoader : public AMAbstractFileLoader
+{
+public:
+	SGM2011XASFileLoader(AMXASScan *scan = 0);
+
+	QString formatTag() const { return "sgm2011XAS"; }
+
+	/// load from SGM dacqLibrary file format (the setMetaData flag is not used)
+	bool loadFromFile(const QString& filepath, bool setMetaData, bool setRawDataSources, bool createDefaultAnalysisBlocks);
+
+	/// save to SGM legacy file format (\todo UNIMPLEMENTED)
+	bool saveToFile(const QString& filepath) {
+		Q_UNUSED(filepath)
+		return false;
+	}
+
+	/// translate a PV string from the file header to a meaningful name, if we recognize it.  Unrecognized PV strings are untouched and false is returned.
+	static bool pv2columnName(QString& pv) {
+		QString newName = columns2pvNames_.valueR(pv);
+		if(newName.isEmpty())
+			return false;
+		pv = newName.section('_', 0, 0);//Want to remove anything after the underscore
+		return true;
+	}
+
+protected:
+
+	/// A forward- and reverse-mapping from meaningful data column names to process variable strings
+	static AMBiHash<QString, QString> columns2pvNames_;
+
+	/* Want/need to use this ... get some help
+	/// A forward- and reverse-mapping from offset data (for the spectra.dat files) to their measurement infos
+	static AMBiHash<QString, AMMeasurementInfo> offsets2MeasurementInfos_;
+	*/
+	/**/
+	QStringList offsets2MeasurementInfos_;
+	/**/
+
+	/// A list (in order) of the column names that our users might be interested in (ie: those which we should expose the raw data for, by default).  These names should match the column names in columns2pvNames_.
+	QStringList defaultUserVisibleColumns_;
+};
+
+#endif // SGM2011XASFILELOADER_H
