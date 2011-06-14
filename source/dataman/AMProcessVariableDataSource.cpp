1bdb708473994399b4c68798459dd7ebe00cb541
Mark Boots <mark.boots@usask.ca>
2011-06-14 00:03:36 -0600

    improves performance in AM1DProcessVariableDataSource and AM2DProcessVariableDataSource by not creating a complete QList<AMAxisInfo> structure every time someone checks the size(), etc.

--- source/dataman/AMProcessVariableDataSource.cpp
+++ source/dataman/AMProcessVariableDataSource.cpp
@@ -15,6 +15,11 @@ AM1DProcessVariableDataSource::AM1DProcessVariableDataSource(const AMProcessVari
 	scale_ = 1;
 	connect(data_, SIGNAL(valueChanged()), this, SLOT(onDataChanged()));
 	connect(data_, SIGNAL(hasValuesChanged(bool)), this, SLOT(onStateChanged()));
+
+	AMAxisInfo ai("xAxis", 0, "Array X-axis");
+	ai.isUniform = true;
+	ai.increment = scale_;
+	axes_ << ai;
 }
 
 AM2DProcessVariableDataSource::AM2DProcessVariableDataSource(const AMProcessVariable *data, const QString &name, int rowLength, QObject *parent)
@@ -26,4 +31,12 @@ AM2DProcessVariableDataSource::AM2DProcessVariableDataSource(const AMProcessVari
 	length_ = rowLength;
 	connect(data_, SIGNAL(valueChanged()), this, SLOT(onDataChanged()));
 	connect(data_, SIGNAL(hasValuesChanged(bool)), this, SLOT(onStateChanged()));
+
+	AMAxisInfo xaxis("xAxis", length_, "Image X-axis");
+	xaxis.isUniform = true;
+	xaxis.increment = sx_;
+	AMAxisInfo yaxis("yAxis", data_->count()/length_, "Image Y-axis");
+	yaxis.isUniform = true;
+	yaxis.increment = sy_;
+	axes_ << xaxis << yaxis;
 }
