78a7c1b23619a4ecd3c31434e8a4f0e0c4edb8c0
Mark Boots <mark.boots@usask.ca>
2011-07-27 17:59:03 -0600

    adds forgotten AMCompositeControl to 3f6ff413ee975943f7e2

---
+++ source/beamline/AMCompositeControl.cpp
@@ -0,0 +1,39 @@
+#include "AMCompositeControl.h"
+
+AMCompositeControl::AMCompositeControl(const QString& name, const QString& units, QObject* parent, const QString description) :
+	AMControl(name, units, parent, description)
+{
+}
+
+void AMCompositeControl::addChildControl(AMControl *control)
+{
+	children_ << control;
+	connect(control, SIGNAL(connected(bool)), this, SLOT(onChildControlConencted()));
+	connect(control, SIGNAL(movingChanged(bool)), this, SLOT(onChildControlMovingChanged()));
+}
+
+void AMCompositeControl::onChildControlConnected()
+{
+	bool nowConnected = isConnected();
+
+	// Losing the connection:
+	if(wasConnected_ && !nowConnected ) {
+		emit connected(wasConnected_ = nowConnected);
+	}
+
+	// gaining the connection:
+	if(!wasConnected_ && nowConnected ) {
+		emit connected(wasConnected_ = nowConnected);
+	}
+}
+
+void AMCompositeControl::onChildControlMovingChanged()
+{
+	bool nowMoving = isMoving();
+
+	if(wasMoving_ && !nowMoving)
+		emit movingChanged(wasMoving_ = nowMoving);
+
+	if(!wasMoving_ && nowMoving)
+		emit movingChanged(wasMoving_ = nowMoving);
+}
