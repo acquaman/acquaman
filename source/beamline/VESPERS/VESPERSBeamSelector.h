6304b925499c877f302c622298403a1d19d574cb
Darren Hunter <darren.hunter@usask.ca>
2011-08-10 22:17:07 -0600

    VESPERS:  Fleshed out more of the beam selector class.  However, I need to have access to some of the PVs for the monochromator before I finish it.  In particular the SCAN ALLOW PV, and rather than just adding it to this class I'm going to make a VESPERSMonochromator class.

--- source/beamline/VESPERS/VESPERSBeamSelector.h
+++ source/beamline/VESPERS/VESPERSBeamSelector.h
@@ -2,6 +2,7 @@
 #define VESPERSBEAMSELECTOR_H
 
 #include <QObject>
+#include <QHash>
 
 #include "beamline/CLS/CLSVMEMotor.h"
 
@@ -21,7 +22,7 @@ public:
 		- OnePointSixPercent (1.6%) is a narrow band pass filter.
 		- Si is the monochromator with 0.01% band pass.
 	  */
-	enum Beam { Pink = 0, TenPercent, OnePointSixPercent, Si };
+	enum Beam { None = 0, Pink, TenPercent, OnePointSixPercent, Si };
 
 	/// Constructor.
 	explicit VESPERSBeamSelector(QObject *parent = 0);
@@ -37,11 +38,21 @@ public slots:
 	/// Changes the beam.  Moves the motor to the necessary position.
 	void changeBeam(Beam beam);
 
+protected slots:
+	/// Determines is currently active on startup.  Also keeps track if the beam is changed outside of Acquaman.  Beam is set to None is if not inside any of the tolerances for the known beam positions.
+	void determineBeam();
+
 protected:
+	/// Helper function that takes in a desired beam and returns true or false whether the motor is within that beam's tolerance.
+	bool isAtBeam(Beam beam) { return (fabs(motor_->step()-positions_.value(beam)) < 1000) ? true : false; }
+
 	/// The current beam in use by the beamline.
 	Beam beam_;
 	/// Pointer to the motor that controls which beam makes it down the beamline.
 	CLSVMEMotor *motor_;
+
+	/// Look up table with the beam and its position.
+	QHash<Beam, int> positions_;
 };
 
 #endif // VESPERSBEAMSELECTOR_H
