b1ee537aaf28e38f1e48305e38da2780e57ebf51
David Chevrier <david.k.chevrier@gmail.com>
2011-08-14 16:55:21 -0600

    Deleted unused VESPERS code. Grabbed trimmed version of BareBonesMain.

--- source/application/BareBones/BareBonesMain.cpp
+++ source/application/BareBones/BareBonesMain.cpp
@@ -23,13 +23,6 @@ along with Acquaman.  If not, see <http://www.gnu.org/licenses/>.
 #include <QApplication>
 #include "application/AMAppController.h"
 
-#include "beamline/AMProcessVariable.h"
-#include <QDoubleSpinBox>
-#include "ui/AMControlEditor.h"
-#include "dataman/AMScan.h"
-#include "ui/AMScanView.h"
-#include "ui/AMGenericScanEditor.h"
-
 int main(int argc, char *argv[])
 {
 
@@ -40,102 +33,6 @@ int main(int argc, char *argv[])
 
 
 	AMAppController* appController = new AMAppController();
-
-//	// Create a process variable connection:
-//	AMProcessVariable* myPV = new AMProcessVariable("PCT1402-01:mA:fbk", true);
-
-//	// Make some widgets to watch it:
-//	QDoubleSpinBox* readbackMonitor = new QDoubleSpinBox();
-//	QDoubleSpinBox* setpointEditor = new QDoubleSpinBox();
-
-//	// widget layout code here
-//	// ...
-//	/////////////
-
-//	// Make connections
-//	connect(myPV, SIGNAL(valueChanged(double)), readbackMonitor, SLOT(setValue(double)));
-//	connect(setpointEditor, SIGNAL(valueChanged(double)), myPV, SLOT(setValue(double)));
-
-//	// Let's assume we have one already:
-//	AMProcessVariable* myPV = MyBeamline::bl()->myPV1();
-//	// Or create and connect one:
-//	AMProcessVariable* myPV = new AMProcessVariable("PCT1402-01:mA:fbk", true);
-
-//	myPV->setValue(3.0);
-//	myPV->setValue(QString("3.4"));
-//	myPV->setValue(3);
-
-//	myPV->lastValue();	// 3.0 (most recent monitored value)
-//						// (If monitoring is on)
-
-//	myPV->getInt();		// 3
-//	myPV->getDouble();	// 3.0
-//	myPV->getString();	// "3.0"
-
-//	connect(myPV, SIGNAL(valueChanged(double)), someObject, SLOT(someSlot(double)));
-
-
-//	// Is it connected?
-//	myPV->isConnected();	// true  (After some time)
-//	// permissions:
-//	myPV->canRead();	// true
-//	myPV->canWrite();	// also true (interesting, for the ring current)
-
-//	// Other handy tricks:
-//	myPV->units();	// "mA"
-
-
-
-//	AMPVControl* myControl =
-//	        new AMPVwStatusControl("hexapodX",
-//	                               "HXPD1610-401:X:sp",
-//	                               "HXPD1610-401:X",
-//	                               "HXPD1610-401:moving",
-//	                               QString(),
-//	                               this,
-//	                               0.1);
-
-//	AMBasicControlEditor* myEditor =
-//	        new AMBasicControlEditor(myControl, this);
-
-//	AMBeamline::bl()
-
-
-
-//AMScan s;
-//// Get it full and interesting...
-//// ...
-//int databaseId = s.storeToDb(AMDatabase::userdb());
-
-//// Restore it back from the database:
-//AMScan s2;
-//s2.loadFromDb(AMDatabase::userdb(), databaseId);
-
-//	AMScanView* view = new AMScanView();
-//	AMScan* scan1 = getFromSomewhere();
-//	AMScan* scan2 = getFromSomewhere();
-
-//	view->addScan(scan1);
-//	view->addScan(scan2);
-
-//	AMGenericScanEditor* editor = new AMGenericScanEditor();
-
-
-//MyAnalysisBlock* b1  = new MyAnalysisBlock(...);
-//My2ndAnalysisBlock* b2 = new My2ndAnalysisBlock(...);
-
-//QList<AMDataSources*> input1;
-//input1 << src1;
-//input1 << src2;
-//b1->setInputDataSources(input1);
-
-//QList<AMDataSource*> input2;
-//intput2 << b1;
-//input2 << src3;
-//b2->setInputDataSources(input2);
-
-
-
 
 	/// Program Run-loop:
 	// =================================
