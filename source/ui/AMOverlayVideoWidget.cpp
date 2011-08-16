63ae8a5634fe84ab24721f5f00cdc4eb74aa06ab
David Chevrier <david.k.chevrier@gmail.com>
2011-08-16 16:33:56 -0600

    Grabbed AMOverlayVideoWidget.h and .cpp from master.

--- source/ui/AMOverlayVideoWidget.cpp
+++ source/ui/AMOverlayVideoWidget.cpp
@@ -1,26 +1,57 @@
 #include "AMOverlayVideoWidget.h"
+#include <QResizeEvent>
 
 #include <QGLWidget>
 
-#include <QPushButton>
+#include <QGraphicsVideoItem>
 
-AMOverlayVideoWidget::AMOverlayVideoWidget(QWidget *parent) :
+
+AMOverlayVideoWidget::AMOverlayVideoWidget(QWidget *parent, bool useOpenGlViewport) :
 	QGraphicsView(parent)
 {
+	setSizePolicy(QSizePolicy::MinimumExpanding, QSizePolicy::MinimumExpanding);
 
-	//setViewport(new QGLWidget(QGLFormat(QGL::SampleBuffers)));
-	//setViewport(new QGLWidget());
-	//setViewportUpdateMode(QGraphicsView::FullViewportUpdate);
+	if(useOpenGlViewport) {
+		// setViewport(new QGLWidget());
+		setViewport(new QGLWidget(QGLFormat(QGL::SampleBuffers)));
+		setViewportUpdateMode(QGraphicsView::FullViewportUpdate);
+	}
+
 	setScene(new QGraphicsScene());
+	scene()->setBackgroundBrush(QBrush(QColor(50,50,50)));
 
-	videoPlayer_ = new Phonon::VideoPlayer();
-	scene()->addWidget(videoPlayer_);
+	videoItem_ = new QGraphicsVideoItem();
+	mediaPlayer_ = new QMediaPlayer();
+	mediaPlayer_->setVideoOutput(videoItem_);
 
+	scene()->addItem(videoItem_);
 	resize(size());
 
-	QPushButton* test = new QPushButton("Help!");
-	scene()->addWidget(test);
+	// Widgets on top!
+//	QPushButton* test = new QPushButton("Help!");
+//	scene()->addWidget(test);
 
-	// phonon version is 4.4.0
+}
+
+AMOverlayVideoWidget::~AMOverlayVideoWidget() {
+	// necessary to avoid a bug on Mac, where if the video widget is deleted while the mediaPlayer is still playing, and then the media player is deleted (also while still playing), there is a crash in:
+	/* QT7MovieViewRenderer::setupVideoOutput() from
+  QT7PlayerSession::~Qt7PlayerSession() from
+  QMediaService::~QMediaService()
+  QMediaPlayer::~QMediaPlayer()
+  */
+	mediaPlayer_->setMedia(QMediaContent());
+
+	delete videoItem_;
+	delete mediaPlayer_;
+}
+
+void AMOverlayVideoWidget::resizeEvent(QResizeEvent *event)
+{
+	if (scene()) {
+		scene()->setSceneRect(QRect(QPoint(0, 0), event->size()));
+		videoItem_->setSize(event->size());
+	}
+	QGraphicsView::resizeEvent(event);
 }
 
--- source/ui/AMOverlayVideoWidget.h
+++ source/ui/AMOverlayVideoWidget.h
@@ -2,31 +2,57 @@
 #define AMOVERLAYVIDEOWIDGET_H
 
 #include <QGraphicsView>
-#include <QResizeEvent>
-#include <phonon/VideoPlayer>
+#include <QMediaPlayer>
 
+class QGraphicsVideoItem;
+
+/// Ever wanted to draw on top of live video? This is a convenience class that provides a QMediaPlayer, and a QGraphicsVideoItem embedded  inside a QGraphicsScene, so that other QGraphicsItems can be painted on top of it.
+/*! The class itself is a QGraphicsView widget, and it creates the following objects automatically for you:
+
+  - a QGraphicsScene to display all the visual stuff. (Access this via scene() to add more items.)
+  - a QGraphicsVideoItem that actually renders the video.
+  - a QMediaPlayer to load and control the video. Access this (for example, to set the URL of the video to play using setMedia()) via mediaPlayer().
+
+  <b>Resizing</b>
+
+  When this widget is resized, the scene inside the view it will be resized to keep the same size as the whole widget.  The internal video item will also be resized to fill the entire scene.
+  */
 class AMOverlayVideoWidget : public QGraphicsView
 {
 	Q_OBJECT
 public:
-	explicit AMOverlayVideoWidget(QWidget *parent = 0);
+	/// Constructor.  If you don't want to (or can't) use OpenGL acceleration on the view, you can set \c useOpenGLView to false. However, leaving it on provides a huge reduction in CPU usage, at least on Mac OS X.
+	explicit AMOverlayVideoWidget(QWidget *parent = 0, bool useOpenGlView = true);
+	/// Destructor.
+	~AMOverlayVideoWidget();
 
-	Phonon::VideoPlayer* videoPlayer() { return videoPlayer_; }
+	/// Access the internal QMediaPlayer that controls playback of video.
+	/*! For example, to use this widget to start playing a network stream:
+
+	  \code
+	  mediaPlayer()->setMedia(QUrl("http://v2e1607-001.cs.clsi.ca/mjpg/1/video.mjpg"));
+	  mediaPlayer()->play();
+	  \endcode
+
+	  \note QMediaPlayer is part of the QMultimediaKit module in the QtMobility add-on component. It may be included in Qt proper in 4.8 or 5.  See http://doc.qt.nokia.com/qtmobility-1.1.3/multimedia.html for details.
+	  */
+	QMediaPlayer* mediaPlayer() { return mediaPlayer_; }
+
+	/// Access the QGraphicsVideoItem embedded inside this QGraphicsView/scene.
+	/*! \note QGraphicsVideoItem is part of the QMultimediaKit module in the QtMobility add-on component. It may be included in Qt proper in 4.8 or 5.  See http://doc.qt.nokia.com/qtmobility-1.1.3/multimedia.html for details.
+	  */
+	QGraphicsVideoItem* videoItem() { return videoItem_; }
 
 signals:
 
 public slots:
 
 protected:
-	void resizeEvent(QResizeEvent *event) {
-		if (scene()) {
-			scene()->setSceneRect(QRect(QPoint(0, 0), event->size()));
-			videoPlayer_->setGeometry(QRect(QPoint(0,0), event->size()));
-		}
-		QGraphicsView::resizeEvent(event);
-	}
+	/// When this widget is resized, the scene inside the view it will be resized to keep the same size as the whole widget.  The internal videoItem() will also be resized to fill the entire scene.
+	void resizeEvent(QResizeEvent *event);
 
-	Phonon::VideoPlayer* videoPlayer_;
+	QGraphicsVideoItem* videoItem_;
+	QMediaPlayer* mediaPlayer_;
 
 };
 
