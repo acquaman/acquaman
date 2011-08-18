89c30291658444439ac91ab9766f64c68669b61c
Darren Hunter <darren.hunter@usask.ca>
2011-08-18 14:55:39 -0600

    VESPERS: Forgot to add the new split ion chamber to the layout.

--- source/ui/VESPERS/VESPERSIonChamberCalibrationView.cpp
+++ source/ui/VESPERS/VESPERSIonChamberCalibrationView.cpp
@@ -15,12 +15,14 @@ VESPERSIonChamberCalibrationView::VESPERSIonChamberCalibrationView(VESPERSIonCha
 	calibrationLayout->addWidget(new QLabel("Sensitivity Value and Units"), 0, 1, 1, 2, Qt::AlignCenter);
 	calibrationLayout->addWidget(new QLabel("Voltage (V)"), 0, 3, Qt::AlignCenter);
 
+	calibrationLayout->addWidget(new VESPERSSplitIonChamberView(calibration_->splitIonChamber()), 1, 0, 1, 4);
+
 	VESPERSIonChamberView *temp;
 
 	for (int i = 0; i < calibration_->ionChamberCount(); i++){
 
 		temp = new VESPERSIonChamberView(calibration_->ionChamberAt(i));
-		calibrationLayout->addWidget(temp, i+1, 0, 1, 4);
+		calibrationLayout->addWidget(temp, i+2, 0, 1, 4);
 	}
 
 	setLayout(calibrationLayout);
