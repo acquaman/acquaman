862cc1abef8f6fa5d9c30aadbb0802799e72a6b6
David Chevrier <david.k.chevrier@gmail.com>
2011-08-25 21:08:12 -0600

    Fixed location of AMDetectorInfoList from acquaman to dataman folder. Related changes to common.pri, AMDetectorSet, and AMFirstTimeController are for pathing changes.

--- source/dataman/AMFirstTimeController.cpp
+++ source/dataman/AMFirstTimeController.cpp
@@ -29,7 +29,7 @@ along with Acquaman.  If not, see <http://www.gnu.org/licenses/>.
 #include <dataman/AMExperiment.h>
 #include <dataman/AMControlInfoList.h>
 #warning "Hey David, you need to move this file to dataman, clearly"
-#include <acquaman/AMDetectorInfoList.h>
+#include <dataman/AMDetectorInfoList.h>
 #include <dataman/AMSamplePlate.h>
 #include <dataman/AMSpectralOutputDetectorInfo.h>
 #include "dataman/AMUser.h"
