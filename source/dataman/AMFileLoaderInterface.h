b0d83f2f1dba3c6ab934a34c3b2fb054909d1f4f
David Chevrier <david.k.chevrier@gmail.com>
2011-09-01 21:18:09 -0600

    Missed this one (was an add) in first stab at file loader plugins.

---
+++ source/dataman/AMFileLoaderInterface.h
@@ -0,0 +1,22 @@
+#ifndef AMFILELOADERINTERFACE_H
+#define AMFILELOADERINTERFACE_H
+
+//#include "dataman/AMScan.h"
+
+class AMScan;
+
+class AMFileLoaderInterface
+{
+public:
+	virtual ~AMFileLoaderInterface() {}
+	virtual bool accepts(AMScan *scan) = 0;
+
+	virtual bool load(AMScan *scan, const QString &userDataFolder) = 0;
+};
+
+QT_BEGIN_NAMESPACE
+Q_DECLARE_INTERFACE(AMFileLoaderInterface,
+		    "AMFileLoaderInterface/1.0");
+QT_END_NAMESPACE
+
+#endif // AMFILELOADERINTERFACE_H
