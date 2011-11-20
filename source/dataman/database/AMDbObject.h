310a51d57c2dc3a8befd720a84af20bf339bc369
Mark Boots <mark.boots@usask.ca>
2011-11-19 23:21:02 -0600

    fixes valgrind warning for jump based on unintialized AMDbObject::modified_

--- source/dataman/database/AMDbObject.h
+++ source/dataman/database/AMDbObject.h
@@ -326,7 +326,7 @@ public slots:
 protected:
 
 	/// Subclasses should call this to set or un-set the modified flag.  Handles emission of the modifiedChanged() signal when required.
-	void setModified(bool isModified) {
+	void setModified(bool isModified = true) {
 		if(isModified != modified_)
 			emit modifiedChanged(modified_ = isModified);
 		modified_ = isModified;
