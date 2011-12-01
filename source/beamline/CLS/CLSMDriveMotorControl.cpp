1424cb078704481dd5b803c83146299625b48f65
Mark Boots <mark.boots@usask.ca>
2011-12-01 14:32:39 -0600

    CLS: Fixes name of encoder readback PV in CLSMDriveMotorControl

--- source/beamline/CLS/CLSMDriveMotorControl.cpp
+++ source/beamline/CLS/CLSMDriveMotorControl.cpp
@@ -3,6 +3,6 @@
 #include <QStringBuilder>
 
 CLSMDriveMotorControl::CLSMDriveMotorControl(const QString &name, const QString &baseName, const QString& units, double unitsPerRev, double offset, int microsteps, const QString &description, double tolerance, double moveStartTimeoutSeconds, QObject *parent)
-	: AMPVwStatusAndUnitConversionControl(name, baseName % ":step:fbk", baseName % ":step", baseName % ":status", baseName % ":stop", new AMScaleAndOffsetUnitConverter(units, unitsPerRev/4000.0, offset), new AMScaleAndOffsetUnitConverter(units, unitsPerRev/(200.0*microsteps), offset), parent, tolerance, moveStartTimeoutSeconds, new AMControlStatusCheckerDefault(1), 1, description) {
+	: AMPVwStatusAndUnitConversionControl(name, baseName % ":enc:fbk", baseName % ":step", baseName % ":status", baseName % ":stop", new AMScaleAndOffsetUnitConverter(units, unitsPerRev/4000.0, offset), new AMScaleAndOffsetUnitConverter(units, unitsPerRev/(200.0*microsteps), offset), parent, tolerance, moveStartTimeoutSeconds, new AMControlStatusCheckerDefault(1), 1, description) {
 
 }
