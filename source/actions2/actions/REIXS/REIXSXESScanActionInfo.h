8f0a26ea8ff75689b7188d4473928de6f4d79df0
Mark Boots <mark.boots@usask.ca>
2012-02-19 17:59:47 -0600

    REIXS: gives REIXSXESScanActionInfo a valid default constructor, so it can be added to a workflow using AMAddActionDialog.

--- source/actions2/actions/REIXS/REIXSXESScanActionInfo.h
+++ source/actions2/actions/REIXS/REIXSXESScanActionInfo.h
@@ -2,8 +2,7 @@
 #define REIXSXESSCANACTIONINFO_H
 
 #include "actions2/actions/AMScanControllerActionInfo.h"
-
-class REIXSXESScanConfiguration;
+#include "acquaman/REIXS/REIXSXESScanConfiguration.h"
 
 /// This is a subclass of AMScanControllerActionInfo for the purpose of registering REIXS XES Scan actions with the AMActionRegistry.
 class REIXSXESScanActionInfo : public AMScanControllerActionInfo
@@ -12,21 +11,22 @@ class REIXSXESScanActionInfo : public AMScanControllerActionInfo
 	Q_PROPERTY(AMDbObject* scanConfig READ dbReadScanConfig WRITE dbLoadScanConfig)
 
 public:
-	/// Constructor: takes a REIXSXESScanConfiguration \c scanConfig, but still needs a default constructor for database loading
-	Q_INVOKABLE REIXSXESScanActionInfo(REIXSXESScanConfiguration* scanConfig = 0, QObject *parent = 0);
-	/// Copy constructor. The base class takes care of making a copy of \c other's scan configuration, if there is one.
+	/// Constructor: takes a REIXSXESScanConfiguration \c scanConfig, but still needs a default constructor for database loading.
+	/*! If no \c scanConfig is provided, we create a valid REIXSXESScanConfiguration.*/
+	Q_INVOKABLE REIXSXESScanActionInfo(REIXSXESScanConfiguration* scanConfig = new REIXSXESScanConfiguration(), QObject *parent = 0);
+	/// Copy constructor. The base class takes care of making a copy of \c other's scan configuration.
 	REIXSXESScanActionInfo(const REIXSXESScanActionInfo& other) : AMScanControllerActionInfo(other) {}
 	/// Virtual copy constructor
 	virtual AMActionInfo* createCopy() const { return new REIXSXESScanActionInfo(*this); }
 
 
 
-	/// Convenience function to extract the detector energy from the config. Returns 0 if this was a default-constructed actionInfo with no valid configuration.
-	double configCenterEV() const;
-	/// Convenience function to extract the defocus distance from the config. Returns 0 if this was a default-constructed actionInfo with no valid configuration.
-	double configDefocusMm() const;
-	/// Convenience function to extract the tilt offset from the config. Returns 0 if this was a default-constructed actionInfo with no valid configuration.
-	double configTiltOffset() const;
+	/// Convenience function to extract the detector energy from the config.
+	double configCenterEV() const { return xesConfig()->centerEV(); }
+	/// Convenience function to extract the defocus distance from the config.
+	double configDefocusMm() const { return xesConfig()->defocusDistanceMm(); }
+	/// Convenience function to extract the tilt offset from the config.
+	double configTiltOffset() const { return xesConfig()->detectorTiltOffset(); }
 
 signals:
 
@@ -42,12 +42,12 @@ public slots:
 protected:
 	/// Saves the scan config in the database, and also used to load the scan config from the db whenever we already have a valid one.
 	AMDbObject* dbReadScanConfig();
-	/// Used to load the scan config from the database when we don't have a valid one yet.
+	// Used to load the scan config from the database when we don't have a valid one yet.  This will never actually be called, since even the default-constructed instance returns a valid object for dbReadScanConfig(). Loading from the database will just call a dbLoad() on that valid scan configuration.
 	void dbLoadScanConfig(AMDbObject* newObject);
 
 	/// The base class scanConfig() returns our REIXSXESScanConfiguration as an AMScanConfiguration. Here we return it for convenience as a REIXSXESScanConfiguration.
-	REIXSXESScanConfiguration* xesConfig();
-	const REIXSXESScanConfiguration* xesConfig() const;
+	REIXSXESScanConfiguration* xesConfig() { return qobject_cast<REIXSXESScanConfiguration*>(scanConfig_); }
+	const REIXSXESScanConfiguration* xesConfig() const { return qobject_cast<const REIXSXESScanConfiguration*>(scanConfig_); }
 };
 
 #endif // REIXSXESSCANACTIONINFO_H
