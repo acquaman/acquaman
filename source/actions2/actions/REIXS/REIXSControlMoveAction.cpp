98cfdf00497db9376c1ae570f03ba3bc9ae11ec8
Mark Boots <mark.boots@usask.ca>
2012-02-19 18:02:42 -0600

    REIXS: Enables more controls to be used with REIXSControlMoveAction. TODO: clean up by using an AMControlSet inside REIXSBeamline.

--- source/actions2/actions/REIXS/REIXSControlMoveAction.cpp
+++ source/actions2/actions/REIXS/REIXSControlMoveAction.cpp
@@ -25,18 +25,18 @@ void REIXSControlMoveAction::populateControlLookup() const
 
 	name2Control_.insert("spectrometer", bl->spectrometer());
 
-	// Deciding not to make directly accessible to actions:
+	// These should probably not be shown to end-users, but we want them for comissioning
 
-//	name2Control_.insert("spectrometerRotationDrive", bl->spectrometer()->spectrometerRotationDrive());
-//	name2Control_.insert("detectorTranslation", bl->spectrometer()->detectorTranslation());
-//	name2Control_.insert("detectorTiltDrive", bl->spectrometer()->detectorTiltDrive());
-//	name2Control_.insert("hexapodX", bl->spectrometer()->hexapod()->x());
-//	name2Control_.insert("hexapodX", bl->spectrometer()->hexapod()->y());
-//	name2Control_.insert("hexapodX", bl->spectrometer()->hexapod()->z());
-//	name2Control_.insert("hexapodR", bl->spectrometer()->hexapod()->r());
-//	name2Control_.insert("hexapodS", bl->spectrometer()->hexapod()->s());
-//	name2Control_.insert("hexapodT", bl->spectrometer()->hexapod()->t());
-//	name2Control_.insert("hexapodU", bl->spectrometer()->hexapod()->u());
-//	name2Control_.insert("hexapodV", bl->spectrometer()->hexapod()->v());
-//	name2Control_.insert("hexapodW", bl->spectrometer()->hexapod()->w());
+	name2Control_.insert("spectrometerRotationDrive", bl->spectrometer()->spectrometerRotationDrive());
+	name2Control_.insert("detectorTranslation", bl->spectrometer()->detectorTranslation());
+	name2Control_.insert("detectorTiltDrive", bl->spectrometer()->detectorTiltDrive());
+	name2Control_.insert("hexapodX", bl->spectrometer()->hexapod()->x());
+	name2Control_.insert("hexapodX", bl->spectrometer()->hexapod()->y());
+	name2Control_.insert("hexapodX", bl->spectrometer()->hexapod()->z());
+	name2Control_.insert("hexapodR", bl->spectrometer()->hexapod()->r());
+	name2Control_.insert("hexapodS", bl->spectrometer()->hexapod()->s());
+	name2Control_.insert("hexapodT", bl->spectrometer()->hexapod()->t());
+	name2Control_.insert("hexapodU", bl->spectrometer()->hexapod()->u());
+	name2Control_.insert("hexapodV", bl->spectrometer()->hexapod()->v());
+	name2Control_.insert("hexapodW", bl->spectrometer()->hexapod()->w());
 }
