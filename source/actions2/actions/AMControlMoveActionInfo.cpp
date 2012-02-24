78190e5e2f915a8c41a5473787c19be519e66459
Mark Boots <mark.boots@usask.ca>
2012-02-24 15:25:33 -0600

    Fixes a missing initialization of isRelative_ in AMControlMoveActionInfo.

--- source/actions2/actions/AMControlMoveActionInfo.cpp
+++ source/actions2/actions/AMControlMoveActionInfo.cpp
@@ -1,15 +1,17 @@
 #include "AMControlMoveActionInfo.h"
 
 AMControlMoveActionInfo::AMControlMoveActionInfo(const AMControlInfo &setpoint, QObject *parent)
-	: AMActionInfo(QString("Moving %1 to %2 %3").arg(setpoint.description().isEmpty() ? setpoint.name() : setpoint.description()).arg(setpoint.value()).arg(setpoint.units()),
-				   QString("Moving %1 to %2 %3").arg(setpoint.description().isEmpty() ? setpoint.name() : setpoint.description()).arg(setpoint.value()).arg(setpoint.units()),
-				   ":/system-run.png", parent)
+    : AMActionInfo("Control Move", "Control Move", ":/system-run.png", parent)
 {
 	controlInfo_.setValuesFrom(setpoint);
+    isRelative_ = false;
+
+    updateDescriptionText();
 }
 
 AMControlMoveActionInfo::AMControlMoveActionInfo(const AMControlMoveActionInfo &other) : AMActionInfo(other) {
 	controlInfo_.setValuesFrom(*(other.controlInfo()));
+    isRelative_ = other.isRelative_;
 }
 
 void AMControlMoveActionInfo::setSetpoint(double setpoint)
