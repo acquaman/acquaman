b125dc675a231a2e358c4302b832584cebee60e7
Mark Boots <mark.boots@usask.ca>
2012-03-12 19:09:25 -0600

    REIXS: Fixes font size in REIXSXESSpectrometerControlEditor.

--- source/ui/REIXS/REIXSXESSpectrometerControlEditor.cpp
+++ source/ui/REIXS/REIXSXESSpectrometerControlEditor.cpp
@@ -24,11 +24,17 @@ along with Acquaman.  If not, see <http://www.gnu.org/licenses/>.
 #include "util/AMErrorMonitor.h"
 #include <QStringBuilder>
 
+#include "util/AMFontSizes.h"
+
 REIXSXESSpectrometerControlEditor::REIXSXESSpectrometerControlEditor(REIXSSpectrometer* spectrometer, QWidget *parent) :
 	QGroupBox(parent),
 	ui_(new Ui::REIXSXESSpectrometerControlEditor)
 {
 	ui_->setupUi(this);
+	QFont font = ui_->gratingFeedbackLabel->font();
+	font.setPointSize(AM_FONT_SMALL);
+	ui_->gratingFeedbackLabel->setFont(font);
+	ui_->energyFeedbackLabel->setFont(font);
 
 	spectrometer_ = spectrometer;
 
