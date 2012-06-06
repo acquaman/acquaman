d6f14b629f3c1a6b5df96d59eea86d1eaba55e0d
Darren Hunter <darren.hunter@usask.ca>
2012-06-06 11:01:16 -0600

    Changed the export to pdf method to inform you if the file already exits.  Only overwrites if told explicitly.  The check is in AMScanView because it is the class in charge of the actual file copying.

--- source/ui/dataman/AMGenericScanEditor.cpp
+++ source/ui/dataman/AMGenericScanEditor.cpp
@@ -704,8 +704,8 @@ void AMGenericScanEditor::exportGraphicsToFile()
 			fileName.append(".pdf");
 
 		if(!using2DScanView()) {
-			scanView_->exportGraphicsFile(fileName);
-			AMErrorMon::information(this, 0, QString("Exported the current plot to '%1'").arg(fileName));
+			if (scanView_->exportGraphicsFile(fileName))
+				AMErrorMon::information(this, 0, QString("Exported the current plot to '%1'").arg(fileName));
 		}
 	}
 }
