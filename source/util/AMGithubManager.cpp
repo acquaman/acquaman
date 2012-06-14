66e57c8e09ef13ad39f5e2bfbdf05adfb821fe15
David Chevrier <david.k.chevrier@gmail.com>
2012-06-14 14:42:37 -0600

    Trying to see if I can determine the Jenkins issue with QSslError in AMGithubManager ... again by removing access to QSslError?

--- source/util/AMGithubManager.cpp
+++ source/util/AMGithubManager.cpp
@@ -241,10 +241,9 @@ void AMGithubManager::onSomeErrorOccured(QNetworkReply::NetworkError nError){
 	qDebug() << "Error occurred " << nError;
 }
 
-#include <QSslError>
 void AMGithubManager::onSomeSSLErrorOccurred(QList<QSslError> sslErrors){
-	for(int x = 0; x < sslErrors.count(); x++)
-		qDebug() << "SSL Error as " << sslErrors.at(x).errorString();
+//	for(int x = 0; x < sslErrors.count(); x++)
+//		qDebug() << "SSL Error as " << sslErrors.at(x).errorString();
 }
 
 void AMGithubManager::initialize(){
