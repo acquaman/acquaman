c3776ec4c039f27e9045295260ba835021eb5e2e
Darren Hunter <darren.hunter@usask.ca>
2012-07-25 11:50:35 -0600

    Fixed a bug in AM2DNormalization that would return properly when the normalizer contains negative numbers.

--- source/analysis/AM2DNormalizationAB.cpp
+++ source/analysis/AM2DNormalizationAB.cpp
@@ -226,6 +226,10 @@ AMNumber AM2DNormalizationAB::value(const AMnDIndex &indexes, bool doBoundsCheck
 	if (double(normalizer_->value(indexes)) == 0)
 		return 0;
 
+	// The normalizer must be positive.
+	if (double(normalizer_->value(indexes)) < 0)
+		return -1;
+
 	return double(data_->value(indexes))/double(normalizer_->value(indexes));
 }
 
