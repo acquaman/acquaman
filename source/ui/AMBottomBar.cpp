1c7f71f2858d69c7a64b7dea161a85981044d392
Mark Boots <mark.boots@usask.ca>
2012-07-26 23:11:35 -0600

    AMBottomBar: makes it possible to differentiate disabled and enabled buttons; adds additional slots for manipulating progress and time elapsed/remaining labels separately; adds a (default invisible) resume button.

--- source/ui/AMBottomBar.cpp
+++ source/ui/AMBottomBar.cpp
@@ -25,8 +25,11 @@ along with Acquaman.  If not, see <http://www.gnu.org/licenses/>.
 AMBottomBar::AMBottomBar(QWidget *parent) : QWidget(parent)
 {
 	setupUi(this);
+
 	timeElapsedLabel->setStyleSheet("color: white; font: " AM_FONT_SMALL_ "pt \"Lucida Grande\";");
 	timeRemainingLabel->setStyleSheet("color: white; font: " AM_FONT_SMALL_ "pt \"Lucida Grande\";");
+	progressBar->setStyleSheet("QProgressBar { border: 1px solid rgb(116, 116, 116);border-radius: 3px;background-color: rgb(42, 42, 42);text-align: center;color: white; font: " AM_FONT_SMALL_ "pt \"Lucida Grande\";} QProgressBar::chunk {background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:0, y2:1, stop:0 rgba(136, 136, 136, 255), stop:0.494444 rgba(133, 133, 133, 255), stop:0.5 rgba(121, 121, 121, 255), stop:1 rgba(116, 116, 116, 255));}");
+
 	addButton->setToolTip("Add Experiment Folder...");
 
 	statusView_ = new AMStatusView();
@@ -37,6 +40,7 @@ AMBottomBar::AMBottomBar(QWidget *parent) : QWidget(parent)
 	progressBar->setValue(100);
 	timeElapsedLabel->setText("0:00");
 	timeRemainingLabel->setText("0:00");
+	resumeScanButton->setVisible(false);	// hidden unless you want to show it.
 
 	makeConnections();
 
@@ -51,20 +55,53 @@ void AMBottomBar::makeConnections()
 {
 	connect(pauseScanButton, SIGNAL(clicked()), this, SIGNAL(pauseScanIssued()));
 	connect(stopScanButton, SIGNAL(clicked()), this, SIGNAL(stopScanIssued()));
+	connect(resumeScanButton, SIGNAL(clicked()), this, SIGNAL(resumeScanIssued()));
+
+	connect(restartScanButton, SIGNAL(clicked()), this, SIGNAL(restartScanIssued()));
+	connect(adjustScanFinishButton, SIGNAL(clicked()), this, SIGNAL(adjustFinishPointsIssued()));
+	connect(fullScreenButton, SIGNAL(clicked()), this, SIGNAL(fullScreenIssued()));
+
 	connect(addButton, SIGNAL(clicked()), this, SIGNAL(addButtonClicked()));
 }
 
 void AMBottomBar::updateScanProgress(double secondsElapsed, double secondsTotal){
 	progressBar->setMaximum((int)secondsTotal);
 	progressBar->setValue((int)secondsElapsed);
+
 	double secondsRemaining = secondsTotal - secondsElapsed;
-	QTime elapsed = QTime(0,0,0,0).addMSecs(int(1000*secondsElapsed));
-	QTime remaining = QTime(0,0,0,0).addMSecs(int(1000*secondsRemaining));
-	QString eStr = (elapsed.hour() > 0) ? "h:mm:ss" : "m:ss" ;
-	QString rStr = (remaining.hour() > 0) ? "h:mm:ss" : "m:ss" ;
+	updateTimeElapsed(secondsElapsed);
+	updateTimeRemaining(secondsRemaining);
+
+	// Why this?
 	if(secondsRemaining == 0)
 		timeElapsedLabel->setText("0:00");
-	else
-		timeElapsedLabel->setText(elapsed.toString(eStr));
+
+
+}
+
+void AMBottomBar::updateTimeElapsed(double seconds)
+{
+	QTime elapsed = QTime(0,0,0,0).addMSecs(int(1000*seconds));
+	QString eStr = (elapsed.hour() > 0) ? "h:mm:ss" : "m:ss" ;
+	timeElapsedLabel->setText(elapsed.toString(eStr));
+}
+
+void AMBottomBar::updateTimeRemaining(double seconds)
+{
+	QTime remaining = QTime(0,0,0,0).addMSecs(int(1000*seconds));
+	QString rStr = (remaining.hour() > 0) ? "h:mm:ss" : "m:ss" ;
 	timeRemainingLabel->setText("-"+remaining.toString(rStr));
 }
+
+void AMBottomBar::updateProgress(double fractionElapsed, double total)
+{
+	progressBar->setMaximum(1000);
+	progressBar->setValue(int(1000.0*fractionElapsed/total));
+}
+
+void AMBottomBar::setStatusText(const QString &text)
+{
+	progressBar->setTextVisible(true);
+	progressBar->setFormat(text);
+}
+
