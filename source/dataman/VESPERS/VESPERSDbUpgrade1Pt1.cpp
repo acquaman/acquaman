8284f1c84bf04ac8ea10b3873235f247a2e3f1d2
Darren Hunter <darren.hunter@usask.ca>
2012-08-13 15:21:30 -0600

    VESPERS: Finished building the database upgrade for adding spectral raw data sources to 2D scans.  No testing yet.

--- source/dataman/VESPERS/VESPERSDbUpgrade1Pt1.cpp
+++ source/dataman/VESPERS/VESPERSDbUpgrade1Pt1.cpp
@@ -20,25 +20,77 @@ bool VESPERSDbUpgrade1Pt1::upgradeImplementation()
 	QList<int> indicesToUpgrade = databaseToUpgrade_->objectsMatching("AMScan_table", "AMDbObjectType", "AM2DScan");
 	QList<int> measurements;
 	QVariant detectorChoice;
+	QString configName;
+	int configId;
 	QVariant numberOfElements;
 
-	for (int i = 0, size = indexesToUpgrade.size(); i < size; i++){
+	for (int i = 0, size = indicesToUpgrade.size(); i < size; i++){
 
 		detectorChoice = databaseToUpgrade_->retrieve(indicesToUpgrade.at(i), "AMScan_table", "scanConfiguration");
+		configName = detectorChoice.toString().split(";").first();
+		configId = detectorChoice.toString().split(";").at(1).toInt();
 
+		if (configName != "VESPERS2DScanConfiguration_table")
+			return false;
 
+		numberOfElements = databaseToUpgrade_->retrieve(configId, configName, "fluorescenceDetectorChoice");
+
+		if (numberOfElements.toInt() == 1){
+
+			measurements = databaseToUpgrade_->objectsMatching("AMScan_table_rawDataSources", "id1", indicesToUpgrade.at(i));
+			int rawDatasourceId = databaseToUpgrade_->insertOrUpdate(0, "AMRawDataSource_table",
+																	 QStringList() << "AMDbObjectType" << "thumbnailCount" << "name" << "mearurementId" << "description" << "scanRank" << "measurementRank" << "rank" << "visibleInPlots" << "hiddenFromUsers",
+																	 QVariantList() << "AMRawDataSource" << 0 << "spectra" << measurements.size() << "1-el Vortex" << 1 << 1 << 1 << "false" << "true"
+																	 );
+
+			if (!rawDatasourceId || !databaseToUpgrade_->insertOrUpdate(0, "AMScan_table_rawDataSources",
+																		   QStringList() << "id1" << "table1" << "id2" << "table2",
+																		   QVariantList() << indicesToUpgrade.at(i) << "AMScan_table" << rawDatasourceId << "AMRawDataSource_table"))
+					return false;
+		}
+
+		else if (numberOfElements.toInt() == 4){
+
+			measurements = databaseToUpgrade_->objectsMatching("AMScan_table_rawDataSources", "id1", indicesToUpgrade.at(i));
+			int rawDatasourceId = databaseToUpgrade_->insertOrUpdate(0, "AMRawDataSource_table",
+																	 QStringList() << "AMDbObjectType" << "thumbnailCount" << "name" << "mearurementId" << "description" << "scanRank" << "measurementRank" << "rank" << "visibleInPlots" << "hiddenFromUsers",
+																	 QVariantList() << "AMRawDataSource" << 0 << "corrSum" << measurements.size() << "4-el Vortex" << 1 << 1 << 2 << "false" << "true"
+																	 );
+
+			if (!rawDatasourceId || !databaseToUpgrade_->insertOrUpdate(0, "AMScan_table_rawDataSources",
+																		   QStringList() << "id1" << "table1" << "id2" << "table2",
+																		   QVariantList() << indicesToUpgrade.at(i) << "AMScan_table" << rawDatasourceId << "AMRawDataSource_table"))
+					return false;
+
+			for (int index = 1; index < 5; index++){
+
+				rawDatasourceId = databaseToUpgrade_->insertOrUpdate(0, "AMRawDataSource_table",
+																	 QStringList() << "AMDbObjectType" << "thumbnailCount" << "name" << "mearurementId" << "description" << "scanRank" << "measurementRank" << "rank" << "visibleInPlots" << "hiddenFromUsers",
+																	 QVariantList() << "AMRawDataSource" << 0 << QString("raw%1").arg(index) << measurements.size()+index << "4-el Vortex" << 1 << 1 << 2 << "false" << "true"
+																	 );
+
+				if (!rawDatasourceId || !databaseToUpgrade_->insertOrUpdate(0, "AMScan_table_rawDataSources",
+																			   QStringList() << "id1" << "table1" << "id2" << "table2",
+																			   QVariantList() << indicesToUpgrade.at(i) << "AMScan_table" << rawDatasourceId << "AMRawDataSource_table"))
+						return false;
+			}
+		}
+
+		else
+			return false;
 	}
-	return false;
+
+	return true;
 }
 
-bool VESPERSDbUpgrade1Pt1::createCopy() const
+AMDbUpgrade *VESPERSDbUpgrade1Pt1::createCopy() const
 {
 	AMDbUpgrade *copy = new VESPERSDbUpgrade1Pt1(this->databaseNameToUpgrade());
 
-	if (this->databaseNameToUpgrade())
+	if (this->databaseToUpgrade())
 		copy->loadDatabaseFromName();
 
-	retur copy;
+	return copy;
 }
 
 QString VESPERSDbUpgrade1Pt1::upgradeToTag() const
