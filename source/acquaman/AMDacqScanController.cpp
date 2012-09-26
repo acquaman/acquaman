92b14ce8b693a46dd28d084575927aa8837b6424
Darren Hunter <darren.hunter@usask.ca>
2012-09-26 10:12:03 -0600

    Fixed a small bug in the CDF data store part of ::startImplementation.  I was setting setExpectsSpectrumFromScanController to true when it should have been set to whether we were usingSpectraDotDatFile_.

--- source/acquaman/AMDacqScanController.cpp
+++ source/acquaman/AMDacqScanController.cpp
@@ -110,7 +110,6 @@ bool AMDacqScanController::startImplementation(){
 			// Synchronizing the .dat and _spectra.dat to match the cdf name.
 			else if (qobject_cast<AMCDFDataStore *>(scan_->rawData())){
 
-				qDebug() << scan_->filePath();
 				QFileInfo fullPath(scan_->filePath());	// ex: 2010/09/Mon_03_12_24_48_0000   (Relative, and with no extension)
 
 				QString path = fullPath.path();// just the path, not the file name. Still relative.
@@ -118,7 +117,7 @@ bool AMDacqScanController::startImplementation(){
 
 				abop->setProperty( "File Template", file.toStdString());
 				abop->setProperty( "File Path", (AMUserSettings::userDataFolder + "/" + path).toStdString());	// given an absolute path here
-				((AMAcqScanSpectrumOutput*)abop)->setExpectsSpectrumFromScanController(true);
+				((AMAcqScanSpectrumOutput*)abop)->setExpectsSpectrumFromScanController(usingSpectraDotDatFile_);
 			}
 
 			((AMAcqScanSpectrumOutput*)abop)->setScan(scan_);
