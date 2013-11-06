bc18ab6bb3e25e55c7648a4e385a273bf112ef42
Darren Hunter <darren.hunter@usask.ca>
2013-11-06 10:45:32 -0600

    Ported the SGMXASScanActionController to use the new generalized scan controller.

--- source/acquaman/SGM/SGMXASScanActionController.h
+++ source/acquaman/SGM/SGMXASScanActionController.h
@@ -1,80 +1,30 @@
 #ifndef SGMXASSCANACTIONCONTROLLER_H
 #define SGMXASSCANACTIONCONTROLLER_H
 
-#include "acquaman/AMScanActionController.h"
+#include "acquaman/AMRegionScanActionController.h"
 #include "acquaman/SGM/SGMXASScanConfiguration2013.h"
-#include "dataman/AMUser.h"
-#include "actions3/AMAction3.h"
 
-#include <QThread>
-
-class AMScanActionControllerScanAssembler;
-class AMListAction3;
-
-#include "acquaman/AMRegionScanActionControllerBasicFileWriter.h"
-Q_DECLARE_METATYPE(AMRegionScanActionControllerBasicFileWriter::FileWriterError)
-
-#define SGMXASSCANACTIONCONTROLLER_CANNOT_INTIALIZE 272001
-#define SGMXASSCANACTIONCONTROLLER_CANNOT_CONVERT_CONFIGURATION 272002
-#define SGMXASSCANACTIONCONTROLLER_FILE_ALREADY_EXISTS 272003
-#define SGMXASSCANACTIONCONTROLLER_COULD_NOT_OPEN_FILE 272004
-#define SGMXASSCANACTIONCONTROLLER_UNKNOWN_FILE_ERROR 272005
-
-class SGMXASScanActionController : public AMScanActionController
+/// SGM specific subclass for X-ray Absorption Spectroscopy.
+class SGMXASScanActionController : public AMRegionScanActionController
 {
-Q_OBJECT
+	Q_OBJECT
+
 public:
+	/// Constructor.  Builds a runnable scan action controller for the SGM beamline.
 	SGMXASScanActionController(SGMXASScanConfiguration2013 *cfg, QObject *parent = 0);
 
-	AMAction3* scanningActions();
-
-	virtual bool isReadyForDeletion() const;
-	virtual void buildScanController() {}
-
-signals:
-	void requestWriteToFile(int fileRank, const QString &textToWrite);
-	void finishWritingToFile();
-
-protected slots:
-	void onInitializationActionsListSucceeded();
-	void onInitializationActionsListFailed();
-	void onActionsTreeSucceeded();
-	void onActionsTreeFailed();
-	void onCleanupActionsListSucceeded();
-	void onCleanupActionsListFailed();
-
-	void onActionTreeGenerated(AMAction3 *actionTree);
-	void onFileWriterError(AMRegionScanActionControllerBasicFileWriter::FileWriterError error);
-	void onFileWriterIsBusy(bool isBusy);
-
 protected:
-	virtual bool initializeImplementation();
-	virtual bool startImplementation();
-	virtual void cancelImplementation();
-
-	bool event(QEvent *e);
-
-	void writeHeaderToFile();
-	void writeDataToFiles();
-
+	/// Reimplemented to provide actions that will setupd the beamine for optimzed operation of the XAS scan.
 	AMAction3* createInitializationActions();
+	/// Reimplemented to put the beamline in a good state after a scan has been completed.
 	AMAction3* createCleanupActions();
 
+	/// Adds anything extra.
 	virtual void buildScanControllerImplementation() {}
 
 protected:
-	AMAction3 *scanningActions_;
-	bool scanningActionsSucceeded_;
-	AMListAction3 *initializationActions_;
-	AMListAction3 *cleanupActions_;
-	AMScanActionControllerScanAssembler *newScanAssembler_;
+	/// Specific scan configuration with all the SGM specific information inside.
 	SGMXASScanConfiguration2013 *configuration_;
-
-	AMnDIndex currentAxisValueIndex_;
-	double currentAxisValue_;
-
-	QThread *fileWriterThread_;
-	bool fileWriterIsBusy_;
 };
 
 #endif // SGMXASSCANACTIONCONTROLLER_H
--- source/acquaman/VESPERS/VESPERSXASScanActionController.h
+++ source/acquaman/VESPERS/VESPERSXASScanActionController.h
@@ -4,21 +4,26 @@
 #include "acquaman/AMRegionScanActionController.h"
 #include "acquaman/VESPERS/VESPERSEXAFSScanConfiguration.h"
 
+/// VESPERS specific subclass for X-ray Absorption Spectroscopy.
 class VESPERSXASScanActionController : public AMRegionScanActionController
 {
 	Q_OBJECT
 
 public:
+	/// Constructor.  Builds a runnable scan action controller for the VESPERS beamline.
 	VESPERSXASScanActionController(VESPERSEXAFSScanConfiguration *configuration, QObject *parent = 0);
 
 protected:
-
+	/// Reimplemented to provide actions that will setup the beamline for optimized operation of the XAS scan.
 	AMAction3* createInitializationActions();
+	/// Reimplemented to put the beamline in a good state after a scan has been completed.
 	AMAction3* createCleanupActions();
 
+	/// Adds anything extra (eg: analysis blocks) to the scan before it's started.
 	virtual void buildScanControllerImplementation() {}
 
 protected:
+	/// Specific scan configuration with all the VESPERS specific information inside.
 	VESPERSEXAFSScanConfiguration *configuration_;
 };
 
