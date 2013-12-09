19750748704e51ed176ffced4f2406fce3a1866c
Ethan Ludba <ethan.ludba@lightsource.ca>
2013-12-09 09:39:20 -0600

    Added destructor finding for non-virtual destructors.  Save point before actually altering files.

--- source/virtualDestructors.bash
+++ source/virtualDestructors.bash
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+arg=$1
+
+if [[ $arg == "fix" ]]; then
+	fix=1
+else
+	fix=0
+fi
+
 input=T1.txt
 if [ -e $input ]; then
-	echo T1.txt already exits
-	input=temp_file_T1_afjslksekjlskdf.txt #random name. shouldnt match anything ever. except itself
+	echo T1.txt already exists
+	input=temp_file_T1_01f23c0.txt #random name. shouldnt match anything ever. except itself
 fi
 	
 
@@ -19,15 +27,24 @@ do
 		echo $file $line
 		echo $className
 		echo "No Destructor"
-		#grep to the constructor, add the virtual destructor on the next line
-		#as well, add the destructor to the .cpp file
+		if [[ $fix == 1 ]]; then
+			constructorLine=`grep -nE "$className\(" $file | cut -d: -f-1 | tail -1`
+			echo "Constructor on line $constructorLine"
+			#grep to the constructor, add the virtual destructor on the next line
+			#as well, add the destructor to the .cpp file
+		fi
 	else
 		if ! `echo ${destructor} | grep "virtual" 1>/dev/null 2>&1`; then
+			
 			echo $file $line
 			echo $className
 			echo "Destructor needs to be virtual" 
-			#grep to the destructor, add virtual in front of it
-			
+			destructorLine=`grep -nE "~$className" $file | cut -d: -f-1`
+			echo "Destructor on line: $destructorLine"
+			if [[ $fix == 1 ]]; then
+				
+			fi	
+				
 			if `echo ${destructor} | grep '{.*}' 1>/dev/null 2>&1`; then
 				echo "Destructor declared in line"
 			elif `echo ${destructor} | grep ';' 1>/dev/null 2>&1`; then
@@ -40,3 +57,9 @@ do
 done < "$input"
 
 rm -f  $input
+
+if [[ $fix == 1 ]]; then
+	echo "in fix mode"
+else
+	echo "in report mode"
+fi
