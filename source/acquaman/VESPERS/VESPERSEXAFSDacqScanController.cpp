35d02ac79d2c56be62578c89d6df1ac16aeacb36
Darren Hunter <darren.hunter@usask.ca>
2014-02-07 14:00:51 -0600

    VESPERS: Fixed a bug that was fixed months ago... bad merge a long time ago I guess?

--- source/acquaman/VESPERS/VESPERSEXAFSDacqScanController.cpp
+++ source/acquaman/VESPERS/VESPERSEXAFSDacqScanController.cpp
@@ -310,7 +310,7 @@ VESPERSEXAFSDacqScanController::VESPERSEXAFSDacqScanController(VESPERSEXAFSScanC
 
 				normPFY = new AM1DExpressionAB("norm_"+scan_->rawDataSources()->at(i)->name());
 				normPFY->setDescription("Normalized "+scan_->rawDataSources()->at(i)->description());
-				normPFY->setInputDataSources(QList<AMDataSource *>() << scan_->rawDataSources()->at(0) << scan_->rawDataSources()->at(0));
+				normPFY->setInputDataSources(QList<AMDataSource *>() << scan_->rawDataSources()->at(0) << scan_->rawDataSources()->at(i));
 				normPFY->setExpression(QString("%1/%2").arg(scan_->rawDataSources()->at(i)->name()).arg(scan_->rawDataSources()->at(0)->name()));
 				scan_->addAnalyzedDataSource(normPFY, true, false);
 			}
