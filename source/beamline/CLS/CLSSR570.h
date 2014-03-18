027b5348eb36b89d793716525ce2c8e38b20d6b9
David Chevrier <david.k.chevrier@gmail.com>
2014-03-18 13:39:08 -0600

    Added new class in CLSSR570 to replace AMProcessVariable with AMControl. As of yet untested, but should be able to direclty replace the current class as it offers the same public API.

--- source/beamline/CLS/CLSSR570.h
+++ source/beamline/CLS/CLSSR570.h
@@ -123,4 +123,76 @@ private:
 	int indexToValue(int index) const;
 };
 
+class AMControl;
+class AMControlSet;
+
+class CLSSR570New : public QObject
+{
+Q_OBJECT
+public:
+	/// Constructor.  Builds a SR570 model based on \param valueName and \param untisName.
+	CLSSR570New(const QString &valueName, const QString &unitsName, QObject *parent = 0);
+
+	/// Returns the value of the sensitivity (ie: 1, 2, 5, 10, 20, 50, 100, 200, 500).
+	int value() const;
+	/// Returns the index of the sensitivity (value between 0 and 8).
+	int valueIndex() const;
+	/// Returns the units of the sensitivity.
+	QString units() const;
+
+	/// Returns whether the SR570 is connected.
+	bool isConnected() const;
+
+	/// Returns whether the ion chamber is at maximum senstivity.
+	virtual bool atMaximumSensitivity() const;
+	/// Returns whether the ion chamber is at minimum sensitivity.
+	virtual bool atMinimumSensitivity() const;
+
+signals:
+	/// General notifier.  Emitted if either the value or the units have changed.
+	void sensitivityChanged();
+	/// Notifier that the sensitivity value has changed.  Passes the new value.
+	void valueChanged(int);
+	/// Notifier that the sensitivity value index has changed.  Passes the new index.
+	void valueIndexChanged(int);
+	/// Notifier that the sensitivity units have changed.  Passes the new value.
+	void unitsChanged(QString);
+	/// Notifier that the ion chamber is at the minimum sensitivity.  Passes the truth value.
+	void minimumSensitivity(bool);
+	/// Notifier that the ion chamber is at the maximums sensitivity.  Passes the truth value.
+	void maximumSensitivity(bool);
+	/// Notifier that the connectivity of the SR570 has changed.  Passes the value.
+	void connected(bool);
+
+public slots:
+	/// Sets the sensitivity value.  Must be 1, 2, 5, 10, 20, 50, 100, 200, or 500.  Does nothing otherwise.
+	void setValue(int value);
+	/// Sets the value index.  Must be between 0 and 8.
+	void setValueIndex(int index);
+	/// Sets the sensitivity units.  Must be pA/V, nA/V, uA/V, or mA/V.  Does nothing otherwise.
+	void setUnits(QString units);
+
+	/// Increases the sensitivity of the ion chamber by one step.
+	bool increaseSensitivity();
+	/// Decreases the sensitivity of the ion chamber by one step.
+	bool decreaseSensitivity();
+
+protected slots:
+	void onValueControlValueChanged(double newValue);
+	void onUnitsControlValueChanged(double newUnits);
+
+protected:
+	void checkSensitivityHelper();
+
+protected:
+	AMControl *valueControl_;
+	AMControl *unitsControl_;
+	AMControlSet *allControls_;
+
+	/// Holds the state of whether the ion chamber is at its maximum sensitivity.
+	bool atMaximumSensitivity_;
+	/// Holds the state of whether the ion chamber is at its minimum sensitivity.
+	bool atMinimumSensitivity_;
+};
+
 #endif // CLSSR570_H
