7c71d92bbda6c091fefcf168cce456c3261b6dab
Iain Workman <iain.workman@googlemail.com>
2014-05-09 12:01:48 -0600

    Added: -A class AMScanListModel to use as a model for a group of scans (currently loads no scans, and cannot access the configs) -Added a QTableView to AMScanSearchView and tied the AMScanListModel to it

--- source/ui/dataman/AMScanSearchView.cpp
+++ source/ui/dataman/AMScanSearchView.cpp
@@ -1,5 +1,8 @@
 #include "AMScanSearchView.h"
 
+// Definitions for AMScanSearchView
+///////////////////////////////////
+
 AMScanSearchView::AMScanSearchView(QWidget *parent) :
 	QWidget(parent)
 {
@@ -9,4 +12,110 @@ AMScanSearchView::AMScanSearchView(QWidget *parent) :
 void AMScanSearchView::initDialog()
 {
 	setWindowTitle(QString("Search Scans"));
+	QHBoxLayout* layout = new QHBoxLayout();
+
+	QTableView* searchResults = new QTableView();
+	searchResults->setModel(new AMScanListModel());
+	layout->addWidget(searchResults);
+
+	this->setLayout(layout);
+}
+
+// Definitions for AMScanListModel
+///////////////////////////////////
+AMScanListModel::AMScanListModel(QObject *parent) :
+	QAbstractTableModel(parent)
+{
+	this->scans_ = new QList<AMScan*>();
+}
+
+
+QVariant AMScanListModel::data(const QModelIndex &index, int role) const
+{
+	// Invalid index:
+	if(!index.isValid())
+		return QVariant();
+
+	// If handling the alignment.
+	if (role == Qt::TextAlignmentRole)
+		return Qt::AlignCenter;
+
+	// If handling the background color.
+	//if (role == Qt::BackgroundRole)
+		//return scans_->at(index.row())->isValid() ? Qt::white : Qt::red;
+
+	// We only answer to Qt::DisplayRole right now
+	if(role != Qt::DisplayRole)
+		return QVariant();
+
+	// Out of range: (Just checking for too big.  isValid() checked for < 0)
+	if(index.row() >= scans_->count())
+		return QVariant();
+
+	QVariant dataVal = QVariant();
+	AMScan *scan = scans_->at(index.row());
+
+	switch(index.column()){
+
+	case 0: // The scan id.
+		dataVal = QString::number(scan->id());
+		break;
+	case 1: // The scan name.
+		dataVal = scan->name();
+		break;
+	case 2: // The scan number.
+		dataVal = QString::number(scan->number());
+		break;
+	case 3:  // Date the scan was performed.
+		dataVal = scan->dateTime().toString(QString("ddd MMMM d yy"));
+		break;
+	case 4: // The name of the sample.
+		dataVal = scan->sample()->name();
+		break;
+	case 5: // The sample time.
+		dataVal = scan->sample()->dateTime().toString(QString("ddd MMMM d yy"));
+		break;
+	default:
+		break; // Return null if not a specific case.
+	}
+
+	return dataVal;
+}
+
+QVariant AMScanListModel::headerData(int section, Qt::Orientation orientation, int role) const
+{
+	if (role != Qt::DisplayRole)
+		return QVariant();
+
+	// Vertical headers:
+	if(orientation == Qt::Vertical) {
+		return section;
+	}
+
+	// Horizontal Headers: (Column labels)
+	QVariant header = QVariant();
+
+	switch(section){
+
+	case 0:
+		header = "id";
+		break;
+	case 1:
+		header = "Name";
+		break;
+	case 2:
+		header = "#";
+		break;
+	case 3:
+		header = "When";
+		break;
+	case 4:
+		header = "Sample Name";
+		break;
+	case 5:
+		header = "Sample Time";
+		break;
+	}
+
+	return header;
 }
--- source/ui/dataman/AMScanSearchView.h
+++ source/ui/dataman/AMScanSearchView.h
@@ -2,6 +2,12 @@
 #define AMSCANSEARCHVIEW_H
 
 #include <QWidget>
+#include <QTableView>
+#include <QAbstractTableModel>
+#include <dataman/AMScan.h>
+#include <dataman/AMSample.h>
+#include <QList>
+#include <QHBoxLayout>
 
 /// A class representing a Dialog Window for searching scan's for:
 ///		Sample name
@@ -19,10 +25,35 @@ public:
 protected:
 	/// Initialises all the needed settings for the window (constructs Widgets, sets title etc.)
 	void initDialog();
+	/// Gets a list of all the required scans
+	void refreshScanList();
 signals:
 	
 public slots:
 	
+};
+
+/// A class representing a scan list model
+class AMScanListModel : public QAbstractTableModel{
+	Q_OBJECT
+public:
+	explicit AMScanListModel(QObject* parent = 0);
+	/// Returns the number of scans in the list to generate the number of rows in a table or list
+	int rowCount(const QModelIndex & /*parent*/) const { return scans_->count(); }
+	/// Returns "6" statically. There are always 6 fields in the ScanModel: id, name, number, dataTime, sampleName, sampleTime
+	int columnCount(const QModelIndex & /*parent*/) const { return 6; }
+	/// Retrieves the data from an index (row and column) and returns as a QVariant. Only valid role is Qt::DisplayRole right now.
+	QVariant data(const QModelIndex &index, int role) const;
+	/// Retrieves the header data for a column or row and returns as a QVariant. Only valid role is Qt::DisplayRole right now.
+	QVariant headerData(int section, Qt::Orientation orientation, int role) const;
+
+	QList<AMScan*>* scans() { return scans_; }
+protected:
+	QList<AMScan*>* scans_;
+
+signals:
+
+public slots:
 };
 
 #endif // AMSCANSEARCHVIEW_H
