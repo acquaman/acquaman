c603da3c2cc70c1141ff340676b717cd8c1b3dc9
Iain Workman <iain.workman@googlemail.com>
2014-05-09 16:10:53 -0600

    Fixed: Added call to init() in AMSample(int databaseID, AMDatabase* database,...) constructor to fix seg fault when called Added: -new class AMScanSearchInfo to represent a simple fast way of representing scans in a table view which automatically populates with all scans currently in the database -new class AMScanSearchInfoListModel to use this class in views -changed AMScanSearchView's table to use AMScanSearchInfoListModel

--- source/ui/dataman/AMScanSearchView.cpp
+++ source/ui/dataman/AMScanSearchView.cpp
@@ -11,26 +11,142 @@ AMScanSearchView::AMScanSearchView(QWidget *parent) :
 
 void AMScanSearchView::initDialog()
 {
+	// Set Window properties
 	setWindowTitle(QString("Search Scans"));
+	resize(750, 800);
+	// Set layout and widgets
 	QHBoxLayout* layout = new QHBoxLayout();
 
 	QTableView* searchResults = new QTableView();
-	searchResults->setModel(new AMScanListModel());
+	searchResults->setModel(new AMScanSearchInfoListModel());
 	layout->addWidget(searchResults);
 
 	this->setLayout(layout);
 }
 
-// Definitions for AMScanListModel
-///////////////////////////////////
-AMScanListModel::AMScanListModel(QObject *parent) :
-	QAbstractTableModel(parent)
+void AMScanSearchView::refreshScanList()
 {
-	this->scans_ = new QList<AMScan*>();
+
+
+}
+
+// Definitions for AMScanSearchInfo
+///////////////////////////////////
+AMScanSearchInfo::AMScanSearchInfo(int scanID, QObject *parent)
+	:QObject(parent)
+{
+	AMDatabase* database = AMDatabase::database("user");
+
+	configuration_ = 0;
+	sample_ = 0;
+
+	if(database == 0)
+		return;
+
+	QStringList columns;
+	columns.append(QString("id"));
+	columns.append(QString("name"));
+	columns.append(QString("number"));
+	columns.append(QString("dateTime"));
+	columns.append(QString("scanConfiguration"));
+	columns.append(QString("sample"));
+
+	QVariantList results = database->retrieve(scanID, "AMScan_table", columns);
+
+	if(results.count()!=0)
+	{
+		scanID_ = results.at(0).toInt();
+		scanName_ = results.at(1).toString();
+		scanNumber_ = results.at(2).toInt();
+		scanDateTime_ = results.at(3).toDateTime();
+		QStringList configResult = results.at(4).toString().split(";");
+		if(configResult.count() > 1 && configResult.at(0).compare(QString("SGMXASScanConfiguration_table"))==0)
+		{
+			initializeConfig(configResult.at(1).toInt());
+		}
+		QStringList sampleResult = results.at(5).toString().split(";");
+		if(sampleResult.count() > 1)
+		{
+			initializeSample(sampleResult.at(1).toInt());
+		}
+	}
+
+
+}
+
+int AMScanSearchInfo::scanID()
+{
+	return scanID_;
 }
 
 
-QVariant AMScanListModel::data(const QModelIndex &index, int role) const
+int AMScanSearchInfo::scanNumber()
+{
+	return scanNumber_;
+}
+
+QDateTime AMScanSearchInfo::scanDateTime()
+{
+	return scanDateTime_;
+}
+
+QString AMScanSearchInfo::scanName()
+{
+	return scanName_;
+}
+
+SGMXASScanConfiguration* AMScanSearchInfo::configuration()
+{
+	return configuration_;
+}
+
+AMSample *AMScanSearchInfo::sample()
+{
+	return sample_;
+}
+
+void AMScanSearchInfo::initializeConfig(int configID)
+{
+
+	AMDatabase* database = AMDatabase::database("user");
+	if(database == 0)
+		return;
+
+	configuration_ = new SGMXASScanConfiguration();
+	configuration_->loadFromDb(database, configID);
+}
+
+void AMScanSearchInfo::initializeSample(int sampleID)
+{
+
+	AMDatabase* database = AMDatabase::database("user");
+	if(database == 0)
+		return;
+
+	sample_ = new AMSample(sampleID, database);
+
+}
+
+
+// Definitions for AMScanListModel
+///////////////////////////////////
+AMScanSearchInfoListModel::AMScanSearchInfoListModel(QObject *parent) :
+	QAbstractTableModel(parent)
+{
+	this->scanInfos_ = new QList<AMScanSearchInfo*>();
+	// Load Scans
+	AMDatabase* database = AMDatabase::database("user");
+	if(database == 0)
+		return;
+	QVariantList scanIds = database->retrieve("AMScan_table", "id");
+	for(int iScanID = 0; iScanID < scanIds.count(); iScanID++)
+	{
+		scanInfos_->append(new AMScanSearchInfo(iScanID));
+	}
+}
+
+
+QVariant AMScanSearchInfoListModel::data(const QModelIndex &index, int role) const
 {
 	// Invalid index:
 	if(!index.isValid())
@@ -49,31 +165,45 @@ QVariant AMScanListModel::data(const QModelIndex &index, int role) const
 		return QVariant();
 
 	// Out of range: (Just checking for too big.  isValid() checked for < 0)
-	if(index.row() >= scans_->count())
+	if(index.row() >= this->scanInfos_->count())
 		return QVariant();
 
 	QVariant dataVal = QVariant();
-	AMScan *scan = scans_->at(index.row());
+	AMScanSearchInfo *scanInfo = scanInfos_->at(index.row());
 
 	switch(index.column()){
 
 	case 0: // The scan id.
-		dataVal = QString::number(scan->id());
+		dataVal = QString::number(scanInfo->scanID());
 		break;
 	case 1: // The scan name.
-		dataVal = scan->name();
+		dataVal = scanInfo->scanName();
 		break;
 	case 2: // The scan number.
-		dataVal = QString::number(scan->number());
+		dataVal = QString::number(scanInfo->scanNumber());
 		break;
 	case 3:  // Date the scan was performed.
-		dataVal = scan->dateTime().toString(QString("ddd MMMM d yy"));
+		dataVal = scanInfo->scanDateTime().toString(QString("MM/dd/yyyy hh:mm:ss"));
 		break;
 	case 4: // The name of the sample.
-		dataVal = scan->sample()->name();
+		if(scanInfo->sample() != 0)
+			dataVal = scanInfo->sample()->name();
 		break;
 	case 5: // The sample time.
-		dataVal = scan->sample()->dateTime().toString(QString("ddd MMMM d yy"));
+		if(scanInfo->sample() != 0)
+			dataVal = scanInfo->sample()->dateTime().toString(QString("MM/dd/yyyy hh:mm:ss"));
+		break;
+	case 6: // The configuration exit slit
+		if(scanInfo->configuration() != 0)
+			dataVal = scanInfo->configuration()->exitSlitGap();
+		break;
+	case 7: // The configuration grating
+		if(scanInfo->configuration() != 0)
+			dataVal = scanInfo->configuration()->grating();
+		break;
+	case 8: // The configuration harmonic
+		if(scanInfo->configuration() != 0)
+			dataVal = scanInfo->configuration()->harmonic();
 		break;
 	default:
 		break; // Return null if not a specific case.
@@ -82,7 +212,7 @@ QVariant AMScanListModel::data(const QModelIndex &index, int role) const
 	return dataVal;
 }
 
-QVariant AMScanListModel::headerData(int section, Qt::Orientation orientation, int role) const
+QVariant AMScanSearchInfoListModel::headerData(int section, Qt::Orientation orientation, int role) const
 {
 	if (role != Qt::DisplayRole)
 		return QVariant();
@@ -115,7 +245,18 @@ QVariant AMScanListModel::headerData(int section, Qt::Orientation orientation, i
 	case 5:
 		header = "Sample Time";
 		break;
+	case 6:
+		header = "Exit Slit";
+		break;
+	case 7:
+		header = "Grating";
+		break;
+	case 8:
+		header = "Harmonic";
 	}
 
 	return header;
 }
+
+
+
--- source/ui/dataman/AMScanSearchView.h
+++ source/ui/dataman/AMScanSearchView.h
@@ -4,10 +4,12 @@
 #include <QWidget>
 #include <QTableView>
 #include <QAbstractTableModel>
-#include <dataman/AMScan.h>
+#include <acquaman/SGM/SGMXASScanConfiguration.h>
 #include <dataman/AMSample.h>
+#include <dataman/database/AMDatabase.h>
 #include <QList>
 #include <QHBoxLayout>
+#include <QDebug>
 
 /// A class representing a Dialog Window for searching scan's for:
 ///		Sample name
@@ -33,23 +35,52 @@ public slots:
 	
 };
 
-/// A class representing a scan list model
-class AMScanListModel : public QAbstractTableModel{
+/// A lightweight class representing scan information loaded from the db to display in the search results
+class AMScanSearchInfo : public QObject{
 	Q_OBJECT
 public:
-	explicit AMScanListModel(QObject* parent = 0);
+	explicit AMScanSearchInfo(int scanID, QObject* parent = 0);
+	int scanID();
+	int scanNumber();
+	QDateTime scanDateTime();
+	QString scanName();
+	SGMXASScanConfiguration* configuration();
+	AMSample* sample();
+
+protected:
+	int scanID_;
+	int scanNumber_;
+	QDateTime scanDateTime_;
+	QString scanName_;
+	SGMXASScanConfiguration* configuration_;
+	AMSample* sample_;
+	void initializeConfig(int configID);
+	void initializeSample(int sampleID);
+
+signals:
+public slots:
+};
+
+/// A class representing a scan list model
+class AMScanSearchInfoListModel : public QAbstractTableModel{
+	Q_OBJECT
+public:
+	explicit AMScanSearchInfoListModel(QObject* parent = 0);
 	/// Returns the number of scans in the list to generate the number of rows in a table or list
-	int rowCount(const QModelIndex & /*parent*/) const { return scans_->count(); }
-	/// Returns "6" statically. There are always 6 fields in the ScanModel: id, name, number, dataTime, sampleName, sampleTime
-	int columnCount(const QModelIndex & /*parent*/) const { return 6; }
+	int rowCount(const QModelIndex & /*parent*/) const { return scanInfos_->count(); }
+	/// Returns "9" statically. There are always 9 fields in the ScanModel: id, name, number, dataTime, sampleName, sampleTime, configExitSlit, configGrating and configHarmonic
+	int columnCount(const QModelIndex & /*parent*/) const { return 9; }
 	/// Retrieves the data from an index (row and column) and returns as a QVariant. Only valid role is Qt::DisplayRole right now.
 	QVariant data(const QModelIndex &index, int role) const;
 	/// Retrieves the header data for a column or row and returns as a QVariant. Only valid role is Qt::DisplayRole right now.
 	QVariant headerData(int section, Qt::Orientation orientation, int role) const;
 
-	QList<AMScan*>* scans() { return scans_; }
+	/// A list of the scans which the model represents
+	QList<AMScanSearchInfo*>* scanInfos() { return scanInfos_; }
+
+
 protected:
-	QList<AMScan*>* scans_;
+	QList<AMScanSearchInfo*>* scanInfos_;
 
 signals:
 
