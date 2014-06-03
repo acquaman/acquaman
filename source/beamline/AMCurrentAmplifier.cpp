dfd8ed80be6b1645e35c26016d167feb3ce772e6
helfrij <jessie.helfrich@gmail.com>
2014-06-03 14:41:20 -0600

    Made modifications to the AMCurrentAmplifier superclass. Removed pure virtual requirement for sensitivity-related methods (increase/decreaseSensitivity(), atMinimum/MaximumSensitivity(), etc). Added supportsSensitivity/GainMode() functions to declare what modes are available, making both false in AMCurrentAmplifier. Added gain-related methods (increase/decreaseGain(), etc) that depend on whether gain is supported (same with sensitivity).

--- source/beamline/AMCurrentAmplifier.cpp
+++ source/beamline/AMCurrentAmplifier.cpp
@@ -5,8 +5,8 @@ AMCurrentAmplifier::AMCurrentAmplifier(const QString &name, QObject *parent) :
 {
     name_ = name;
     connected_ = false;
-
-    setAmplifierMode(Sensitivity);
+    supportsGainMode_ = false;
+    supportsSensitivityMode_ = false;
 }
 
 AMCurrentAmplifier::~AMCurrentAmplifier()
@@ -17,6 +17,48 @@ AMCurrentAmplifier::~AMCurrentAmplifier()
 AMCurrentAmplifier::AmplifierMode AMCurrentAmplifier::amplifierMode() const
 {
     return amplifierMode_;
+}
+
+bool AMCurrentAmplifier::supportsGainMode() const
+{
+    return supportsGainMode_;
+}
+
+bool AMCurrentAmplifier::atMinimumGain() const
+{
+    if (!supportsGainMode())
+        return false;
+
+    return atMinimumGainImplementation();
+}
+
+bool AMCurrentAmplifier::atMaximumGain() const
+{
+    if (!supportsGainMode())
+        return false;
+
+    return atMaximumGainImplementation();
+}
+
+bool AMCurrentAmplifier::supportsSensitivityMode() const
+{
+    return supportsSensitivityMode_;
+}
+
+bool AMCurrentAmplifier::atMinimumSensitivity() const
+{
+    if (!supportsSensitivityMode())
+        return false;
+
+    return atMinimumSensitivityImplementation();
+}
+
+bool AMCurrentAmplifier::atMaximumSensitivity() const
+{
+    if (!supportsSensitivityMode())
+        return false;
+
+    return atMaximumSensitivityImplementation();
 }
 
 void AMCurrentAmplifier::setAmplifierMode(AmplifierMode newMode)
@@ -31,4 +73,41 @@ void AMCurrentAmplifier::setName(const QString &newName)
 {
     name_ = newName;
     emit nameChanged(name_);
+}
+
+void AMCurrentAmplifier::setValue(int valueIndex)
+{
+    setValueImplementation(valueIndex);
+}
+
+bool AMCurrentAmplifier::increaseGain() const
+{
+    if (!supportsGainMode())
+        return false;
+
+    return increaseGainImplementation();
+}
+
+bool AMCurrentAmplifier::decreaseGain() const
+{
+    if (!supportsGainMode())
+        return false;
+
+    return decreaseGainImplementation();
+}
+
+bool AMCurrentAmplifier::increaseSensitivity() const
+{
+    if (!supportsSensitivityMode())
+        return false;
+
+    return increaseSensitivityImplementation();
+}
+
+bool AMCurrentAmplifier::decreaseSensitivity() const
+{
+    if (!supportsSensitivityMode())
+        return false;
+
+    return decreaseSensitivityImplementation();
 }
--- source/beamline/AMCurrentAmplifier.h
+++ source/beamline/AMCurrentAmplifier.h
@@ -20,46 +20,87 @@ public:
 
     /// Returns the current amplifier mode.
     AmplifierMode amplifierMode() const;
-
-    /// Returns whether the current amplifier is at maximum sensitivity.
-    virtual bool atMaximumSensitivity() const = 0;
-    /// Returns whether the current amplifier is at minimum sensitivity.
-    virtual bool atMinimumSensitivity() const = 0;
-
     /// Returns the name of the current amplifier.
     QString name() const {return name_;}
-
     /// Returns whether the current amplifier is connected.
     bool isConnected() const {return connected_;}
+
+    /// Returns the value.
+    virtual double value() const = 0;
+    /// Returns the value at the given index.
+    virtual double valueAt(int index) const = 0;
+
+    /// Returns true if the current amplifier supports Gain mode.
+    bool supportsGainMode() const;
+    /// Returns true if the current amplifier is at the minimum gain.
+    bool atMinimumGain() const;
+    /// Returns true if the current amplifier is at the maximum gain.
+    bool atMaximumGain() const;
+
+    /// Returns true if the current amplifier supports Sensitivity mode.
+    bool supportsSensitivityMode() const;
+    /// Returns whether the current amplifier is at minimum sensitivity.
+    bool atMinimumSensitivity() const;
+    /// Returns whether the current amplifier is at maximum sensitivity.
+    bool atMaximumSensitivity() const;
 
 signals:
     /// Emitted when the amplifier mode has been changed.
     void amplifierModeChanged(AmplifierMode newMode);
-    /// General notifier. Emitted when the sensitivity of the current amplifier is changed.
-    void sensitivityChanged();
-    /// Notifier that the current amplifier is at the minimum sensitivity/gain.
-    void minimumSensitivity(bool);
-    /// Notifier that the current amplifier is at the maximum sensitivity/gain.
-    void maximumSensitivity(bool);
     /// Notifier that the name of the current amplifier has changed.
     void nameChanged(const QString &newName);
     /// Notifier that the connection state of the current amplifier has changed.
     void isConnected(bool);
+
+    /// Emitted when the gain of the current amplifier is changed.
+    void gainChanged();
+    /// Emitted when the current amplifier has reached the minimum gain.
+    void minimumGain(bool);
+    /// Emitted when the current amplifier has reached the maximum gain.
+    void maximumGain(bool);
+
+    /// General notifier. Emitted when the sensitivity of the current amplifier is changed.
+    void sensitivityChanged();
+    /// Notifier that the current amplifier is at the minimum sensitivity.
+    void minimumSensitivity(bool);
+    /// Notifier that the current amplifier is at the maximum sensitivity.
+    void maximumSensitivity(bool);
 
 public slots:
     /// Sets the current amplifier mode.
     void setAmplifierMode(AmplifierMode newMode);
     /// Sets a new name for the current amplifier.
     void setName(const QString &newName);
+    /// Sets the amplifier's value.
+    void setValue(int valueIndex);
+
+    /// Increases the gain of the current amplifier.
+    bool increaseGain() const;
+    /// Decreases the gain of the current amplifier.
+    bool decreaseGain() const;
+
     /// Increases the sensitivity of the current amplifier.
-    virtual bool increaseSensitivity() = 0;
+    bool increaseSensitivity() const;
     /// Decreases the sensitivity of the current amplifier.
-    virtual bool decreaseSensitivity() = 0;
+    bool decreaseSensitivity() const;
+
+protected:
+    virtual void setValueImplementation(int valueIndex);
+    virtual bool atMinimumGainImplementation() const;
+    virtual bool atMaximumGainImplementation() const;
+    virtual bool atMinimumSensitivityImplementation() const;
+    virtual bool atMaximumSensitivityImplementation() const;
+    virtual bool increaseGainImplementation() const;
+    virtual bool decreaseGainImplementation() const;
+    virtual bool increaseSensitivityImplementation() const;
+    virtual bool decreaseSensitivityImplementation() const;
 
 protected:
     QString name_;
     bool connected_;
     AmplifierMode amplifierMode_;
+    bool supportsGainMode_;
+    bool supportsSensitivityMode_;
 
 };
 
