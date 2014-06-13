0d621dbb352e79a66c98801e4c4901e16c5cda4c
helfrij <jessie.helfrich@gmail.com>
2014-06-13 09:20:16 -0600

    Changed CLSKeithley428::setValueImplementation(const QString&) to make use of new arguments from AMCurrentAmplifierView to set the amplifier value.

--- source/beamline/CLS/CLSKeithley428.cpp
+++ source/beamline/CLS/CLSKeithley428.cpp
@@ -233,157 +233,160 @@ double CLSKeithley428::toSensitivity(double gain) const
 
 void CLSKeithley428::setValueImplementation(const QString &valueArg)
 {
-    int newIndex = valueArg.split(" ").at(0).toInt();
-    setValueIndex(newIndex);
-}
+    double newValue = valueArg.split(" ").at(0).toDouble();
 
-CLSKeithley428ValueMap::CLSKeithley428ValueMap(QObject *parent) : QObject(parent)
-{
-    tolerance_ = 0.01;
-    map_ = new QMultiMap<int, double>();
-}
-
-CLSKeithley428ValueMap::~CLSKeithley428ValueMap()
-{
-
-}
-
-QMultiMap<int, double>* CLSKeithley428ValueMap::map() const
-{
-    return map_;
-}
-
-double CLSKeithley428ValueMap::valueAt(int index, AMCurrentAmplifier::AmplifierMode mode)
-{
-    return map_->values(index).at(mode);
-}
-
-QStringList* CLSKeithley428ValueMap::valueStringList(AMCurrentAmplifier::AmplifierMode mode) const
-{
-    double toAdd = -1;
-    QStringList *valueList = new QStringList();
-
-    foreach (int key, map_->uniqueKeys()) {
-        toAdd = map_->values(key).at(mode);
-        valueList->append(QString("%1").arg(toAdd, 0, 'e', 2));
-    }
-
-    return valueList;
-}
-
-bool CLSKeithley428ValueMap::isIndexOfMin(AMCurrentAmplifier::AmplifierMode mode, int index)
-{
-    return (findIndexOfMin(mode) == index);
-}
-
-bool CLSKeithley428ValueMap::isIndexOfMax(AMCurrentAmplifier::AmplifierMode mode, int index)
-{
-    return (findIndexOfMax(mode) == index);
-}
-
-int CLSKeithley428ValueMap::findIndexOfMin(AMCurrentAmplifier::AmplifierMode mode)
-{
-    double firstIndex = map_->uniqueKeys().first();
-    double firstValue = map_->values(firstIndex).at(mode);
-    double temp = firstValue;
-
-    int minIndex = firstIndex;
-    double minValue = firstValue;
-
-    foreach (int key, map_->uniqueKeys()) {
-        temp = map_->values(key).at(mode);
-
-        if (temp < minValue) {
-            minIndex = key;
-            minValue = temp;
-        }
-    }
-
-    return minIndex;
-}
-
-int CLSKeithley428ValueMap::findIndexOfMax(AMCurrentAmplifier::AmplifierMode mode)
-{
-    double firstIndex = map_->uniqueKeys().first();
-    double firstValue = map_->values(firstIndex).at(mode);
-    double temp = firstValue;
-
-    int maxIndex = firstIndex;
-    double maxValue = firstValue;
-
-    foreach (int key, map_->uniqueKeys()) {
-        temp = map_->values(key).at(mode);
-
-        if (temp > maxValue) {
-            maxIndex = key;
-            maxValue = temp;
-        }
-    }
-
-    return maxIndex;
-}
-
-int CLSKeithley428ValueMap::nextIndex(CLSKeithley428::IndexChange change, int currentIndex)
-{
-    int nextIndex = -1;
-
-    if (!map_->contains(currentIndex)) {
-
-    } else if (change == CLSKeithley428::IncreaseOne) {
-        nextIndex = currentIndex + 1;
-
-    } else if (change == CLSKeithley428::DecreaseOne) {
-        nextIndex = currentIndex - 1;
-    }
-
-    return nextIndex;
-}
-
-void CLSKeithley428ValueMap::setValues(AMCurrentAmplifier::AmplifierMode mode, QList<double>* toAdd)
-{
-    map_->clear();
-
-    for (int i = 0; i < toAdd->size(); i++) {
-        addValue(i, mode, toAdd->at(i));
+    if (gains_.contains(newValue)) {
+        setValueIndex(gains_.indexOf(newValue));
     }
 }
 
-void CLSKeithley428ValueMap::addValue(int index, AMCurrentAmplifier::AmplifierMode mode, double value)
-{
-    if (!map_->contains(index)) {
-        double gain, sensitivity;
+//CLSKeithley428ValueMap::CLSKeithley428ValueMap(QObject *parent) : QObject(parent)
+//{
+//    tolerance_ = 0.01;
+//    map_ = new QMultiMap<int, double>();
+//}
 
-        if (mode == AMCurrentAmplifier::Gain) {
-            gain = value;
-            sensitivity = toSensitivity(value);
+//CLSKeithley428ValueMap::~CLSKeithley428ValueMap()
+//{
 
-        } else if (mode == AMCurrentAmplifier::Sensitivity) {
-            gain = toGain(value);
-            sensitivity = value;
+//}
 
-        } else {
-            return;
-        }
+//QMultiMap<int, double>* CLSKeithley428ValueMap::map() const
+//{
+//    return map_;
+//}
 
-        addIndexValues(index, gain, sensitivity);
-    }
-}
+//double CLSKeithley428ValueMap::valueAt(int index, AMCurrentAmplifier::AmplifierMode mode)
+//{
+//    return map_->values(index).at(mode);
+//}
 
-void CLSKeithley428ValueMap::addIndexValues(int index, double gain, double sensitivity)
-{
-    map_->insert(index, gain);
-    map_->insert(index, sensitivity);
+//QStringList* CLSKeithley428ValueMap::valueStringList(AMCurrentAmplifier::AmplifierMode mode) const
+//{
+//    double toAdd = -1;
+//    QStringList *valueList = new QStringList();
 
-    emit valuesAdded(index, gain, sensitivity);
+//    foreach (int key, map_->uniqueKeys()) {
+//        toAdd = map_->values(key).at(mode);
+//        valueList->append(QString("%1").arg(toAdd, 0, 'e', 2));
+//    }
 
-}
+//    return valueList;
+//}
 
-double CLSKeithley428ValueMap::toGain(double sensitivity)
-{
-    return 1.0/sensitivity;
-}
+//bool CLSKeithley428ValueMap::isIndexOfMin(AMCurrentAmplifier::AmplifierMode mode, int index)
+//{
+//    return (findIndexOfMin(mode) == index);
+//}
 
-double CLSKeithley428ValueMap::toSensitivity(double gain)
-{
-    return 1.0/gain;
-}
+//bool CLSKeithley428ValueMap::isIndexOfMax(AMCurrentAmplifier::AmplifierMode mode, int index)
+//{
+//    return (findIndexOfMax(mode) == index);
+//}
+
+//int CLSKeithley428ValueMap::findIndexOfMin(AMCurrentAmplifier::AmplifierMode mode)
+//{
+//    double firstIndex = map_->uniqueKeys().first();
+//    double firstValue = map_->values(firstIndex).at(mode);
+//    double temp = firstValue;
+
+//    int minIndex = firstIndex;
+//    double minValue = firstValue;
+
+//    foreach (int key, map_->uniqueKeys()) {
+//        temp = map_->values(key).at(mode);
+
+//        if (temp < minValue) {
+//            minIndex = key;
+//            minValue = temp;
+//        }
+//    }
+
+//    return minIndex;
+//}
+
+//int CLSKeithley428ValueMap::findIndexOfMax(AMCurrentAmplifier::AmplifierMode mode)
+//{
+//    double firstIndex = map_->uniqueKeys().first();
+//    double firstValue = map_->values(firstIndex).at(mode);
+//    double temp = firstValue;
+
+//    int maxIndex = firstIndex;
+//    double maxValue = firstValue;
+
+//    foreach (int key, map_->uniqueKeys()) {
+//        temp = map_->values(key).at(mode);
+
+//        if (temp > maxValue) {
+//            maxIndex = key;
+//            maxValue = temp;
+//        }
+//    }
+
+//    return maxIndex;
+//}
+
+//int CLSKeithley428ValueMap::nextIndex(CLSKeithley428::IndexChange change, int currentIndex)
+//{
+//    int nextIndex = -1;
+
+//    if (!map_->contains(currentIndex)) {
+
+//    } else if (change == CLSKeithley428::IncreaseOne) {
+//        nextIndex = currentIndex + 1;
+
+//    } else if (change == CLSKeithley428::DecreaseOne) {
+//        nextIndex = currentIndex - 1;
+//    }
+
+//    return nextIndex;
+//}
+
+//void CLSKeithley428ValueMap::setValues(AMCurrentAmplifier::AmplifierMode mode, QList<double>* toAdd)
+//{
+//    map_->clear();
+
+//    for (int i = 0; i < toAdd->size(); i++) {
+//        addValue(i, mode, toAdd->at(i));
+//    }
+//}
+
+//void CLSKeithley428ValueMap::addValue(int index, AMCurrentAmplifier::AmplifierMode mode, double value)
+//{
+//    if (!map_->contains(index)) {
+//        double gain, sensitivity;
+
+//        if (mode == AMCurrentAmplifier::Gain) {
+//            gain = value;
+//            sensitivity = toSensitivity(value);
+
+//        } else if (mode == AMCurrentAmplifier::Sensitivity) {
+//            gain = toGain(value);
+//            sensitivity = value;
+
+//        } else {
+//            return;
+//        }
+
+//        addIndexValues(index, gain, sensitivity);
+//    }
+//}
+
+//void CLSKeithley428ValueMap::addIndexValues(int index, double gain, double sensitivity)
+//{
+//    map_->insert(index, gain);
+//    map_->insert(index, sensitivity);
+
+//    emit valuesAdded(index, gain, sensitivity);
+
+//}
+
+//double CLSKeithley428ValueMap::toGain(double sensitivity)
+//{
+//    return 1.0/sensitivity;
+//}
+
+//double CLSKeithley428ValueMap::toSensitivity(double gain)
+//{
+//    return 1.0/gain;
+//}
