5f58c9a14fc44c9cdab34bbfa359a13a2ee5d205
helfrij <jessie.helfrich@gmail.com>
2014-06-20 10:15:43 -0600

    Filled in the rest of the method bodies for AM1DTimedDataAB. I've commented out connections to the timestamps_ data source, I think it may cause some internal confusion if data_ updates before timestamps_... come back to this later.

--- source/analysis/AM1DTimedDataAB.cpp
+++ source/analysis/AM1DTimedDataAB.cpp
@@ -45,8 +45,8 @@ void AM1DTimedDataAB::setInputDataSourcesImplementation(const QList<AMDataSource
 
     if (timestamps_){
 
-        disconnect(timestamps_->signalSource(), SIGNAL(valuesChanged(AMnDIndex,AMnDIndex)), this, SLOT(onInputSourceValuesChanged(AMnDIndex,AMnDIndex)));
-        disconnect(timestamps_->signalSource(), SIGNAL(sizeChanged(int)), this, SLOT(onInputSourceSizeChanged()));
+//        disconnect(timestamps_->signalSource(), SIGNAL(valuesChanged(AMnDIndex,AMnDIndex)), this, SLOT(onInputSourceValuesChanged(AMnDIndex,AMnDIndex)));
+//        disconnect(timestamps_->signalSource(), SIGNAL(sizeChanged(int)), this, SLOT(onInputSourceSizeChanged()));
         disconnect(timestamps_->signalSource(), SIGNAL(stateChanged(int)), this, SLOT(onInputSourceStateChanged()));
         timestamps_ = 0;
     }
@@ -73,8 +73,8 @@ void AM1DTimedDataAB::setInputDataSourcesImplementation(const QList<AMDataSource
         connect(data_->signalSource(), SIGNAL(sizeChanged(int)), this, SLOT(onInputSourceSizeChanged()));
         connect(data_->signalSource(), SIGNAL(stateChanged(int)), this, SLOT(onInputSourceStateChanged()));
 
-        connect(timestamps_->signalSource(), SIGNAL(valuesChanged(AMnDIndex,AMnDIndex)), this, SLOT(onInputSourceValuesChanged(AMnDIndex,AMnDIndex)));
-        connect(timestamps_->signalSource(), SIGNAL(sizeChanged(int)), this, SLOT(onInputSourceSizeChanged()));
+//        connect(timestamps_->signalSource(), SIGNAL(valuesChanged(AMnDIndex,AMnDIndex)), this, SLOT(onInputSourceValuesChanged(AMnDIndex,AMnDIndex)));
+//        connect(timestamps_->signalSource(), SIGNAL(sizeChanged(int)), this, SLOT(onInputSourceSizeChanged()));
         connect(timestamps_->signalSource(), SIGNAL(stateChanged(int)), this, SLOT(onInputSourceStateChanged()));
     }
 
@@ -127,31 +127,62 @@ AMNumber AM1DTimedDataAB::axisValue(int axisNumber, int index) const
 
 bool AM1DTimedDataAB::loadFromDb(AMDatabase *db, int id)
 {
-    Q_UNUSED(db)
-    Q_UNUSED(id)
-
-    return false;
+    bool success = AMDbObject::loadFromDb(db, id);
+    if(success)
+        AMDataSource::name_ = AMDbObject::name();	/// \todo This might change the name of a data-source in mid-life, which is technically not allowed.
+    return success;
 }
 
 void AM1DTimedDataAB::onInputSourceValuesChanged(const AMnDIndex &start, const AMnDIndex &end)
 {
-    Q_UNUSED(start)
-    Q_UNUSED(end)
+    emitValuesChanged(start, end);
 }
 
 void AM1DTimedDataAB::onInputSourceSizeChanged()
 {
-    return;
+    if(axes_.at(0).size != data_->size(0)){
+
+        axes_[0].size = data_->size(0);
+        emitSizeChanged(0);
+    }
 }
 
 void AM1DTimedDataAB::onInputSourceStateChanged()
 {
+    // just in case the size has changed while the input source was invalid, and now it's going valid.  Do we need this? probably not, if the input source is well behaved. But it's pretty inexpensive to do it twice... and we know we'll get the size right everytime it goes valid.
+    onInputSourceSizeChanged();
     reviewState();
 }
 
 void AM1DTimedDataAB::reviewState()
 {
-    return;
+    // are there data sources for this AB?
+    if (sources_.isEmpty()) {
+        setState(AMDataSource::InvalidFlag);
+        return;
+    }
+
+    // are there two sources?
+    if (sources_.count() != 2) {
+        setState(AMDataSource::InvalidFlag);
+        return;
+    }
+
+    // are the two sources identified appropriately?
+    if (!data_ || !timestamps_) {
+        setState(AMDataSource::InvalidFlag);
+        return;
+    }
+
+    // are both sources valid?
+    foreach (AMDataSource *source, sources_)
+        if (!source->isValid()) {
+            setState(AMDataSource::InvalidFlag);
+            return;
+        }
+
+    // otherwise, this AB is valid.
+    setState(0);
 }
 
 
