70ba44fee2e7780126da58290ba218c8d3133351
helfrij <jessie.helfrich@gmail.com>
2014-06-26 16:01:24 -0600

    Removed all functionality in AMCurrentAmplifierSingleView that now belongs in AMCurrentAmplifierView.

--- source/ui/beamline/AMCurrentAmplifierSingleView.cpp
+++ source/ui/beamline/AMCurrentAmplifierSingleView.cpp
@@ -3,51 +3,13 @@
 AMCurrentAmplifierSingleView::AMCurrentAmplifierSingleView(AMCurrentAmplifier *amplifier, QWidget *parent) :
     AMCurrentAmplifierView(parent)
 {
-//    initialized_ = false;
-
     amplifier_ = amplifier;
     connect( amplifier_, SIGNAL(amplifierModeChanged()), this, SLOT(refreshView()) );
     connect( amplifier_, SIGNAL(valueChanged()), this, SLOT(onAmplifierValueChanged()) );
-
-//    name_ = new QLabel(amplifier_->name());
-
-//    minus_ = new QToolButton();
-//    minus_->show();
-//    minus_->setMaximumSize(25, 25);
-//    minus_->setIcon(QIcon(":/22x22/list-remove.png"));
-//    connect( minus_, SIGNAL(clicked()), this, SLOT(onMinusClicked()) );
     connect( amplifier_, SIGNAL(minimumValue(bool)), minus_, SLOT(setDisabled(bool)) );
-
-//    plus_ = new QToolButton();
-//    plus_->show();
-//    plus_->setMaximumSize(25, 25);
-//    plus_->setIcon(QIcon(":/22x22/list-add.png"));
-//    connect( plus_, SIGNAL(clicked()), this, SLOT(onPlusClicked()) );
     connect( amplifier_, SIGNAL(maximumValue(bool)), plus_, SLOT(setDisabled(bool)) );
 
-//    value_ = new QComboBox();
-//    value_->setMaxVisibleItems(5);
-//    value_->hide();
-//    connect( value_, SIGNAL(currentIndexChanged(QString)), this, SLOT(onValueComboBoxChanged(QString)) );
-
-//    setViewMode(Basic);
-//    showName(false);
-
-//    setContextMenuPolicy(Qt::CustomContextMenu);
-//    connect( this, SIGNAL(customContextMenuRequested(QPoint)), this, SLOT(onCustomContextMenuRequested(QPoint)) );
-
     refreshView();
-
-//    QHBoxLayout *layout = new QHBoxLayout();
-
-//    layout->addWidget(name_);
-//    layout->addWidget(minus_);
-//    layout->addWidget(plus_);
-//    layout->addWidget(value_);
-
-//    setLayout(layout);
-
-//    initialized_ = true;
 }
 
 AMCurrentAmplifierSingleView::~AMCurrentAmplifierSingleView()
@@ -60,55 +22,21 @@ AMCurrentAmplifier* AMCurrentAmplifierSingleView::amplifier() const
     return amplifier_;
 }
 
-//bool AMCurrentAmplifierSingleView::initialized() const
-//{
-//    return initialized_;
-//}
-
-//AMCurrentAmplifierView::ViewMode AMCurrentAmplifierSingleView::viewMode() const
-//{
-//    return mode_;
-//}
-
-//void AMCurrentAmplifierSingleView::setViewMode(ViewMode newMode)
-//{
-//    if (newMode != mode_) {
-//        mode_ = newMode;
-
-//        if (mode_ == Basic) {
-//            value_->hide();
-//            plus_->show();
-//            minus_->show();
-
-//        } else {
-//            value_->show();
-//            plus_->hide();
-//            minus_->hide();
-//        }
-
-//        emit viewModeChanged(mode_);
-//    }
-//}
-
-//void AMCurrentAmplifierSingleView::showName(bool show)
-//{
-//    if (show) {
-//        name_->show();
-
-//    } else {
-//        name_->hide();
-//    }
-//}
-
-//void AMCurrentAmplifierSingleView::setViewableValuesMax(int newMax)
-//{
-//    value_->setMaxVisibleItems(newMax);
-//}
-
 void AMCurrentAmplifierSingleView::refreshView()
 {
-    refreshDisplayValues();
+    refreshValues();
     refreshButtons();
+}
+
+void AMCurrentAmplifierSingleView::onAmplifierValueChanged()
+{
+    amplifier_->blockSignals(true);
+
+    int newIndex = value_->findText( valueToString(amplifier_->value(), amplifier_->units()) );
+    if (newIndex != -1)
+        value_->setCurrentIndex(newIndex);
+
+    amplifier_->blockSignals(false);
 }
 
 void AMCurrentAmplifierSingleView::onValueComboBoxChanged(const QString &newText)
@@ -117,15 +45,6 @@ void AMCurrentAmplifierSingleView::onValueComboBoxChanged(const QString &newText
     if (initialized_) {
         amplifier_->setValue(newText);
     }
-}
-
-void AMCurrentAmplifierSingleView::onAmplifierValueChanged()
-{
-    amplifier_->blockSignals(true);
-    int newIndex = value_->findText( valueToString(amplifier_->value(), amplifier_->units()) );
-    if (newIndex != -1)
-        value_->setCurrentIndex(newIndex);
-    amplifier_->blockSignals(false);
 }
 
 void AMCurrentAmplifierSingleView::onMinusClicked()
@@ -180,7 +99,7 @@ QString AMCurrentAmplifierSingleView::valueToString(double value, const QString 
     return QString("%1 %2").arg(value, 0, 'g', 2).arg(units);
 }
 
-void AMCurrentAmplifierSingleView::refreshDisplayValues()
+void AMCurrentAmplifierSingleView::refreshValues()
 {
     initialized_ = false;
 
--- source/ui/beamline/AMCurrentAmplifierSingleView.h
+++ source/ui/beamline/AMCurrentAmplifierSingleView.h
@@ -23,29 +23,19 @@ public:
 
     /// Returns the pointer to the AMCurrentAmplifier this view is visualizing.
     AMCurrentAmplifier *amplifier() const;
-//    /// Returns boolean indicating whether this view has been initialized.
-//    bool initialized() const;
-//    /// Returns the current view mode.
-//    AMCurrentAmplifierView::ViewMode viewMode() const;
 
 signals:
-//    void viewModeChanged(AMCurrentAmplifierView::ViewMode newMode);
 
 public slots:
-//    /// Sets the view mode.
-//    void setViewMode(AMCurrentAmplifierView::ViewMode newMode);
-//    /// Sets whether the amplifier name should be shown.
-//    void showName(bool showName);
-//    /// Sets the maximum viewable elements in the combo box. Note that QComboBox::setMaxVisibleItems() is ignored in some combobox styles, such as the Mac style (see Qt documentatation).
-//    void setViewableValuesMax(int newMax);
     /// Clears and resets view widgets.
     void refreshView();
 
 protected slots:
-    /// Handles passing changes in the value combo box to the amplifier.
-    virtual void onValueComboBoxChanged(const QString &newText);
     /// Updates the value_ widget selection to reflect amplifier's new selection.
     void onAmplifierValueChanged();
+
+    /// Handles passing changes in the value combo box to the amplifier.
+    virtual void onValueComboBoxChanged(const QString &newText);
     /// Calls either AMCurrentAmplifier::decreaseGain/decreaseSensitivity depending on the amplifier mode.
     virtual void onMinusClicked();
     /// Calls either AMCurrentAmplifier::increaseGain/increaseSensitivity depending on the amplifier mode.
@@ -57,25 +47,13 @@ protected:
     /// Returns a string of the amplifier's value and units. Provides consistent formatting.
     QString valueToString(double value, const QString &units) const;
     /// Clears and repopulates value_ widget with information from amplifier_.
-    void refreshDisplayValues();
+    void refreshValues();
     /// Sets whether buttons should be en/disabled according to whether amplifier_ is at a max/min gain/sensitivity state.
     void refreshButtons();
 
 protected:
-//    /// Boolean indicating whether this view has been initialized.
-//    bool initialized_;
     /// The pointer to the current amplifier this view manages.
     AMCurrentAmplifier *amplifier_;
-//    /// Flag holding whether or not the view is basic or advanced.
-//    ViewMode mode_;
-//    /// Label to display amplifier name.
-//    QLabel *name_;
-//    /// The tool button for the minus button.
-//    QToolButton *minus_;
-//    /// The tool button for the plus button.
-//    QToolButton *plus_;
-//    /// Combo box holding the available options for amplifier value.
-//    QComboBox *value_;
 };
 
 #endif // AMCURRENTAMPLIFIERSINGLEVIEW_H
