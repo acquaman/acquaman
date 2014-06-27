470490bca573feaf477c892c704e6fdeaa922fbc
helfrij <jessie.helfrich@gmail.com>
2014-06-27 11:04:48 -0600

    Added comments to function definitions in AMCurrentAmplifierCompositeView.

--- source/ui/beamline/AMCurrentAmplifierCompositeView.h
+++ source/ui/beamline/AMCurrentAmplifierCompositeView.h
@@ -11,7 +11,7 @@ class AMCurrentAmplifierCompositeView : public AMCurrentAmplifierView
     Q_OBJECT
 
 public:
-    /// Constructor.
+    /// Constructor. Takes two AMCurrentAmplifier arguments.
     explicit AMCurrentAmplifierCompositeView(AMCurrentAmplifier *amp1, AMCurrentAmplifier *amp2, QWidget *parent = 0);
     /// Destructor.
     virtual ~AMCurrentAmplifierCompositeView();
@@ -20,22 +20,28 @@ public:
     AMCurrentAmplifier *amplifier1() const;
     /// Returns the second amplifier.
     AMCurrentAmplifier *amplifier2() const;
+    /// Returns true if this view is valid, false otherwise.
     virtual bool isValid() const;
-
 
 signals:
 
 public slots:
 
 protected slots:
+    /// Updates the value_ widget selection to reflect the amplifier's new selection.
+    void onAmplifierValueChanged();
+    /// Handles passing changes in the value_ combobox to the amplifier.
     virtual void onValueComboBoxChangedImplementation(const QString &newText);
+    /// Calls AMCurrentAmplifier::decreaseValue() for both amplifiers.
     virtual void onMinusClickedImplementation();
+    /// Calls AMCurrentAmplifier::increaseValue() for both amplifiers.
     virtual void onPlusClickedImplementation();
+    /// Clears and repopulations value_ widget.
     virtual void refreshViewImplementation();
 
-    void onAmplifierValueChanged();
 
 protected:
+    /// Returns a string of the amplifier value and units. Provides consistent formatting.
     QString valueToString(double value, const QString &units) const;
 
 protected:
