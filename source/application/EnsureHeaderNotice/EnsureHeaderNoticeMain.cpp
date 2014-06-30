2ab8d6cf419e4dad01fa8f3f9fadb96501641ef7
David Chevrier <david.k.chevrier@gmail.com>
2014-06-30 11:18:57 -0600

    Tring to fix up an issue with doubling replace old by adding a newline.

--- source/application/EnsureHeaderNotice/EnsureHeaderNoticeMain.cpp
+++ source/application/EnsureHeaderNotice/EnsureHeaderNoticeMain.cpp
@@ -10,8 +10,8 @@ int main(int argc, char *argv[])
     if(arguments.count() < 2)
 	    return -1;
 
-    QString oldNotice = "Copyright 2010-2012 Mark Boots, David Chevrier, and Darren Hunter.";
-    QString newNotice = "Copyright 2010-2012 Mark Boots, David Chevrier, and Darren Hunter.\nCopyright 2013-2014 David Chevrier and Darren Hunter.";
+    QString oldNotice = "Copyright 2010-2012 Mark Boots, David Chevrier, and Darren Hunter.\n";
+    QString newNotice = "Copyright 2010-2012 Mark Boots, David Chevrier, and Darren Hunter.\nCopyright 2013-2014 David Chevrier and Darren Hunter.\n";
     HeaderNoticeChecker *checker = new HeaderNoticeChecker(oldNotice, newNotice, arguments.at(1));
 
     Q_UNUSED(checker)
--- source/application/EnsureHeaderNotice/HeaderNoticeChecker.cpp
+++ source/application/EnsureHeaderNotice/HeaderNoticeChecker.cpp
@@ -8,7 +8,7 @@ HeaderNoticeChecker::HeaderNoticeChecker(const QString &oldNotice, const QString
 	oldNotice_ = oldNotice;
 	newNotice_ = newNotice;
 	anyNotice_ = "This file is part of the Acquaman Data Acquisition and Management framework (\\\"Acquaman\\\").";
-	fullNotice_ = "\n\nThis file is part of the Acquaman Data Acquisition and Management framework (\\\"Acquaman\\\").\nAcquaman is free software: you can redistribute it and/or modify\nit under the terms of the GNU General Public License as published by\nthe Free Software Foundation, either version 3 of the License, or\n(at your option) any later version.\n\nAcquaman is distributed in the hope that it will be useful,\nbut WITHOUT ANY WARRANTY; without even the implied warranty of\nMERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\nGNU General Public License for more details.\n\nYou should have received a copy of the GNU General Public License\nalong with Acquaman.  If not, see <http://www.gnu.org/licenses/>.\n";
+	fullNotice_ = "\nThis file is part of the Acquaman Data Acquisition and Management framework (\\\"Acquaman\\\").\nAcquaman is free software: you can redistribute it and/or modify\nit under the terms of the GNU General Public License as published by\nthe Free Software Foundation, either version 3 of the License, or\n(at your option) any later version.\n\nAcquaman is distributed in the hope that it will be useful,\nbut WITHOUT ANY WARRANTY; without even the implied warranty of\nMERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\nGNU General Public License for more details.\n\nYou should have received a copy of the GNU General Public License\nalong with Acquaman.  If not, see <http://www.gnu.org/licenses/>.\n";
 
 	ignoreDirectories_ << "muParser" << "qjson" << "Eigen";
 
