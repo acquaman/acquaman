e9177e3a001d1c5c25b28ca802273ef60e20e2ad
helfrij <jessie.helfrich@gmail.com>
2014-07-03 15:31:31 -0600

    Filled in some basic code in STVariableEditor that could be used to display and edit characteristics of STVariables.

--- source/StripTool2/STVariableEditor.cpp
+++ source/StripTool2/STVariableEditor.cpp
@@ -1,7 +1,92 @@
 #include "STVariableEditor.h"
 
-STVariableEditor::STVariableEditor(QWidget *parent) :
-    QWidget(parent)
+STVariableEditor::STVariableEditor(STVariable *toEdit, QWidget *parent) : QWidget(parent)
+{
+    variable_ = toEdit;
+    setupComponents();
+    getVariableInfo();
+}
+
+STVariableEditor::~STVariableEditor()
 {
 
+}
+
+void STVariableEditor::setNameText(const QString &name)
+{
+    name_->setText(name);
+}
+
+void STVariableEditor::setDescriptionText(const QString &description)
+{
+    descriptionEntry_->setText(description);
+}
+
+void STVariableEditor::setCreationText(const QString &creation)
+{
+    creation_->setText(creation);
+}
+
+void STVariableEditor::setConnectionState(bool isConnected)
+{
+    if (isConnected)
+        connected_->setText("Connected");
+    else
+        connected_->setText("Not connected");
+}
+
+void STVariableEditor::setLatestValue(double newValue)
+{
+    value_->setText(QString::number(newValue));
+}
+
+void STVariableEditor::setupComponents()
+{
+    QGridLayout *mainLayout = new QGridLayout();
+
+    QLabel *nameLabel = new QLabel("Name: ", this);
+    name_ = new QLabel(this);
+    nameLabel->setBuddy(name_);
+    mainLayout->addWidget(nameLabel, 0, 0);
+    mainLayout->addWidget(name_, 0, 1);
+
+    QLabel *descriptionLabel = new QLabel("Description: ", this);
+    descriptionEntry_ = new QLineEdit(this);
+    descriptionLabel->setBuddy(descriptionEntry_);
+    mainLayout->addWidget(descriptionLabel, 1, 0);
+    mainLayout->addWidget(descriptionEntry_, 1, 1);
+
+    QLabel *creationLabel = new QLabel("Created: ", this);
+    creation_ = new QLabel(this);
+    creationLabel->setBuddy(creation_);
+    mainLayout->addWidget(creationLabel, 2, 0);
+    mainLayout->addWidget(creation_, 2, 1);
+
+    QLabel *connectedLabel = new QLabel("Connected: ", this);
+    connected_ = new QLabel(this);
+    connectedLabel->setBuddy(connected_);
+    mainLayout->addWidget(connectedLabel, 3, 0);
+    mainLayout->addWidget(connected_, 3, 1);
+
+    QLabel *valueLabel = new QLabel("Latest value: ", this);
+    value_ = new QLabel(this);
+    valueLabel->setBuddy(value_);
+    mainLayout->addWidget(valueLabel, 4, 0);
+    mainLayout->addWidget(value_, 4, 1);
+
+    setLayout(mainLayout);
+}
+
+void STVariableEditor::getVariableInfo()
+{
+    if (variable_) {
+        setNameText(variable_->name());
+        setDescriptionText(variable_->description());
+        setCreationText(variable_->created().toString());
+        setConnectionState(variable_->isConnected());
+        setLatestValue(variable_->value());
+
+        connect( variable_, SIGNAL(connected(bool)), this, SLOT(setConnectionState(bool)) );
+        connect( variable_, SIGNAL(valueChanged(double)), this, SLOT(setLatestValue(double)) );
+    }
 }
--- source/StripTool2/STVariableEditor.h
+++ source/StripTool2/STVariableEditor.h
@@ -3,18 +3,42 @@
 
 #include <QWidget>
 #include <QDialog>
+#include <QLabel>
+#include <QLineEdit>
+#include <QLayout>
+
+#include "STVariable.h"
 
 class STVariableEditor : public QWidget
 {
     Q_OBJECT
 
 public:
-    explicit STVariableEditor(QWidget *parent = 0);
+    explicit STVariableEditor(STVariable *toEdit, QWidget *parent = 0);
+    virtual ~STVariableEditor();
 
 signals:
 
-public slots:
+protected slots:
+    void setNameText(const QString &name);
+    void setDescriptionText(const QString &description);
+    void setCreationText(const QString &creation);
+    void setConnectionState(bool isConnected);
+    void setLatestValue(double newValue);
 
+protected:
+    void setupComponents();
+    void getVariableInfo();
+    void applyChangesToVariable();
+
+protected:
+    STVariable *variable_;
+
+    QLabel *name_;
+    QLineEdit *descriptionEntry_;
+    QLabel *creation_;
+    QLabel *connected_;
+    QLabel *value_;
 };
 
 #endif // STVARIABLEEDITOR_H
