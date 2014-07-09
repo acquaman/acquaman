49cd977e7a41945371776aa9dff1eafedfbd3d68
Iain Workman <iain.workman@googlemail.com>
2014-07-09 15:48:07 -0600

    Altered - AMScanThumbnailView ~ Impmeneted some of the initial methods to populate the data
    Added:
    - class AMScanThumbnailViewItem ~ Represents a single thumbnail in an
    AMScanThumbnailView

--- source/ui/dataman/AMScanThumbnailView.cpp
+++ source/ui/dataman/AMScanThumbnailView.cpp
@@ -1,9 +1,16 @@
 #include "AMScanThumbnailView.h"
-
+#include "dataman/AMLightweightScanInfoFilterProxyModel.h"
+#include "ui/dataman/AMFlowGraphicsLayout.h"
+#include <QHBoxLayout>
+#include <QGraphicsWidget>
 AMScanThumbnailView::AMScanThumbnailView(QWidget *parent)
 	:QAbstractItemView(parent)
 {
+	centralView_ = new QGraphicsView();
+	QHBoxLayout* layout = new QHBoxLayout();
 
+	layout->addWidget(centralView_);
+	setLayout(layout);
 }
 
 QRect AMScanThumbnailView::visualRect(const QModelIndex &index) const
@@ -21,6 +28,29 @@ QModelIndex AMScanThumbnailView::indexAt(const QPoint &point) const
 	return QModelIndex();
 }
 
+void AMScanThumbnailView::setModel(QAbstractItemModel *model)
+{
+	QAbstractItemView::setModel(model);
+
+	AMLightweightScanInfoFilterProxyModel* actualModel = qobject_cast<AMLightweightScanInfoFilterProxyModel*>(model);
+
+	if(!actualModel)
+		return;
+
+	QGraphicsScene* scene = new QGraphicsScene(this);
+	AMFlowGraphicsLayout* layout = new AMFlowGraphicsLayout();
+	for(int iScanInfo = 0; iScanInfo < actualModel->rowCount(); iScanInfo++)
+	{
+		AMScanThumbnailViewItem* thumbnailView = new AMScanThumbnailViewItem(iScanInfo);
+		thumbnailView->setModel(actualModel);
+		layout->addItem(thumbnailView);
+	}
+	QGraphicsWidget* widget = new QGraphicsWidget();
+	widget->setLayout(layout);
+	scene->addItem(widget);
+	centralView_->setScene(scene);
+}
+
 QModelIndex AMScanThumbnailView::moveCursor(QAbstractItemView::CursorAction cursorAction, Qt::KeyboardModifiers modifiers)
 {
 	return QModelIndex();
@@ -28,15 +58,15 @@ QModelIndex AMScanThumbnailView::moveCursor(QAbstractItemView::CursorAction curs
 
 int AMScanThumbnailView::horizontalOffset() const
 {
-	return -1;
+	return 50;
 }
 
 int AMScanThumbnailView::verticalOffset() const
 {
-	return -1;
+	return 50;
 }
 
-bool AMScanThumbnailView::isIndexHidden(const QModelIndex &index) const
+bool AMScanThumbnailView::isIndexHidden(const QModelIndex &) const
 {
 	return false;
 }
@@ -51,4 +81,66 @@ QRegion AMScanThumbnailView::visualRegionForSelection(const QItemSelection &sele
 	return QRegion();
 }
 
+////////////////////////////////////////////
+// Defintions for AMScanThumbnailViewItem //
+////////////////////////////////////////////
+AMScanThumbnailViewItem::AMScanThumbnailViewItem(int viewIndex, QGraphicsItem *parent)
+	: QGraphicsItem(parent), QGraphicsLayoutItem()
+{
+	viewIndex_ = viewIndex;
+	currentThumbnail_ = 0;
+	thumbnailIndex_ = 0;
+	model_ = 0;
+	thumbnailCount_ = 0;
+	width_ = 240;
+	height_ = 180;
+}
+
+void AMScanThumbnailViewItem::setModel(AMLightweightScanInfoFilterProxyModel *model)
+{
+	model_ = model;
+	currentThumbnail_ = model_->thumbnailAt(viewIndex_, thumbnailIndex_);
+	thumbnailCount_ = model_->thumbnailCount(viewIndex_);
+}
+
+void AMScanThumbnailViewItem::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
+{
+	Q_UNUSED(option)
+	Q_UNUSED(widget)
+
+	// Are we selected? Draw a selection background
+	//if(isSelected()) {
+		painter->save();
+		painter->setRenderHint(QPainter::Antialiasing);
+		painter->setCompositionMode(QPainter::CompositionMode_Source);
+		painter->setPen(Qt::NoPen);
+		//painter->setBrush(QColor(56,117,215));
+		painter->setBrush(QColor(25,87,172));
+		//painter->setBrush(QColor(48,107,186));
+		painter->drawRoundedRect(QRectF(0, 0, width_+20, height_ + 20), 5, 5);
+		painter->restore();
+	//}
+}
+
+void AMScanThumbnailViewItem::hoverMoveEvent(QGraphicsSceneHoverEvent *event)
+{
+}
+
+void AMScanThumbnailViewItem::hoverEnterEvent(QGraphicsSceneHoverEvent *event)
+{
+}
+
+void AMScanThumbnailViewItem::hoverLeaveEvent(QGraphicsSceneHoverEvent *event)
+{
+}
+
+QRectF AMScanThumbnailViewItem::boundingRect() const
+{
+	return QRectF(0, 0, 260, 200);
+}
+
+QSizeF AMScanThumbnailViewItem::sizeHint(Qt::SizeHint which, const QSizeF &constraint) const
+{
+	return QSizeF(QSize(width_, height_));
+}
 
--- source/ui/dataman/AMScanThumbnailView.h
+++ source/ui/dataman/AMScanThumbnailView.h
@@ -3,21 +3,55 @@
 
 #include <QAbstractItemView>
 #include <QWidget>
+#include <QPaintEvent>
+#include <QPainter>
+#include <QGraphicsView>
+#include <QGraphicsItem>
+#include <QGraphicsLayoutItem>
+class AMLightweightScanInfoFilterProxyModel;
+class AMDbThumbnail;
+
+class AMScanThumbnailViewItem : public QGraphicsItem, public QGraphicsLayoutItem {
+private:
+	AMLightweightScanInfoFilterProxyModel* model_;
+	AMDbThumbnail* currentThumbnail_;
+	int thumbnailIndex_;
+	int viewIndex_;
+	int thumbnailCount_;
+	int width_;
+	int height_;
+public:
+	explicit AMScanThumbnailViewItem(int viewIndex, QGraphicsItem* parent = 0);
+	void setModel(AMLightweightScanInfoFilterProxyModel* model);
+protected:
+	virtual void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget);
+	void hoverMoveEvent(QGraphicsSceneHoverEvent *event);
+	void hoverEnterEvent(QGraphicsSceneHoverEvent *event);
+	void hoverLeaveEvent(QGraphicsSceneHoverEvent *event);
+	QRectF boundingRect() const;
+	QSizeF sizeHint(Qt::SizeHint which, const QSizeF &constraint) const;
+};
+
 class AMScanThumbnailView : public QAbstractItemView {
 	Q_OBJECT
+private:
+	QGraphicsView* centralView_;
 public:
 	explicit AMScanThumbnailView(QWidget* parent = 0);
 	virtual QRect visualRect(const QModelIndex &index) const;
 	virtual void scrollTo(const QModelIndex &index, ScrollHint hint);
 	virtual QModelIndex indexAt(const QPoint &point) const;
+	virtual void setModel(QAbstractItemModel *model);
 protected:
 	virtual QModelIndex moveCursor(CursorAction cursorAction, Qt::KeyboardModifiers modifiers);
 	virtual int horizontalOffset() const;
 	virtual int verticalOffset() const;
-	virtual bool isIndexHidden(const QModelIndex &index) const;
+	virtual bool isIndexHidden(const QModelIndex &) const;
 	virtual void setSelection(const QRect &rect, QItemSelectionModel::SelectionFlags command);
 	virtual QRegion visualRegionForSelection(const QItemSelection &selection) const;
 
 };
+
+
 
 #endif // AMSCANTHUMBNAILVIEW_H
