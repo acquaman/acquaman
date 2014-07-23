d7d15a76eddc4205e50f257c303e9a0ef0255ca1
Darren Hunter <darren.hunter@usask.ca>
2014-07-23 16:24:05 -0600

    Added: - Added a little bit more to the database upgrade required to bring SGM and VESPERS to the point where they can use AMStepScanConfiguration/controller.  Still needs a fair bit of work.

--- source/dataman/AMDbUpgrade1Pt5.cpp
+++ source/dataman/AMDbUpgrade1Pt5.cpp
@@ -1,6 +1,7 @@
 #include "AMDbUpgrade1Pt5.h"
 
 #include "util/AMErrorMonitor.h"
+#include <QStringBuilder>
 
 AMDbUpgrade1Pt5::AMDbUpgrade1Pt5(const QString &databaseNameToUpgrade, QObject *parent)
 	: AMDbUpgrade(databaseNameToUpgrade, parent)
@@ -45,6 +46,87 @@ bool AMDbUpgrade1Pt5::upgradeImplementation()
 	query.finish();
 
 	qDebug() << scanConfigurations;
+
+	if (!scanConfigurations.isEmpty()){
+
+		if (!databaseToUpgrade_->tableExists("AMStepScanConfiguration_table")
+				&& databaseToUpgrade_->ensureTable("AMStepScanConfiguration_table",
+												   QStringList() << "AMDbObjectTypes" << "thumbnailCount" << "thumbnailFirstId" << "name" << "detectorConfigurations" << "axisControlInfos",
+												   QStringList() << "TEXT" << "INTEGER" << "INTEGER" << "TEXT" << "TEXT" << "TEXT")){
+
+			databaseToUpgrade_->rollbackTransaction();
+			return false;
+		}
+
+		if (!databaseToUpgrade_->tableExists("AMStepScanConfiguration_table_scanAxes")
+				&& databaseToUpgrade_->ensureTable("AMStepScanConfiguration_table_scanAxes",
+												   QStringList() << "id1" << "table1" << "id2" << "table2",
+												   QStringList() << "INTEGER" << "TEXT" << "INTEGER" << "TEXT")){
+
+			databaseToUpgrade_->rollbackTransaction();
+			return false;
+		}
+
+		if (!databaseToUpgrade_->tableExists("AMScanAxis_table")
+				&& databaseToUpgrade_->ensureTable("AMScanAxis_table",
+												   QStringList() << "AMDbObjectTypes" << "thumbnailCount" << "thumbnailFirstId" << "name" << "axisType",
+												   QStringList() << "TEXT" << "INTEGER" << "INTEGER" << "TEXT" << "INTEGER")){
+
+			databaseToUpgrade_->rollbackTransaction();
+			return false;
+		}
+
+		if (!databaseToUpgrade_->tableExists("AMScanAxis_table_axisRegions")
+				&& databaseToUpgrade_->ensureTable("AMScanAxis_table_axisRegions",
+												   QStringList() << "id1" << "table1" << "id2" << "table2",
+												   QStringList() << "INTEGER" << "TEXT" << "INTEGER" << "TEXT")){
+
+			databaseToUpgrade_->rollbackTransaction();
+			return false;
+		}
+
+		if (!databaseToUpgrade_->tableExists("AMScanAxisRegion_table")
+				&& databaseToUpgrade_->ensureTable("AMScanAxisRegion_table",
+												   QStringList() << "AMDbObjectTypes" << "thumbnailCount" << "thumbnailFirstId" << "name" << "regionStart" << "regionStep" << "regionEnd" << "regionTime",
+												   QStringList() << "TEXT" << "INTEGER" << "INTEGER" << "TEXT" << "REAL" << "REAL" << "REAL" << "REAL")){
+
+			databaseToUpgrade_->rollbackTransaction();
+			return false;
+		}
+
+		if (!databaseToUpgrade_->tableExists("AMScanAxisEXAFSRegion_table")
+				&& databaseToUpgrade_->ensureTable("AMScanAxisEXAFSRegion_table",
+												   QStringList() << "AMDbObjectTypes" << "thumbnailCount" << "thumbnailFirstId" << "name" << "regionStart" << "regionStep" << "regionEnd" << "regionTime" << "inKSpace" << "edgeEnergy" << "maximumTime" << "a2" << "equation",
+												   QStringList() << "TEXT" << "INTEGER" << "INTEGER" << "TEXT" << "REAL" << "REAL" << "REAL" << "REAL" << "TEXT" << "REAL" << "REAL" << "REAL" << "INTEGER")){
+
+			databaseToUpgrade_->rollbackTransaction();
+			return false;
+		}
+
+		QStringList tablesToModify;
+
+		foreach (QString configuration, scanConfigurations)
+			tablesToModify << configuration.split(";").first();
+
+		foreach (QString table, tablesToModify){
+
+			QVariantList ids = databaseToUpgrade_->retrieve(table % "_table", "id");
+			QVariantList regions = databaseToUpgrade_->retrieve(table % "_table", "regions");
+
+			bool areEXAFSRegions = regions.first().toString().contains("exafsVersion1.0");
+
+			int scanAxisId = databaseToUpgrade_->insertOrUpdate(0,
+																"AMScanAxis_table",
+																QStringList() << "AMDbObjectTypes" << "thumbnailCount" << "thumbnailFirstId" << "name" << "axisType",
+																QVariantList() << "AMScanAxis" << 0 << QVariant() << "" << 0);
+
+			for (int i = 0, regionsSize = regions.size(); i < regionsSize; i++){
+
+				QStringList regionComponents = regions.at(i).toString().split(",");
+
+			}
+		}
+	}
 
 	// Doing AM2DScanConfiguration.
 	scanConfigurations = QStringList();
