a8d7fb1d1193ac455f49391a72e901f0ef21255a
xuetaoliu-cls <xuetao.liu@lightsource.ca>
2014-08-14 11:43:22 -0600

    Added:    - new class AMAppArgumentParser added to parse the program arguments

---
+++ source/util/AMAppArgumentParser.cpp
@@ -0,0 +1,102 @@
+#include "AMAppArgumentParser.h"
+
+#include <QStringList>
+#include <QDebug>
+
+//===================== public member functions =======================
+bool AMAppArgumentParser::hasArgument(QString argument)
+{
+	return argumentValuePairs_.contains(argument);
+}
+
+int AMAppArgumentParser::getArgumentType(QString argument)
+{
+	int type = NOT_EXIST;
+
+	if (hasArgument(argument)) {
+		if (argument.startsWith("--"))
+			type = LONG_ARGUMENT;
+		else if (argument.startsWith("-"))
+			type = SINGLE_CHARACTER_ARGUMENT;
+		else
+			type = PROGRAM_ARGUMENT;
+	}
+
+	return type;
+}
+
+QStringList AMAppArgumentParser::getArgumentsByType(AMAppArgumentType argumentType) {
+	QStringList arguments;
+
+	QStringList keys = argumentValuePairs_.keys();
+	for (int i = 0; i < keys.length(); i++) {
+		if (getArgumentType(keys.at(i)) == argumentType) {
+			arguments.append(keys.at(i));
+		}
+	}
+
+	return arguments;
+}
+
+QString AMAppArgumentParser::getArgumentValue(QString argument)
+{
+	QString value = NULL;
+
+	if (argumentValuePairs_.contains(argument)) {
+		value = argumentValuePairs_.value(argument);
+	}
+
+	return value;
+}
+
+// debugging usage
+void AMAppArgumentParser::printArgumentPairs()
+{
+	qDebug() << "Program: " << applicationName_;
+	qDebug() << "argument" << "	" << "value";
+	qDebug() << "----------------------------------------";
+
+	for (int i = 0; i < argumentValuePairs_.count(); i++) {
+		QString key = argumentValuePairs_.keys().at(i);
+		qDebug()<< key << "	" << argumentValuePairs_.value(key);
+	}
+}
+
+//===================== private member functions =======================
+
+void AMAppArgumentParser::parseArguments(QStringList arguments)
+{
+	const QString idSingleCharacterArgument = "-";
+	const QString idLongArgument = "--";
+	const QString regExpArgumentValue = "\\=|:";
+
+	int cursor = 0;
+
+	applicationName_ = arguments.at(cursor++);
+
+	//handle the program argument
+	while (cursor < arguments.size()) {
+		QString argument = arguments.at(cursor++);
+
+		if (argument.startsWith(idSingleCharacterArgument)) {
+			QStringList listArgumentValue = argument.split(QRegExp(regExpArgumentValue));
+			QString argumentName = listArgumentValue.at(0);
+			QString value = listArgumentValue.length() > 1 ? listArgumentValue.at(1) : NULL;
+
+			// if it is a long option or a short option
+			if (argumentName.startsWith(idLongArgument) || argumentName.length() == 2) {
+				argumentValuePairs_.insert(argumentName, value);
+
+			} else if (value.length() == 0) {
+				for (int i = 1; i < argumentName.length(); i++) {
+					argumentValuePairs_.insert(QString("-").append(argumentName.at(i)), NULL);
+				}
+			} else {
+				// ERROR: This should never happen: combined options with value
+			}
+
+		} else {
+			argumentValuePairs_.insert(argument, NULL);
+		}
+	}
+}
---
+++ source/util/AMAppArgumentParser.h
@@ -0,0 +1,94 @@
+/*
+	The parser to parse the customized application arguments
+	Command-line arguments could be:
+		* program argument: the argument of the program, no prefix and separated by space
+		* single-character argument: starts with hyphen-minus "-" and separated by space
+		* combined single-character argument
+		* long/customized argument: prefixed with double hyphen-minuses "--"
+		* can have no more than one argument value (separated with "=" or ":")
+
+		eg: valid argument: -f, --force, -r, --recursive, -fr (equivalent to -f -r), -force (equivalent to -f -o -r -c -e)
+
+	argument value:
+		* the value of an argument, separated with special character "=" or ":".
+		* combined single-character arguments can't have value
+
+		eg: programName programArgument1 programArgument2, or
+			programName -f programArgument1 -r programArgument2, or
+			programname -f:argumentValue programArgument1 --load=argumentValue
+
+	USAGES:
+		AMAppArgumentParser *argumentParser = new AMAppArgumentParser(QApplication::instance().argv());
+		or:
+		AMAppArgumentParser *argumentParser = new AMAppArgumentParser(QApplication::instance()->arguments());
+
+
+		bool hasArgument = argumentParser->hasArgument("--load");
+		QString argumentValue = argumentParser->getArgumentValue("-l");
+		int argumentType = argumentParser->getArgumentType("--load"); // will return 2 -- SINGLE_CHARACTER_ARGUMENT
+		QStringList arguments = argumentParser->getArgumentsByType(AMAppArgumentParser::PROGRAM_ARGUMENT);
+								 // will return a list of arguments with type 1 -- PROGRAM_ARGUMENT
+*/
+
+#ifndef AMAPPARGUMENTPARSER_H
+#define AMAPPARGUMENTPARSER_H
+
+#include <QStringList>
+#include <QMap>
+#include <QDebug>
+
+class AMAppArgumentParser
+{
+public:
+	enum AMAppArgumentType
+	{
+		PROGRAM_ARGUMENT = 1,
+		SINGLE_CHARACTER_ARGUMENT = 2,
+		LONG_ARGUMENT = 3,
+
+		NOT_EXIST = 255
+	};
+
+	/// Constructor
+	AMAppArgumentParser(char* argv[]) {
+		// convert the char* argv to QStringList
+		QStringList arguments;
+		for (unsigned int i = 0; i <= sizeof(argv)+1; i++) {
+			arguments.append(argv[i]);
+		}
+
+		parseArguments(arguments);
+	}
+
+	AMAppArgumentParser(QStringList arguments) {
+		parseArguments(arguments);
+	}
+
+	/// Destructor
+	virtual ~AMAppArgumentParser() { argumentValuePairs_.clear(); }
+
+	/// check whether an argument is provided
+	bool hasArgument(QString argument);
+
+	/// get the type of a given argument
+	int getArgumentType(QString argument);
+
+	/// get the value of an argument
+	/// return "" if there is no value or the argument doesn't exist
+	QString getArgumentValue(QString argument);
+
+	/// get a list of arguments in a given style/type
+	QStringList getArgumentsByType(AMAppArgumentType argumentType);
+
+	/// debugging <<< print the arguments
+	void printArgumentPairs();
+
+private:
+	QString applicationName_;
+	QMap<QString, QString> argumentValuePairs_;
+
+	/// parse the argument list and store the argument/value pair to argumentValuePairs_
+	void parseArguments(QStringList arguments);
+};
+
+#endif // AMAPPARGUMENTPARSER_H
