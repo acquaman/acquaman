0d4cdc61baec0d2bfa487a6d6b12d4e9c43f7b93
helfrij <jessie.helfrich@gmail.com>
2014-08-24 11:04:14 -0600

    Added getters and convenience functions to BioXASSideMonochromator for available pvs, converted all pvs to either AMReadOnlyPVControl or AMSinglePVControl (previously just AMProcessVariable). Many values are assumed--will need to check that the given conditions for convenience functions are valid.

--- source/beamline/BioXAS/BioXASSideMonochromator.cpp
+++ source/beamline/BioXAS/BioXASSideMonochromator.cpp
@@ -1,32 +1,146 @@
 #include "BioXASSideMonochromator.h"
+#include <QDebug>
 
 BioXASSideMonochromator::BioXASSideMonochromator(QObject *parent) :
     QObject(parent)
 {
-    tankClosed_ = new AMProcessVariable("07ID_MainMono:TankClosed", true, this);
-    brakeOff_ = new AMProcessVariable("07ID_MainMono:BrakeOff", true, this);
-    keyStatus_ = new AMProcessVariable("07ID_MainMono:KeyStatus", true, this);
-    paddleExtracted_ = new AMProcessVariable("07ID_MainMono:PaddleExtracted", true, this);
-    crystalChangePosition_ = new AMProcessVariable("07ID_MainMono:XtalChangePos", true, this);
-    upperSlitClosed_ = new AMProcessVariable("07ID_MainMono:UpperSlitClosed", true, this);
-    lowerSlitClosed_ = new AMProcessVariable("07ID_MainMono:LowerSlitClosed", true, this);
-    slitsClosed_ = new AMProcessVariable("07ID_MainMono:SlitsClosed", true, this);
-    thetaACWLimit_ = new AMProcessVariable("07ID_MainMono:Theta:A:CWLIM", true, this);
-    thetaACCWLimit_ = new AMProcessVariable("07ID_MainMono:Theta:A:CCWLIM", true, this);
-    thetaBCWLimit_ = new AMProcessVariable("07ID_MainMono:Theta:B:CWLIM", true, this);
-    thetaBCCWLimit_ = new AMProcessVariable("07ID_MainMono:Theta:B:CCWLIM", true, this);
-    regionA_ = new AMProcessVariable("07ID_MainMono:Region:A", true, this);
-    regionB_ = new AMProcessVariable("07ID_MainMono:Region:B", true, this);
-    translateALimit_ = new AMProcessVariable("07ID_MainMono:Translate:A:Limit", true, this);
-    translateBLimit_ = new AMProcessVariable("07ID_MainMono:Translate:B:Limit", true, this);
-    translatePermitted_ = new AMProcessVariable("07ID_MainMono:Translate:Permited", true, this);
-    slitsCloseCmd_ = new AMProcessVariable("07ID_MainMono:SlitsCloseCmd", true, this);
-    slitsEnabled_ = new AMProcessVariable("07ID_MainMono:SlitsEnabled", true, this);
+    tankClosed_ = new AMReadOnlyPVControl("TankClosed", "07ID_SideMono:TankClosed", this);
+    brakeOff_ = new AMReadOnlyPVControl("BrakeOff", "07ID_SideMono:BrakeOff", this);
+    keyStatus_ = new AMReadOnlyPVControl("KeyStatus", "07ID_SideMono:KeyStatus", this);
+    paddleExtracted_ = new AMReadOnlyPVControl("PaddleExtracted", "07ID_SideMono:PaddleExtracted", this);
+    crystalChangePosition_ = new AMReadOnlyPVControl("CrystalChangePosition", "07ID_SideMono:XtalChangePos", this);
+    upperSlitClosed_ = new AMReadOnlyPVControl("UpperSlitClosed", "07ID_SideMono:UpperSlitClosed", this);
+    lowerSlitClosed_ = new AMReadOnlyPVControl("LowerSlitClosed", "07ID_SideMono:LowerSlitClosed", this);
+    slitsClosed_ = new AMReadOnlyPVControl("SlitsClosed", "07ID_SideMono:SlitsClosed", this);
+    thetaACWLimit_ = new AMReadOnlyPVControl("ThetaACWLimit", "07ID_SideMono:Theta:A:CWLIM", this);
+    thetaACCWLimit_ = new AMReadOnlyPVControl("ThetaACCWLimit", "07ID_SideMono:Theta:A:CCWLIM", this);
+    thetaBCWLimit_ = new AMReadOnlyPVControl("ThetaBCWLimit", "07ID_SideMono:Theta:B:CWLIM", this);
+    thetaBCCWLimit_ = new AMReadOnlyPVControl("ThetaBCCWLimit", "07ID_SideMono:Theta:B:CCWLIM", this);
+    regionA_ = new AMReadOnlyPVControl("RegionA", "07ID_SideMono:Region:A", this);
+    regionB_ = new AMReadOnlyPVControl("RegionB", "07ID_SideMono:Region:B", this);
+    translateALimit_ = new AMReadOnlyPVControl("TranslateALimit", "07ID_SideMono:Translate:A:Limit", this);
+    translateBLimit_ = new AMReadOnlyPVControl("TranslateBLimit", "07ID_SideMono:Translate:B:Limit", this);
+    translatePermitted_ = new AMReadOnlyPVControl("TranslatePermitted", "07ID_SideMono:Translate:Permited", this);
+    slitsCloseCmd_ = new AMSinglePVControl("SlitsCloseCmd", "07ID_SideMono:SlitsCloseCmd", this);
+    slitsEnabled_ = new AMReadOnlyPVControl("SlitsEnabled", "07ID_SideMono:SlitsEnabled", this);
+
+    connect( tankClosed_, SIGNAL(valueChanged(double)), this, SIGNAL(tankClosedChanged(double)) );
+    connect( brakeOff_, SIGNAL(valueChanged(double)), this, SIGNAL(brakeOffChanged(double)) );
+    connect( keyStatus_, SIGNAL(valueChanged(double)), this, SIGNAL(keyStatusChanged(double)) );
+    connect( paddleExtracted_, SIGNAL(valueChanged(double)), this, SIGNAL(paddleExtractedChanged(double)) );
 }
 
 BioXASSideMonochromator::~BioXASSideMonochromator()
 {
 
+}
+
+bool BioXASSideMonochromator::tankClosed() const
+{
+    return (tankClosed_->value() == 0);
+}
+
+bool BioXASSideMonochromator::brakeOff() const
+{
+    return (brakeOff_->value() == 0);
+}
+
+bool BioXASSideMonochromator::keyStatus() const
+{
+    return (keyStatus_->value() == 0);
+}
+
+bool BioXASSideMonochromator::paddleExtracted() const
+{
+    return (paddleExtracted_->value() == 0);
+}
+
+bool BioXASSideMonochromator::crystalChangePosition() const
+{
+    return (crystalChangePosition_->value() == 0);
+}
+
+bool BioXASSideMonochromator::upperSlitClosed() const
+{
+    return (upperSlitClosed_->value() == 0);
+}
+
+bool BioXASSideMonochromator::lowerSlitClosed() const
+{
+    return (lowerSlitClosed_->value() == 0);
+}
+
+bool BioXASSideMonochromator::slitsClosed() const
+{
+    return (slitsClosed_->value() == 0);
+}
+
+bool BioXASSideMonochromator::thetaACWLimit() const
+{
+    return (thetaACWLimit_->value() == 0);
+}
+
+bool BioXASSideMonochromator::thetaACCWLimit() const
+{
+    return (thetaACCWLimit_->value() == 0);
+}
+
+bool BioXASSideMonochromator::thetaBCWLimit() const
+{
+    return (thetaBCWLimit_->value() == 0);
+}
+
+bool BioXASSideMonochromator::thetaBCCWLimit() const
+{
+    return (thetaBCCWLimit_->value() == 0);
+}
+
+bool BioXASSideMonochromator::regionA() const
+{
+    return (regionA_->value() == 0);
+}
+
+bool BioXASSideMonochromator::regionB() const
+{
+    return (regionB_->value() == 0);
+}
+
+bool BioXASSideMonochromator::translateALimit() const
+{
+    return (translateALimit_->value() == 0);
+}
+
+bool BioXASSideMonochromator::translateBLimit() const
+{
+    return (translateBLimit_->value() == 0);
+}
+
+bool BioXASSideMonochromator::translatePermitted() const
+{
+    return (translatePermitted_->value() == 0);
+}
+
+bool BioXASSideMonochromator::thetaPaddlePermitted() const
+{
+    return (thetaPaddlePermitted_->value() == 0);
+}
+
+bool BioXASSideMonochromator::slitsEnabled() const
+{
+    return (slitsEnabled_->value() == 0);
+}
+
+AMAction3* BioXASSideMonochromator::createCloseSlitsAction()
+{
+    if (!slitsCloseCmd_->isConnected())
+        return 0;
+
+    AMControlInfo setpoint = slitsCloseCmd_->toInfo();
+    setpoint.setValue(0);
+    AMControlMoveActionInfo3 *actionInfo = new AMControlMoveActionInfo3(setpoint);
+    AMAction3 *action = new AMControlMoveAction3(actionInfo, slitsCloseCmd_);
+
+    return action;
 }
 
 
--- source/beamline/BioXAS/BioXASSideMonochromator.h
+++ source/beamline/BioXAS/BioXASSideMonochromator.h
@@ -5,7 +5,8 @@
 
 #include "beamline/AMControl.h"
 #include "beamline/AMPVControl.h"
-#include "beamline/CLS/CLSBiStateControl.h"
+#include "actions3/AMAction3.h"
+#include "actions3/actions/AMControlMoveAction3.h"
 
 class BioXASSideMonochromator : public QObject
 {
@@ -17,27 +18,121 @@ public:
     /// Destructor.
     virtual ~BioXASSideMonochromator();
 
+    /// Returns true if the tank is closed, false otherwise.
+    bool tankClosed() const;
+    /// Returns true if the brake is off, false otherwise.
+    bool brakeOff() const;
+    /// Returns true if the key status is enabled, false if disabled.
+    bool keyStatus() const;
+    /// Returns true if the paddle is extracted, false otherwise.
+    bool paddleExtracted() const;
+    /// Returns true if the mono is in the crystal change position, false otherwise.
+    bool crystalChangePosition() const;
+    /// Returns true if the upper slit is closed, false otherwise.
+    bool upperSlitClosed() const;
+    /// Returns true if the lower slit is closed, false otherwise.
+    bool lowerSlitClosed() const;
+    /// Returns true if both upper and lower slits are closed, false otherwise.
+    bool slitsClosed() const;
+    /// Returns true if the mono is at the theta clockwise limit in region A, false otherwise.
+    bool thetaACWLimit() const;
+    /// Returns true if the mono is at the theta counter-clockwise limit in region A, false otherwise.
+    bool thetaACCWLimit() const;
+    /// Returns true if the mono is at the theta clockwise limit in region B, false otherwise.
+    bool thetaBCWLimit() const;
+    /// Returns true if the mono is at the theta counter-clockwise limit in region B, false otherwise.
+    bool thetaBCCWLimit() const;
+    /// Returns true if the mono is in region A, false otherwise.
+    bool regionA() const;
+    /// Returns true if the mono is in region B, false otherwise.
+    bool regionB() const;
+    /// Returns true if the mono is at the translation limit in region A, false otherwise.
+    bool translateALimit() const;
+    /// Returns true if the mono is at the translation limit in region B, false otherwise.
+    bool translateBLimit() const;
+    /// Returns true if translation is permitted, false otherwise.
+    bool translatePermitted() const;
+    /// Returns true if the theta paddle is permitted, false otherwise.
+    bool thetaPaddlePermitted() const;
+    /// Returns true if the slits are enabled, false otherwise.
+    bool slitsEnabled() const;
+
+    /// Returns the tankClosed control.
+    AMControl* tankClosedControl() const { return tankClosed_; }
+    /// Returns the brakeOff control.
+    AMControl* brakeOffControl() const { return brakeOff_; }
+    /// Returns the keyStatus control.
+    AMControl* keyStatusControl() const { return keyStatus_; }
+    /// Returns the paddleExtracted control.
+    AMControl* paddleExtractedControl() const { return paddleExtracted_; }
+    /// Returns the crystalChangePosition control.
+    AMControl* crystalChangePositionControl() const { return crystalChangePosition_; }
+    /// Returns the upperSlitClosed control.
+    AMControl* upperSlitClosedControl() const { return upperSlitClosed_; }
+    /// Returns the lowerSlitClosed control.
+    AMControl* lowerSlitClosedControl() const { return lowerSlitClosed_; }
+    /// Returns the slitsClosed control.
+    AMControl* slitsClosedControl() const { return slitsClosed_; }
+    /// Returns the thetaACWLimit control.
+    AMControl* thetaACWLimitControl() const { return thetaACWLimit_; }
+    /// Returns the thetaACCWLimit control.
+    AMControl* thetaACCWLimitControl() const { return thetaACCWLimit_; }
+    /// Returns the thetaBCWLimit control.
+    AMControl* thetaBCWLimitControl() const { return thetaBCWLimit_; }
+    /// Returns the thetaBCCWLimit control.
+    AMControl* thetaBCCWLimitControl() const { return thetaBCCWLimit_; }
+    /// Returns the regionA control.
+    AMControl* regionAControl() const { return regionA_; }
+    /// Returns the regionB control.
+    AMControl* regionBControl() const { return regionB_; }
+    /// Returns the translateALimit control.
+    AMControl* translateALimitControl() const { return translateALimit_; }
+    /// Returns the translateBLimit control.
+    AMControl* translateBLimitControl() const { return translateBLimit_; }
+    /// Returns the translatePermitted control.
+    AMControl* translatePermittedControl() const { return translatePermitted_; }
+    /// Returns the thetaPaddlePermitted control.
+    AMControl* thetaPaddlePermittedControl() const { return thetaPaddlePermitted_; }
+    /// Returns the slitsEnabled control.
+    AMControl* slitsEnabledControl() const { return slitsEnabled_; }
+
+    /// Returns a new close slits action, 0 if not connected.
+    AMAction3* createCloseSlitsAction();
+
+    /// Returns a new crystal change action, 0 if not connected.
+    /// Possibly? Would depend on whether an action that waits for a signal exists/is buildable.
+
+signals:
+    /// Notifier that the tankClosed status has changed.
+    void tankClosedChanged(double);
+    /// Notifier that the brakeOff status has changed.
+    void brakeOffChanged(double);
+    /// Notifier that the key status has changed.
+    void keyStatusChanged(double);
+    /// Notifier that the paddle extracted status has changed.
+    void paddleExtractedChanged(double);
+
 protected:
-    AMProcessVariable *tankClosed_;
-    AMProcessVariable *brakeOff_;
-    AMProcessVariable *keyStatus_;
-    AMProcessVariable *paddleExtracted_;
-    AMProcessVariable *crystalChangePosition_;
-    AMProcessVariable *upperSlitClosed_;
-    AMProcessVariable *lowerSlitClosed_;
-    AMProcessVariable *slitsClosed_;
-    AMProcessVariable *thetaACWLimit_;
-    AMProcessVariable *thetaACCWLimit_;
-    AMProcessVariable *thetaBCWLimit_;
-    AMProcessVariable *thetaBCCWLimit_;
-    AMProcessVariable *regionA_;
-    AMProcessVariable *regionB_;
-    AMProcessVariable *translateALimit_;
-    AMProcessVariable *translateBLimit_;
-    AMProcessVariable *translatePermitted_;
-    AMProcessVariable *thetaPaddlePermitted_;
-    AMProcessVariable *slitsCloseCmd_;
-    AMProcessVariable *slitsEnabled_;
+    AMReadOnlyPVControl *tankClosed_;
+    AMReadOnlyPVControl *brakeOff_;
+    AMReadOnlyPVControl *keyStatus_;
+    AMReadOnlyPVControl *paddleExtracted_;
+    AMReadOnlyPVControl *crystalChangePosition_;
+    AMReadOnlyPVControl *upperSlitClosed_;
+    AMReadOnlyPVControl *lowerSlitClosed_;
+    AMReadOnlyPVControl *slitsClosed_;
+    AMReadOnlyPVControl *thetaACWLimit_;
+    AMReadOnlyPVControl *thetaACCWLimit_;
+    AMReadOnlyPVControl *thetaBCWLimit_;
+    AMReadOnlyPVControl *thetaBCCWLimit_;
+    AMReadOnlyPVControl *regionA_;
+    AMReadOnlyPVControl *regionB_;
+    AMReadOnlyPVControl *translateALimit_;
+    AMReadOnlyPVControl *translateBLimit_;
+    AMReadOnlyPVControl *translatePermitted_;
+    AMReadOnlyPVControl *thetaPaddlePermitted_;
+    AMSinglePVControl *slitsCloseCmd_;
+    AMReadOnlyPVControl *slitsEnabled_;
 
 
 };
