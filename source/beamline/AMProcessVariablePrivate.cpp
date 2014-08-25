6a925d7c06fac2fd56632b57f622cf48340a21c2
David Chevrier <david.k.chevrier@gmail.com>
2014-08-25 14:04:00 -0600

    Removed one more section of commented out code from a change made to AMProcessVariablePrivate while helping Jessie a while ago.

--- source/beamline/AMProcessVariablePrivate.cpp
+++ source/beamline/AMProcessVariablePrivate.cpp
@@ -93,7 +93,7 @@ void AMProcessVariableSupport::removePVImplementation(chid c) {
 
 	// grab pv name
 	QString pvName = pvName2Private_.key(onePrivate);
-	pvName2Private_.remove(pvName);
+	pvName2Private_.remove(pvName);me
 }
 
 AMProcessVariablePrivate* AMProcessVariableSupport::getPrivateForPVNameImplementation(const QString &pvName)
