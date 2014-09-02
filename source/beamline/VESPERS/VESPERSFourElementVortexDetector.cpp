f9dc580ae6005496e01a05cc16b1d0a262884ee0
Darren Hunter <darren.hunter@usask.ca>
2014-09-02 14:31:56 -0600

    Altered: - Did some code cleanup inside of IDEASXASScanConfiguration.  Old code conventions and lots of unnecessary white space. - OCD related code convention changes for VESPERSFourElementVortexDetector.

--- source/beamline/VESPERS/VESPERSFourElementVortexDetector.cpp
+++ source/beamline/VESPERS/VESPERSFourElementVortexDetector.cpp
@@ -21,7 +21,8 @@ along with Acquaman.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "VESPERSFourElementVortexDetector.h"
 
- VESPERSFourElementVortexDetector::~VESPERSFourElementVortexDetector(){}
+VESPERSFourElementVortexDetector::~VESPERSFourElementVortexDetector(){}
+
 VESPERSFourElementVortexDetector::VESPERSFourElementVortexDetector(const QString &name, const QString &description, QObject *parent)
 	: AMXRFDetector(name, description, parent)
 {
