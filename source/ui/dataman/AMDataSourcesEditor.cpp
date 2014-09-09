928fac1da238a24e5f132228f0633c0a467dbaa8
Darren Hunter <darren.hunter@usask.ca>
2014-09-09 14:15:54 -0600

    Altered: - Removed commented out code that removed the ability to make a REIXSXESImageInterpolationAB and view.

--- source/ui/dataman/AMDataSourcesEditor.cpp
+++ source/ui/dataman/AMDataSourcesEditor.cpp
@@ -283,7 +283,7 @@ void AMDataSourcesEditor::onAddDataSourceButtonClicked() {
 #include "analysis/AM1DNormalizationAB.h"
 #include "analysis/AM1DCalibrationAB.h"
 #include "analysis/AM3DBinningAB.h"
-//#include "analysis/REIXS/REIXSXESImageInterpolationAB.h"
+#include "analysis/REIXS/REIXSXESImageInterpolationAB.h"
 
 void AMDataSourcesEditor::onNewDataSourceNamed() {
 
@@ -388,10 +388,8 @@ void AMDataSourcesEditor::onNewDataSourceNamed() {
 
 	else if (nameOfAnalysisBlockToBeAdded_ == "Interpolated Curve Correction"){
 
-		/*
 		newAnalysisBlock = new REIXSXESImageInterpolationAB(chName);
 		newAnalysisBlock->setInputDataSources(twoDimDataSources);
-		*/
 	}
 
 
