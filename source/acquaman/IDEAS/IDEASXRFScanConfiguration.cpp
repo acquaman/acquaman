ad58b2dfcf23f1073c23e2aefe6b84cb5ef296f1
Darren Hunter <darren.hunter@usask.ca>
2014-09-19 15:41:23 -0600

    Added: - Hot fix that added the ability to choose between the Ketek and 13- - Added a property for saving the choice to the database. - Added setters and getters to IDEASXRFScanConfiguration. - Added the ability to utilize the choice inside of IDEASXRFScanController.

--- source/acquaman/IDEAS/IDEASXRFScanConfiguration.cpp
+++ source/acquaman/IDEAS/IDEASXRFScanConfiguration.cpp
@@ -32,13 +32,20 @@ IDEASXRFScanConfiguration::IDEASXRFScanConfiguration(AMDetectorInfo detectorInfo
 	scanNumber_ = 0;
 	scanNotes_ = "";
 	positions_.clear();
-
+	fluorescenceDetector_ = IDEASXRFScanConfiguration::None;
 }
 
 IDEASXRFScanConfiguration::IDEASXRFScanConfiguration(QObject *parent)
 		: AMScanConfiguration(parent)
 {
 		setAutoExportEnabled(false);
+}
+
+IDEASXRFScanConfiguration::IDEASXRFScanConfiguration(const IDEASXRFScanConfiguration &original)
+	: AMScanConfiguration(original)
+{
+	fluorescenceDetector_ = original.fluorescenceDetector();
+	xrfDetectorInfo_ = original.detectorInfo();
 }
 
 IDEASXRFScanConfiguration::~IDEASXRFScanConfiguration()
@@ -62,4 +69,15 @@ QString IDEASXRFScanConfiguration::detailedDescription() const
 				return QString("XRF Scan\nDetector: %1").arg(xrfDetectorInfo_.name());
 
 		return QString();
+}
+
+void IDEASXRFScanConfiguration::setFluorescenceDetector(IDEASXRFScanConfiguration::FluorescenceDetector detector)
+{
+	if (fluorescenceDetector_ != detector){
+
+		fluorescenceDetector_ = detector;
+		emit fluorescenceDetectorChanged(fluorescenceDetector_);
+		emit fluorescenceDetectorChanged(int(fluorescenceDetector_));
+		setModified(true);
+	}
 }
