4ecb538c02f16ac155825f9e8d8f4620ba6cbc77
helfrij <jessie.helfrich@gmail.com>
2014-10-09 16:55:19 -0600

    Updated BioXASSideMonoCrystalChangeView to reflect changes in BioXASSideMonochromator, removing the code that would no longer work.

--- source/ui/BioXAS/BioXASSideMonoCrystalChangeView.cpp
+++ source/ui/BioXAS/BioXASSideMonoCrystalChangeView.cpp
@@ -7,14 +7,15 @@ BioXASSideMonoCrystalChangeView::BioXASSideMonoCrystalChangeView(BioXASSideMonoc
 
     mono_ = 0;
 
-    // Create UI.
-
-    QVBoxLayout *layout = new QVBoxLayout();
-    layout->setMargin(0);
-
-
-
-    setLayout(layout);
+    closeSlits_ = 0;
+    removePaddle_ = 0;
+    turnKeyCCW_ = 0;
+    toCrystalChangePosition_ = 0;
+    disableBrake_ = 0;
+    fromCrystalChangePosition_ = 0;
+    enableBrake_ = 0;
+    toNewRegion_ = 0;
+    turnKeyCW_ = 0;
 
     // Current conditions.
 
@@ -57,3 +58,249 @@ void BioXASSideMonoCrystalChangeView::onMonoConnectedChanged()
         // if mono is disconnected...
     }
 }
+
+void BioXASSideMonoCrystalChangeView::createSteps()
+{
+
+}
+
+void BioXASSideMonoCrystalChangeView::createUI()
+{
+
+}
+
+
+
+
+
+
+
+
+
+BioXASSideMonoCrystalChangeStepView::BioXASSideMonoCrystalChangeStepView(BioXASSideMonoCrystalChangeStep *toView, QWidget *parent) :
+    QWidget(parent)
+{
+    // Set variables.
+
+    step_ = 0;
+    isExtended_ = false;
+
+    // Create UI.
+
+    QGridLayout *mainLayout = new QGridLayout();
+
+    stepNumber_ = new QLabel(this);
+    mainLayout->addWidget(stepNumber_, 0, 0);
+
+    stepBasicInstructions_ = new QLabel(this);
+    mainLayout->addWidget(stepBasicInstructions_, 0, 1);
+
+    moreButton_ = new QPushButton("More", this);
+    connect( moreButton_, SIGNAL(clicked()), this, SLOT(onMoreButtonClicked()) );
+    mainLayout->addWidget(moreButton_, 0, 2);
+
+    stepDetailedInstructions_ = new QLabel(this);
+    mainLayout->addWidget(stepDetailedInstructions_, 1, 0, 3, 3);
+    stepDetailedInstructions_->hide();
+
+    setLayout(mainLayout);
+
+    // Current settings.
+
+    setStep(toView);
+}
+
+BioXASSideMonoCrystalChangeStepView::~BioXASSideMonoCrystalChangeStepView()
+{
+
+}
+
+BioXASSideMonoCrystalChangeStep* BioXASSideMonoCrystalChangeStepView::step() const
+{
+    return step_;
+}
+
+bool BioXASSideMonoCrystalChangeStepView::isExtended() const
+{
+    return isExtended_;
+}
+
+void BioXASSideMonoCrystalChangeStepView::setStep(BioXASSideMonoCrystalChangeStep *newStep)
+{
+    if (step_ != newStep) {
+
+        if (step_)
+            disconnect( step_, 0, this, 0 );
+
+        step_ = newStep;
+
+        if (step_) {
+            connect( step_, SIGNAL(actionChanged(AMAction3*)), this, SLOT(onStepActionChanged()) );
+            connect( step_, SIGNAL(stepNumberChanged(int)), this, SLOT(onStepNumberChanged()) );
+            connect( step_, SIGNAL(basicInstructionsChanged(QString)), this, SLOT(onStepBasicInstructionsChanged()) );
+            connect( step_, SIGNAL(detailedInstructionsChanged(QString)), this, SLOT(onStepDetailedInstructionsChanged()) );
+            connect( step_, SIGNAL(stepStarted()), this, SLOT(onStepStarted()) );
+            connect( step_, SIGNAL(stepSucceeded()), this, SLOT(onStepSucceeded()) );
+            connect( step_, SIGNAL(stepFailed()), this, SLOT(onStepFailed()) );
+        }
+
+        emit stepChanged(step_);
+    }
+}
+
+void BioXASSideMonoCrystalChangeStepView::setExtended(bool isExtended)
+{
+    if (isExtended_ != isExtended) {
+
+        isExtended_ = isExtended;
+
+        if (isExtended) {
+            stepDetailedInstructions_->show();
+
+        } else {
+            stepDetailedInstructions_->hide();
+        }
+
+        emit extendedChanged(isExtended_);
+    }
+}
+
+void BioXASSideMonoCrystalChangeStepView::onMoreButtonClicked()
+{
+    setExtended(!isExtended_);
+}
+
+void BioXASSideMonoCrystalChangeStepView::onStepActionChanged()
+{
+
+}
+
+void BioXASSideMonoCrystalChangeStepView::onStepNumberChanged()
+{
+    if (step_) {
+        stepNumber_->setText(QString::number(step_->number()));
+    }
+}
+
+void BioXASSideMonoCrystalChangeStepView::onStepBasicInstructionsChanged()
+{
+    if (step_) {
+        stepBasicInstructions_->setText(step_->basicInstructions());
+    }
+}
+
+void BioXASSideMonoCrystalChangeStepView::onStepDetailedInstructionsChanged()
+{
+    if (step_) {
+        stepDetailedInstructions_->setText(step_->detailedInstructions());
+    }
+}
+
+void BioXASSideMonoCrystalChangeStepView::onStepStarted()
+{
+
+}
+
+void BioXASSideMonoCrystalChangeStepView::onStepSucceeded()
+{
+
+}
+
+void BioXASSideMonoCrystalChangeStepView::onStepFailed()
+{
+
+}
+
+
+
+
+
+
+
+
+
+BioXASSideMonoCrystalChangeStep::BioXASSideMonoCrystalChangeStep(AMAction3 *action, QObject *parent) :
+    QObject(parent)
+{
+    // Set variables.
+
+    action_ = 0;
+    number_ = 0;
+    basicInstructions_ = "";
+    detailedInstructions_ = "";
+
+    // Current settings.
+
+    setAction(action);
+}
+
+BioXASSideMonoCrystalChangeStep::~BioXASSideMonoCrystalChangeStep()
+{
+
+}
+
+AMAction3* BioXASSideMonoCrystalChangeStep::action() const
+{
+    return action_;
+}
+
+int BioXASSideMonoCrystalChangeStep::number() const
+{
+    return number_;
+}
+
+QString BioXASSideMonoCrystalChangeStep::basicInstructions() const
+{
+    return basicInstructions_;
+}
+
+QString BioXASSideMonoCrystalChangeStep::detailedInstructions() const
+{
+    return detailedInstructions_;
+}
+
+void BioXASSideMonoCrystalChangeStep::setAction(AMAction3 *newAction)
+{
+    if (action_ != newAction) {
+
+        if (action_)
+            disconnect( action_, 0, this, 0 );
+
+        action_ = newAction;
+
+        if (action_) {
+            connect( action_, SIGNAL(started()), this, SIGNAL(stepStarted()) );
+            connect( action_, SIGNAL(succeeded()), this, SIGNAL(stepSucceeded()) );
+            connect( action_, SIGNAL(failed()), this, SIGNAL(stepFailed()) );
+        }
+
+        emit actionChanged(action_);
+    }
+}
+
+void BioXASSideMonoCrystalChangeStep::setNumber(int newNumber)
+{
+    if (number_ != newNumber) {
+        number_ = newNumber;
+        emit numberChanged(number_);
+    }
+}
+
+void BioXASSideMonoCrystalChangeStep::setBasicInstructions(const QString &newInstructions)
+{
+    if (basicInstructions_ != newInstructions) {
+        basicInstructions_ = newInstructions;
+        emit basicInstructionsChanged(basicInstructions_);
+    }
+}
+
+void BioXASSideMonoCrystalChangeStep::setDetailedInstructions(const QString &newInstructions)
+{
+    if (detailedInstructions_ != newInstructions) {
+        detailedInstructions_ = newInstructions;
+        emit detailedInstructionsChanged(detailedInstructions_);
+    }
+}
+
+
+
--- source/ui/BioXAS/BioXASSideMonoCrystalChangeView.h
+++ source/ui/BioXAS/BioXASSideMonoCrystalChangeView.h
@@ -3,15 +3,22 @@
 
 #include <QWidget>
 #include <QLayout>
+#include <QLabel>
+#include <QPushButton>
 
 #include "beamline/BioXAS/BioXASSideMonochromator.h"
+
+class BioXASSideMonoCrystalChangeStep;
+class BioXASSideMonoCrystalChangeStepView;
 
 class BioXASSideMonoCrystalChangeView : public QWidget
 {
     Q_OBJECT
 
 public:
+    /// Constructor.
     explicit BioXASSideMonoCrystalChangeView(BioXASSideMonochromator *toView, QWidget *parent = 0);
+    /// Destructor.
     virtual ~BioXASSideMonoCrystalChangeView();
 
     /// Returns the monochromator being viewed.
@@ -30,8 +37,140 @@ protected slots:
     void onMonoConnectedChanged();
 
 protected:
+    /// Creates the action steps from the mono's available actions.
+    void createSteps();
+    /// Creates the UI.
+    void createUI();
+
+protected:
     BioXASSideMonochromator *mono_;
 
+    BioXASSideMonoCrystalChangeStep *closeSlits_;
+    BioXASSideMonoCrystalChangeStep *removePaddle_;
+    BioXASSideMonoCrystalChangeStep *turnKeyCCW_;
+    BioXASSideMonoCrystalChangeStep *toCrystalChangePosition_;
+    BioXASSideMonoCrystalChangeStep *disableBrake_;
+    BioXASSideMonoCrystalChangeStep *fromCrystalChangePosition_;
+    BioXASSideMonoCrystalChangeStep *enableBrake_;
+    BioXASSideMonoCrystalChangeStep *toNewRegion_;
+    BioXASSideMonoCrystalChangeStep *turnKeyCW_;
+
+    QList<BioXASSideMonoCrystalChangeStep*> steps_;
+
+};
+
+
+
+
+class BioXASSideMonoCrystalChangeStepView : public QWidget
+{
+    Q_OBJECT
+
+public:
+    /// Constructor.
+    explicit BioXASSideMonoCrystalChangeStepView(BioXASSideMonoCrystalChangeStep *toView, QWidget *parent = 0);
+    /// Destructor.
+    virtual ~BioXASSideMonoCrystalChangeStepView();
+
+    /// Returns the step being viewed.
+    BioXASSideMonoCrystalChangeStep* step() const;
+    /// Returns whether the step is extended.
+    bool isExtended() const;
+
+signals:
+    /// Notifier that the step in this view has changed.
+    void stepChanged(BioXASSideMonoCrystalChangeStep *newStep);
+    /// Notifier that the view is/isn't now extended.
+    void extendedChanged(bool isExtended);
+
+public slots:
+    /// Sets the step being viewed.
+    void setStep(BioXASSideMonoCrystalChangeStep* newStep);
+    /// Sets whether the view is extended.
+    void setExtended(bool isExtended);
+
+protected slots:
+    /// Handles toggling view extension when moreButton is clicked.
+    void onMoreButtonClicked();
+    /// Handles changes in the step action.
+    void onStepActionChanged();
+    /// Handles changes in step number.
+    void onStepNumberChanged();
+    /// Handles changes in the step basic instructions.
+    void onStepBasicInstructionsChanged();
+    /// Handles changes in the step detailed instructions.
+    void onStepDetailedInstructionsChanged();
+    /// Handles view chanes for when the step has started.
+    void onStepStarted();
+    /// Handles view changes for when the step has succeeded.
+    void onStepSucceeded();
+    /// Handles view changes for when the step has failed.
+    void onStepFailed();
+
+protected:
+    BioXASSideMonoCrystalChangeStep* step_;
+    QLabel *stepNumber_;
+    QLabel *stepBasicInstructions_;
+    QPushButton *moreButton_;
+    QLabel *stepDetailedInstructions_;
+
+    bool isExtended_;
+};
+
+
+
+
+class BioXASSideMonoCrystalChangeStep : public QObject
+{
+    Q_OBJECT
+
+public:
+    /// Constructor.
+    explicit BioXASSideMonoCrystalChangeStep(AMAction3 *action, QObject *parent = 0);
+    /// Destructor.
+    virtual ~BioXASSideMonoCrystalChangeStep();
+
+    /// Returns the AMAction3 of the step in the crystal change process.
+    AMAction3* action() const;
+    /// Returns the step number.
+    int number() const;
+    /// Returns the basic instructions for this step.
+    QString basicInstructions() const;
+    /// Returns the detailed instructions for this step.
+    QString detailedInstructions() const;
+
+
+signals:
+    /// Notifier that the action for this step has changed.
+    void actionChanged(AMAction3* newAction);
+    /// Notifier that the step number has changed.
+    void numberChanged(int newNumber);
+    /// Notifier that the basic instructions for this step have changed.
+    void basicInstructionsChanged(const QString &newInstructions);
+    /// Notifier that the detailed instructions for this step have changed.
+    void detailedInstructionsChanged(const QString &newInstructions);
+    /// Notifier that the action for this step has started.
+    void stepStarted();
+    /// Notifier that the action for this step has succeeded.
+    void stepSucceeded();
+    /// Notifier that the action for this step has failed.
+    void stepFailed();
+
+public slots:
+    /// Sets the action for this step.
+    void setAction(AMAction3* newAction);
+    /// Sets the step number for this step.
+    void setNumber(int newNumber);
+    /// Sets the basic instructions for this step.
+    void setBasicInstructions(const QString &newInstructions);
+    /// Sets the detailed instructions for this step.
+    void setDetailedInstructions(const QString &newInstructions);
+
+protected:
+    AMAction3* action_;
+    int number_;
+    QString basicInstructions_;
+    QString detailedInstructions_;
 
 };
 
