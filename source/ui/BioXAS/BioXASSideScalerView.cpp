ea3f2590e5c32ce34bd60cde60ebb070bd3deebb
helfrij <jessie.helfrich@gmail.com>
2014-10-23 13:02:21 -0600

    Added creating basic scaler channel views and setting layout to the constructor of BioXASSideScalerView. Each scaler view is connected to new slots onAmplifierViewModeChanged(...) and onOutputViewModeChanged(...), where all views are updated.

--- source/ui/BioXAS/BioXASSideScalerView.cpp
+++ source/ui/BioXAS/BioXASSideScalerView.cpp
@@ -3,10 +3,47 @@
 BioXASSideScalerView::BioXASSideScalerView(QWidget *parent) :
     QWidget(parent)
 {
+    CLSSIS3820Scaler *scaler = BioXASSideBeamline::bioXAS()->scaler();
 
+    QVBoxLayout *layout = new QVBoxLayout();
+
+    // Create scaler channel views.
+
+    for (int i = 0; i < 2; i++) {
+
+        CLSSIS3820ScalerChannelView *view = new CLSSIS3820ScalerChannelView(scaler->channelAt(i), this);
+        channelViews_ << view;
+
+        view->setFixedHeight(55);
+
+        connect( view, SIGNAL(amplifierViewModeChanged(AMCurrentAmplifierView::ViewMode)), this, SLOT(onAmplifierViewModeChanged(AMCurrentAmplifierView::ViewMode)) );
+        connect( view, SIGNAL(outputViewModeChanged(CLSSIS3820ScalerChannelView::OutputViewMode)), this, SLOT(onOutputViewModeChanged(CLSSIS3820ScalerChannelView::OutputViewMode)) );
+
+        layout->addWidget(view);
+    }
+
+    setLayout(layout);
 }
 
 BioXASSideScalerView::~BioXASSideScalerView()
 {
 
+}
+
+void BioXASSideScalerView::onAmplifierViewModeChanged(AMCurrentAmplifierView::ViewMode newMode)
+{
+    foreach (CLSSIS3820ScalerChannelView *channel, channelViews_) {
+        channel->blockSignals(true);
+        channel->setAmplifierViewMode(newMode);
+        channel->blockSignals(false);
+    }
+}
+
+void BioXASSideScalerView::onOutputViewModeChanged(CLSSIS3820ScalerChannelView::OutputViewMode newMode)
+{
+    foreach (CLSSIS3820ScalerChannelView *channel, channelViews_) {
+        channel->blockSignals(true);
+        channel->setOutputViewMode(newMode);
+        channel->blockSignals(false);
+    }
 }
--- source/ui/BioXAS/BioXASSideScalerView.h
+++ source/ui/BioXAS/BioXASSideScalerView.h
@@ -4,6 +4,8 @@
 #include <QWidget>
 
 #include "beamline/BioXAS/BioXASSideBeamline.h"
+#include "ui/CLS/CLSSIS3820CompositeScalerChannelView.h"
+#include "ui/CLS/CLSSIS3820ScalerView.h"
 
 class BioXASSideScalerView : public QWidget
 {
@@ -19,6 +21,15 @@ signals:
 
 public slots:
 
+protected slots:
+    /// Updates all amplifier views to match the most recent change.
+    void onAmplifierViewModeChanged(AMCurrentAmplifierView::ViewMode newMode);
+    /// Updates all output views to match the most recent change.
+    void onOutputViewModeChanged(CLSSIS3820ScalerChannelView::OutputViewMode newMode);
+
+protected:
+    /// The list of all scaler channel views.
+    QList<CLSSIS3820ScalerChannelView*> channelViews_;
 };
 
 #endif // BIOXASSIDESCALERVIEW_H
