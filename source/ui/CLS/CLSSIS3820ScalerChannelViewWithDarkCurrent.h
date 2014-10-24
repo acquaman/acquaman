ebcbb17d713c8e713851e50869e8c3cbb4ad96a5
helfrij <jessie.helfrich@gmail.com>
2014-10-24 13:56:27 -0600

    Updated comments in header file.

--- source/ui/CLS/CLSSIS3820ScalerChannelViewWithDarkCurrent.h
+++ source/ui/CLS/CLSSIS3820ScalerChannelViewWithDarkCurrent.h
@@ -42,24 +42,22 @@ public:
 signals:
 
 public slots:
-    /// Updates all UI elements.
+    /// Updates all dark current UI elements.
     void updateDarkCurrentView();
 
 protected slots:
-    /// Handles updating the displayed dark current value.
+    /// Updates the displayed dark current value.
     void updateDarkCurrentValue();
-    /// Handles updating the status of the displayed dark current value.
+    /// Updates the status of the displayed dark current value.
     void updateDarkCurrentStatus();
-    /// Handles updating the displayed corrected measurement.
+    /// Updates the displayed corrected measurement.
     void updateDarkCurrentCorrected();
-
-protected:
     /// Sets the text for the dark current value label.
     void setDisplayedDarkCurrentValue(double displayValue);
     /// Sets the text for the corrected measurement label.
     void setDisplayedDarkCurrentCorrected(double displayValue);
 
-private:
+protected:
     /// Label displaying the latest dark current value measurement information. The status of this information (whether or not it's stale) is indicated by label color.
     QLabel *darkCurrentValue_;
     /// Label displaying the latest dark current corrected measurement information.
