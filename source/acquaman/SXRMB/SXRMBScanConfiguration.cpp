99308d7fa37f83283b072702b2cf47258cf12105
Darren Hunter <darren.hunter@usask.ca>
2014-10-28 10:48:39 -0600

    Added: - A basic SXRMBScanConfiguration and SXRMBScanConfigurationDbObject. They have yet to be added to the 2D map scan and the EXAFS scan configurations.

---
+++ source/acquaman/SXRMB/SXRMBScanConfiguration.cpp
@@ -0,0 +1,43 @@
+#include "SXRMBScanConfiguration.h"
+
+SXRMBScanConfiguration::SXRMBScanConfiguration()
+{
+	dbObject_ = new SXRMBScanConfigurationDbObject;
+	timeOffset_ = 0;
+	totalTime_ = 0;
+}
+
+SXRMBScanConfiguration::SXRMBScanConfiguration(const SXRMBScanConfiguration &original)
+{
+	dbObject_ = new SXRMBScanConfigurationDbObject(*original.dbObject());
+	timeOffset_ = original.timeOffset();
+	totalTime_ = original.totalTime();
+}
+
+SXRMBScanConfiguration::~SXRMBScanConfiguration()
+{
+	dbObject_->deleteLater();
+}
+
+double SXRMBScanConfiguration::totalTime(bool recompute)
+{
+	if (recompute)
+		computeTotalTimeImplementation();
+
+	return totalTime_;
+}
+
+QString SXRMBScanConfiguration::regionsOfInterestHeaderString(const QList<AMRegionOfInterest *> &regions) const
+{
+	QString string = "";
+
+	if (!regions.isEmpty()){
+
+		string.append("\nRegions Of Interest\n");
+
+		foreach (AMRegionOfInterest *region, regions)
+			string.append(QString("%1\t%2 eV\t%3 eV\n").arg(region->name()).arg(region->lowerBound()).arg(region->upperBound()));
+	}
+
+	return string;
+}
