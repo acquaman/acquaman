fd9f0b98e4aa1386cec0866ebb8ff54db8fbc85b
helfrij <jessie.helfrich@gmail.com>
2014-11-10 10:09:38 -0600

    Performed a few housekeeping changes to this class. Removed some unnecessary public slots (disableDarkCurrentButton(), enableDarkCurrentButton()) that are unused, renamed the signal darkCurrentButtonClicked(double) to collectDarkCurrentMeasurement(double) to clarify what should happen when this signal is emitted, added keyword virtual to the destructor.

--- source/ui/CLS/CLSDarkCurrentWidget.cpp
+++ source/ui/CLS/CLSDarkCurrentWidget.cpp
@@ -24,15 +24,21 @@ along with Acquaman.  If not, see <http://www.gnu.org/licenses/>.
 CLSDarkCurrentWidget::CLSDarkCurrentWidget(double dwellSeconds, QWidget *parent) :
     QWidget(parent)
 {
-    dwellTime_ = dwellSeconds;
+    dwellTime_ = 0;
+
+    if (dwellSeconds > 0)
+        dwellTime_ = dwellSeconds;
 
     QLabel *widgetTitle = new QLabel("Dark Current Correction");
 
     QLabel *dwellTimeLabel = new QLabel("Dwell time (s): ");
-    dwellTimeEntry_ = new QLineEdit(QString::number(dwellTime_));
+
+    dwellTimeEntry_ = new QDoubleSpinBox(this);
+    dwellTimeEntry_->setMinimum(0);
+    dwellTimeEntry_->setMaximum(100);
+    dwellTimeEntry_->setValue(dwellTime_);
 
     darkCurrentButton_ = new QPushButton("Collect");
-    darkCurrentButton_->setEnabled(true);
     connect( darkCurrentButton_, SIGNAL(clicked()), this, SLOT(onDarkCurrentButtonClicked()) );
 
     QGridLayout *layout = new QGridLayout();
@@ -44,42 +50,22 @@ CLSDarkCurrentWidget::CLSDarkCurrentWidget(double dwellSeconds, QWidget *parent)
     setLayout(layout);
 }
 
-
-
 CLSDarkCurrentWidget::~CLSDarkCurrentWidget()
 {
+
 }
-
-
-
-void CLSDarkCurrentWidget::disableDarkCurrentButton()
-{
-    darkCurrentButton_->setEnabled(false);
-}
-
-
-
-void CLSDarkCurrentWidget::enableDarkCurrentButton()
-{
-    darkCurrentButton_->setEnabled(true);
-}
-
-
 
 void CLSDarkCurrentWidget::setDarkCurrentButtonEnabled(bool isEnabled)
 {
     darkCurrentButton_->setEnabled(isEnabled);
 }
 
-
-
 void CLSDarkCurrentWidget::onDarkCurrentButtonClicked()
 {
     double dwellEntered = dwellTimeEntry_->text().toDouble();
 
-    if (dwellEntered <= 0) {
-        return;
+    if (dwellEntered > 0) {
+        dwellTime_ = dwellEntered;
+        emit collectDarkCurrentMeasurement(dwellTime_);
     }
-
-    emit darkCurrentButtonClicked(dwellTime_ = dwellEntered);
 }
--- source/ui/CLS/CLSDarkCurrentWidget.h
+++ source/ui/CLS/CLSDarkCurrentWidget.h
@@ -23,7 +23,7 @@ along with Acquaman.  If not, see <http://www.gnu.org/licenses/>.
 #define CLSDARKCURRENTWIDGET_H
 
 #include <QWidget>
-#include <QLineEdit>
+#include <QDoubleSpinBox>
 #include <QPushButton>
 #include <QLayout>
 #include <QDebug>
@@ -35,31 +35,28 @@ class CLSDarkCurrentWidget : public QWidget
     Q_OBJECT
 
 public:
-    /// This constructor takes a double value to initialize the dwell time displayed in the dwell time entry field.
+    /// Constructor.
     explicit CLSDarkCurrentWidget(double dwellSeconds, QWidget *parent = 0);
-    ~CLSDarkCurrentWidget();
+    /// Destructor.
+    virtual ~CLSDarkCurrentWidget();
 
 signals:
     /// Signal emitted when the dark current button is clicked, and the dwell time is reasonable.
-    void darkCurrentButtonClicked(double dwellSeconds);
+    void collectDarkCurrentMeasurement(double dwellSeconds);
 
 public slots:
-    /// Disables the dark current button, for when there is a dark current measurement being taken.
-    void disableDarkCurrentButton();
-    /// Enables the dark current button, for when it's possible to take a new dark current measurement.
-    void enableDarkCurrentButton();
-    /// Sets the button to en/disabled, according to the boolean argument.
+    /// Sets the dark current measurement button to en/disabled, according to the boolean argument.
     void setDarkCurrentButtonEnabled(bool isEnabled);
 
 protected slots:
-    /// Checks that the dwell time entered is reasonable, assumes that it is possible to actually take a measurement now. Emits the 'darkCurrentButtonClicked' signal.
+    /// Checks that the dwell time entered is reasonable, assumes that it is possible to actually take a measurement now. Emits the 'collectDarkCurrentMeasurement' signal.
     void onDarkCurrentButtonClicked();
 
 protected:
     /// The desired dwell time (in seconds) for the dark current measurement. Initialized to the dwellSeconds constructor argument.
     double dwellTime_;
     /// The entry widget for updating the desired dwell time.
-    QLineEdit* dwellTimeEntry_;
+    QDoubleSpinBox* dwellTimeEntry_;
     /// Pressing this button should start the process of taking a dark current measurement. Button should be disabled when a measurement isn't possible.
     QPushButton* darkCurrentButton_;
 
