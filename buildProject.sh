901317eebdd973cb180c5eb31ee6cca8e27f2b2d
xuetaoliu-cls <xuetao.liu@lightsource.ca>
2014-12-02 16:50:51 -0600

    Modify buildProject script to support make only option

--- buildProject.sh
+++ buildProject.sh
@@ -23,25 +23,38 @@ function cleanBuild() {
 }
 
 function buildProject() {
-	echo "Build project $1"
+	echo "Build project $2"
 
 	source /home/beamline/tools/qt/startup/startup.qt-4.7.3.sh
-	qmake -r $1
+	if [ $1 -ne 1 ]; then
+		qmake -r $1
+	fi
 	make
 
 	echo
 }
 
+function usage() {
+	echo "Usage:"
+	echo "	buildProject.sh [-c] [-m] [-h] [-p project.pro]"
+	echo 
+	echo "	-h help"
+	echo "	-c make a clean build"
+	echo "	-m make the current project. No Makefile will be updated."
+	echo "	-p make the given project. Makefile will be updated."
+	echo
+}
 
 #### the main process ####
 
 if [ $# -lt 1 ] ; then
-   echo "You mush provide at least one argument. Usage:"
-   echo "buildProject.sh [-c] [-p projectname.pro]"
+   echo "You mush provide at least one argument."
+   usage
    exit 1
 fi
- 
-while getopts ":cp:" opt; do
+
+makeOnly=0
+while getopts ":cmhp:" opt; do
 	case $opt in
 		c)
 			echo "-c was triggered. Make a clean build"
@@ -52,9 +65,19 @@ while getopts ":cp:" opt; do
 			echo "-p was triggered, project name: $OPTARG"
 			project_name=$OPTARG
 			;;
-   	  				
+
+		m)
+			echo "-m was triggered. Make only."
+			makeOnly=1
+			;;
+   	  	
+		h)
+			usage
+			exit 1
+			;;		
  		:)
 			echo "Option -$OPTARG requires an argument."
+			usage
 			exit 1
 			;;
 			
@@ -66,10 +89,10 @@ while getopts ":cp:" opt; do
 	esac
 done 
 
-if [ "$project_name" == "" ]; then
+if [ "$project_name" == "" ] && [ $makeOnly -ne 1 ]; then
 	echo "No project name provided. Stop building."
 	exit 1
 fi
 
-buildProject $project_name
+buildProject $makeOnly $project_name
 
