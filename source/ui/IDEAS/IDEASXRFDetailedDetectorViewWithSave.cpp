0a9922bd596a629db2610354a644f186fae353d5
epengr <david.muir@usask.ca>
2015-01-13 11:01:34 -0600

    MODIFIED IDEASXRFDetailedDetectorViewWithSave::buildDetectorView() to call parent buildDetectorView() to fix crash bug resulting from changes to parent.

--- source/ui/IDEAS/IDEASXRFDetailedDetectorViewWithSave.cpp
+++ source/ui/IDEAS/IDEASXRFDetailedDetectorViewWithSave.cpp
@@ -45,13 +45,13 @@ IDEASXRFDetailedDetectorViewWithSave::~IDEASXRFDetailedDetectorViewWithSave(){}
 void IDEASXRFDetailedDetectorViewWithSave::buildDetectorView()
 {
 	// This is going to extend the base view, so we will simply call it's build method too.
-	AMXRFBaseDetectorView::buildDetectorView();
+	AMXRFDetailedDetectorView::buildDetectorView();
 
-	AMXRFDetailedDetectorView::buildPeriodicTableViewAndElementView();
-	AMXRFDetailedDetectorView::buildShowSpectraButtons();
-	AMXRFDetailedDetectorView::buildPileUpPeakButtons();
-	AMXRFDetailedDetectorView::buildDeadTimeView();
-	AMXRFDetailedDetectorView::buildRegionOfInterestViews();
+//	AMXRFDetailedDetectorView::buildPeriodicTableViewAndElementView();
+//	AMXRFDetailedDetectorView::buildShowSpectraButtons();
+//	AMXRFDetailedDetectorView::buildPileUpPeakButtons();
+//	AMXRFDetailedDetectorView::buildDeadTimeView();
+//	AMXRFDetailedDetectorView::buildRegionOfInterestViews();
 
 	buildScanSaveViews();
 
