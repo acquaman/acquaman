78244f7ab36af07669020b35281eb020390a26c3
helfrij <jessie.helfrich@gmail.com>
2015-01-21 13:47:42 -0600

    A first draft of the control class to be included in the BioXAS Main mono crystal change procedure.

--- source/beamline/BioXAS/BioXASMainMonochromatorCrystalChangeControl.cpp
+++ source/beamline/BioXAS/BioXASMainMonochromatorCrystalChangeControl.cpp
@@ -1,12 +1,75 @@
 #include "BioXASMainMonochromatorCrystalChangeControl.h"
 
-BioXASMainMonochromatorCrystalChangeControl::BioXASMainMonochromatorCrystalChangeControl(QObject *parent) :
+BioXASMainMonochromatorCrystalChangeControl::BioXASMainMonochromatorCrystalChangeControl(BioXASMainMonochromator *mono, QObject *parent) :
     QObject(parent)
 {
+    // Initialize variables.
 
+    mono_ = 0;
+
+    // Current settings.
+
+    setMono(mono);
 }
 
 BioXASMainMonochromatorCrystalChangeControl::~BioXASMainMonochromatorCrystalChangeControl()
 {
 
+}
+
+void BioXASMainMonochromatorCrystalChangeControl::setMono(BioXASMainMonochromator *newMono)
+{
+    if (mono_ != newMono)
+        emit monoChanged(mono_ = newMono);
+}
+
+void BioXASMainMonochromatorCrystalChangeControl::startCrystalChange()
+{
+    if (mono_) {
+        AMListAction3 *crystalChangeAction = qobject_cast<AMListAction3*>(mono_->createCrystalChangeAction());
+
+        if (crystalChangeAction) {
+
+            // connect to action signals.
+            connect( crystalChangeAction, SIGNAL(started()), this, SIGNAL(crystalChangeStarted()) );
+            connect( crystalChangeAction, SIGNAL(currentSubActionChanged(int)), this, SLOT(onCrystalChangeSubActionChanged(int)) );
+            connect( crystalChangeAction, SIGNAL(succeeded()), this, SLOT(onCrystalChangeActionsSucceeded()) );
+            connect( crystalChangeAction, SIGNAL(cancelled()), this, SLOT(onCrystalChangeActionsFailed()) );
+            connect( crystalChangeAction, SIGNAL(destroyed()), this, SLOT(onCrystalChangeActionsFailed()) );
+            connect( crystalChangeAction, SIGNAL(failed()), this, SLOT(onCrystalChangeActionsFailed()) );
+
+            // begin crystal change.
+            crystalChangeAction->start();
+        }
+    }
+}
+
+void BioXASMainMonochromatorCrystalChangeControl::onCrystalChangeSubActionChanged(int actionIndex)
+{
+    AMListAction3 *crystalChangeAction = qobject_cast<AMListAction3*>(sender());
+
+    if (crystalChangeAction) {
+        AMAction3 *currentAction = crystalChangeAction->subActionAt(actionIndex);
+
+        if (currentAction)
+            emit currentActionChanged(currentAction->info()->shortDescription(), currentAction->info()->longDescription());
+    }
+}
+
+void BioXASMainMonochromatorCrystalChangeControl::onCrystalChangeActionsSucceeded()
+{
+    deleteAction(sender());
+    emit crystalChangeEnded(true);
+}
+
+void BioXASMainMonochromatorCrystalChangeControl::onCrystalChangeActionsFailed()
+{
+    deleteAction(sender());
+    emit crystalChangeEnded(false);
+}
+
+void BioXASMainMonochromatorCrystalChangeControl::deleteAction(QObject *listAction)
+{
+    disconnect( listAction, 0, this, 0 );
+    listAction->deleteLater();
 }
--- source/beamline/BioXAS/BioXASMainMonochromatorCrystalChangeControl.h
+++ source/beamline/BioXAS/BioXASMainMonochromatorCrystalChangeControl.h
@@ -3,19 +3,51 @@
 
 #include <QObject>
 
+#include "BioXASMainMonochromator.h"
+
 class BioXASMainMonochromatorCrystalChangeControl : public QObject
 {
     Q_OBJECT
 
 public:
     /// Constructor.
-    explicit BioXASMainMonochromatorCrystalChangeControl(QObject *parent = 0);
+    explicit BioXASMainMonochromatorCrystalChangeControl(BioXASMainMonochromator *mono, QObject *parent = 0);
     /// Destructor.
     virtual ~BioXASMainMonochromatorCrystalChangeControl();
+    /// Returns the monochromator being controlled.
+    BioXASMainMonochromator* mono() const { return mono_; }
 
 signals:
+    /// Notifier that the mono being controlled has changed.
+    void monoChanged(BioXASMainMonochromator *newMono);
+    /// Notifier that the crystal change actions have started.
+    void crystalChangeStarted();
+    /// Notifier that the currently running action has changed, and so has the corresponding description and user information.
+    void currentActionChanged(const QString &newDescription, const QString &newInformation);
+    /// Notifier that the crystal change actions have ended. Argument is true for success, false for fail.
+    void crystalChangeEnded(bool success);
 
 public slots:
+    /// Sets the monochromator.
+    void setMono(BioXASMainMonochromator *newMono);
+    /// Starts the crystal change, if current mono is valid.
+    void startCrystalChange();
+
+protected slots:
+    /// Handles when the currently executing subaction has changed.
+    void onCrystalChangeSubActionChanged(int actionIndex);
+    /// Handles when the crystal change actions have succeeded.
+    void onCrystalChangeActionsSucceeded();
+    /// Handles situation where crystal change actions report themselves cancelled.
+    void onCrystalChangeActionsFailed();
+
+protected:
+    /// The monochromator being controlled.
+    BioXASMainMonochromator *mono_;
+
+private:
+    /// Disconnects from the given action and marks it for deletion.
+    void deleteAction(QObject *listAction);
 
 };
 
