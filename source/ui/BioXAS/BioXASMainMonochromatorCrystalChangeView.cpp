a2b8e51925de10ad26268b6a9aa20d2a9043a11a
helfrij <jessie.helfrich@gmail.com>
2015-02-02 13:11:38 -0600

    Getting started creating a super basic mono view specific to crystal changes. So far, doesn't really do a whole lot, just assembling some basic elements that might be useful later.

--- source/ui/BioXAS/BioXASMainMonochromatorCrystalChangeView.cpp
+++ source/ui/BioXAS/BioXASMainMonochromatorCrystalChangeView.cpp
@@ -10,7 +10,59 @@ BioXASMainMonochromatorCrystalChangeView::BioXASMainMonochromatorCrystalChangeVi
 
     // Create UI elements.
 
-    //     The future home of all the mono crystal change feedback elements.
+    QLabel *slitsClosedPrompt = new QLabel("Slits status:");
+    slitsClosed_ = new QLabel();
+
+    QLabel *paddleOutPrompt = new QLabel("Paddle status:");
+    paddleOut_ = new QLabel();
+
+    QLabel *keyStatusPrompt = new QLabel("Key status:");
+    keyStatus_ = new QLabel();
+
+    QLabel *brakeStatusPrompt = new QLabel("Brake status:");
+    brakeStatus_ = new QLabel();
+
+    braggMotorPosition_ = new AMExtendedControlEditor(0, 0, true);
+    braggMotorPosition_->overrideTitle("Bragg motor position");
+//    braggMotorPosition_->setReadOnly(true);
+//    braggMotorPosition_->setControlFormat('f', 2);
+    braggMotorPosition_->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Preferred);
+
+    crystalChangeMotorPosition_ = new AMExtendedControlEditor(0, 0, true);
+    crystalChangeMotorPosition_->overrideTitle("Crystal change motor position");
+//    crystalChangeMotorPosition_->setReadOnly(true);
+//    crystalChangeMotorPosition_->setControlFormat('f', 2);
+    crystalChangeMotorPosition_->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Preferred);
+
+    QGroupBox *monoControls = new QGroupBox("Monochromator Controls");
+
+    // Create and set layouts.
+
+    QGridLayout *statusLayout = new QGridLayout();
+    statusLayout->setMargin(0);
+    statusLayout->addWidget(slitsClosedPrompt, 0, 0);
+    statusLayout->addWidget(slitsClosed_, 0, 1);
+    statusLayout->addWidget(paddleOutPrompt, 1, 0);
+    statusLayout->addWidget(paddleOut_, 1, 1);
+    statusLayout->addWidget(keyStatusPrompt, 2, 0);
+    statusLayout->addWidget(keyStatus_, 2, 1);
+    statusLayout->addWidget(brakeStatusPrompt, 3, 0);
+    statusLayout->addWidget(brakeStatus_, 3, 1);
+
+    QVBoxLayout *motorsLayout = new QVBoxLayout();
+    motorsLayout->setMargin(0);
+    motorsLayout->addWidget(braggMotorPosition_);
+    motorsLayout->addWidget(crystalChangeMotorPosition_);
+
+    QVBoxLayout *monoControlsLayout = new QVBoxLayout();
+    monoControlsLayout->addLayout(statusLayout);
+    monoControlsLayout->addLayout(motorsLayout);
+    monoControls->setLayout(monoControlsLayout);
+
+    QVBoxLayout *layout = new QVBoxLayout();
+    layout->addWidget(monoControls);
+
+    setLayout(layout);
 
     // Current settings.
 
--- source/ui/BioXAS/BioXASMainMonochromatorCrystalChangeView.h
+++ source/ui/BioXAS/BioXASMainMonochromatorCrystalChangeView.h
@@ -7,6 +7,8 @@
 #include <QProgressBar>
 #include <QMessageBox>
 
+#include "ui/beamline/AMExtendedControlEditor.h"
+
 #include "beamline/BioXAS/BioXASMainMonochromator.h"
 
 class BioXASMainMonochromatorCrystalChangeView : public QWidget
@@ -30,6 +32,24 @@ public slots:
 protected:
     /// The monochromator being viewed.
     BioXASMainMonochromator *mono_;
+    /// Label contents indicate whether the mono slits are closed.
+    QLabel *slitsClosed_;
+    /// Label contents indicate whether the mono phosphor paddle is out.
+    QLabel *paddleOut_;
+    /// Label contents indicate whether the mono key is turned to 'Enabled'.
+    QLabel *keyStatus_;
+    /// Control view shows the position of the mono bragg motor.
+    AMExtendedControlEditor *braggMotorPosition_;
+    /// Label contents indicate whether the bragg motor is at the crystal change position.
+    QLabel *braggMotorAtCrystalChangePosition_;
+    /// Label contents indicate whether the mono brake is enabled.
+    QLabel *brakeStatus_;
+    /// Control view shows the position of the mono crystal change motor.
+    AMExtendedControlEditor *crystalChangeMotorPosition_;
+    /// Label contents indicate whether the mono crystal change motor is at a CW limit.
+    QLabel *crystalChangeMotorCWLimit_;
+    /// Label contents indicate whether the mono crystal change motor is at a CCW limit.
+    QLabel *crystalChangeMotorCCWLimit_;
 };
 
 
