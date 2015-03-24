f0b03d3d27d589c152101734ea88c6b1dde5223e
helfrij <jessie.helfrich@gmail.com>
2015-03-24 14:39:05 -0600

    Missed removing the energy calibration slot in an earlier commit e214b5d. Removed now. #1263

--- source/ui/BioXAS/BioXASSidePersistentView.cpp
+++ source/ui/BioXAS/BioXASSidePersistentView.cpp
@@ -54,13 +54,3 @@ BioXASSidePersistentView::~BioXASSidePersistentView()
 {
 
 }
-
-void BioXASSidePersistentView::onCalibrateEnergyButtonClicked()
-{
-	bool inputOK = false;
-	double newEnergy = QInputDialog::getDouble(this, "Monochromator Energy Calibration", "Enter current calibrated energy:", BioXASSideBeamline::bioXAS()->mono()->energyControl()->value(), -10000000, 10000000, 1, &inputOK, Qt::Sheet);
-
-	if (inputOK) {
-		BioXASSideBeamline::bioXAS()->mono()->setEnergyCalibration(newEnergy);
-	}
-}
