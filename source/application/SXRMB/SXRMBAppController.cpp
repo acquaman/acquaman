f5941508c56d1b54f4df66093c4ab23955ab104f
Sheldon Liu <xuetao.liu@lightsource.ca>
2015-05-05 11:22:24 -0600

    make the ambiant sample stage motor group view raisable

--- source/application/SXRMB/SXRMBAppController.cpp
+++ source/application/SXRMB/SXRMBAppController.cpp
@@ -490,11 +490,15 @@ void SXRMBAppController::onShowAmbiantSampleStageMotorsTriggered()
 {
 	QString motorGroupName = SXRMBBeamline::sxrmb()->ambiantWithoutGasChamberSampleStageMotorGroupObject()->name();
 
-	AMMotorGroupView *motorGroupView = new AMMotorGroupView(SXRMBBeamline::sxrmb()->motorGroup(), AMMotorGroupView::Exclusive);
-	motorGroupView->setMotorGroupView(motorGroupName);
-	motorGroupView->showAvailableMotorGroupChoices(false);
+	if (!ambiantSampleStageMotorGroupView) {
+		ambiantSampleStageMotorGroupView = new AMMotorGroupView(SXRMBBeamline::sxrmb()->motorGroup(), AMMotorGroupView::Exclusive);
+		ambiantSampleStageMotorGroupView->setMotorGroupView(motorGroupName);
+		ambiantSampleStageMotorGroupView->showAvailableMotorGroupChoices(false);
+	}
 
-	motorGroupView->show();
+	ambiantSampleStageMotorGroupView->raise();
+	ambiantSampleStageMotorGroupView->activateWindow();
+	ambiantSampleStageMotorGroupView->showNormal();
 }
 
 void SXRMBAppController::onSwitchBeamlineEndstationTriggered()
