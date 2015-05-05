d3681befb5d49e5b1b88ec0a2d611be63180a857
xuetaoliu-cls <xuetao.liu@lightsource.ca>
2015-05-05 16:02:48 -0600

    First stab to add the iteration box for scan action holder

--- source/ui/actions3/AMActionRunnerAddActionBar3.h
+++ source/ui/actions3/AMActionRunnerAddActionBar3.h
@@ -23,20 +23,47 @@ along with Acquaman.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <QWidget>
 
-class AMAction3;
+class QSpinBox;
 class QPushButton;
 class QLabel;
 class QRadioButton;
 class QVBoxLayout;
+
+class AMAction3;
 
 /// This widget provides a common, standard set of buttons to show below a typical "action configuration" panel. The buttons let the user run the action immediately (if AMActionRunner isn't already running an action), or add it to the action runner's queue. The buttons are automatically enabled/disabled based on the state of the action runner.
 /*! To use this class, you need to re-implement the virtual protected function createAction() to provide the action that should be run/added to the queue.
 
   This widget leaves room in its layout for any kind action configuration widget to be added directly above it... Call addWidget() to insert that widget.*/
 
+/// the definitions of the Action Queue
+class ActionQueue {
+public:
+	/// Queue operations
+	enum QueueOperation {
+		RunAtOnce = 0,
+		AddToBeginningAndStart,
+		AddToEndAndStart,
+		AddToEnd,
+		Cancel
+	};
+
+	/// whether we need to insert tasks to the beginning of the queue
+	static bool insertTaskToBeginning(const QueueOperation operation) {
+		return operation == RunAtOnce || operation == AddToBeginningAndStart;
+	}
+
+	/// whether we need to start the queue
+	static bool startQueue(const QueueOperation operation) {
+		return operation == RunAtOnce || operation == AddToBeginningAndStart || operation == AddToEndAndStart;
+	}
+};
+
 class AMActionRunnerAddActionBar3 : public QWidget
 {
 	Q_OBJECT
+
+
 public:
 	/// Constructor.  If the main "Start ____" should say something other than "Start Action", you can specify that name here.  (ex: "Scan", "Move", etc.)
  	virtual ~AMActionRunnerAddActionBar3();
@@ -62,8 +89,12 @@ protected:
 	/// This function is called to get the action that should be added to the queue. The base class implementation returns 0 for no valid action, so it must be re-implemented.
 	virtual AMAction3* createAction() { return 0; }
 
+	/// create and add actions to the queue
+	void addActionToQueue(ActionQueue::QueueOperation operation);
+
 	/// UI elements
 	QLabel *whenDoneLabel_;
+	QSpinBox *iterationsBox_;
 	QPushButton* startActionButton_, *addToQueueButton_;
 	QRadioButton* goToWorkflowOption_, *setupAnotherActionOption_;
 	QVBoxLayout* layout_;
