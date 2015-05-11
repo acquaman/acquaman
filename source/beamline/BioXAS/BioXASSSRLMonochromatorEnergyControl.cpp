abc1c59a454e27550a584f84c75143b0a50d0fd7
helfrij <jessie.helfrich@gmail.com>
2015-05-11 17:41:05 -0600

    Started work on adapting the mono energy controls for the SSRL monochromators to the pseudomotor regimen. I've mostly scrapped what was existing before, as the pvs used previously were specific to the energy pv application and the pseudomotor subclass should shoulder these calculations eventually. Work is incomplete, BioXASSide doesn't build successfully.

--- source/beamline/BioXAS/BioXASSSRLMonochromatorEnergyControl.cpp
+++ source/beamline/BioXAS/BioXASSSRLMonochromatorEnergyControl.cpp
@@ -1,9 +1,20 @@
 #include "BioXASSSRLMonochromatorEnergyControl.h"
+#include "beamline/BioXAS/BioXASSSRLMonochromator.h"
+#include <math.h>
 
-BioXASSSRLMonochromatorEnergyControl::BioXASSSRLMonochromatorEnergyControl(const QString &name, const QString &readPVname, const QString &writePVname, const QString &movingPVname, const QString &stopPVname, QObject *parent, double tolerance, double moveStartTimeoutSeconds, AMAbstractControlStatusChecker *statusChecker, int stopValue, const QString &description) :
-    AMPVwStatusControl(name, readPVname, writePVname, movingPVname, stopPVname, parent, tolerance, moveStartTimeoutSeconds, statusChecker, stopValue, description)
+BioXASSSRLMonochromatorEnergyControl::BioXASSSRLMonochromatorEnergyControl(const QString &name, QObject *parent) :
+	AMPseudoMotorControl(name, "eV", parent)
 {
+	// Initialize member variables.
 
+	hc_ = 12398.42;
+	crystal2D_ = 3.8403117;
+	energyOffsetDeg_ = 0;
+	m1AngleOffset_ = -0.4;
+	thetaBraggOffset_ = 180.0;
+
+	bragg_ = 0;
+	region_ = 0;
 }
 
 BioXASSSRLMonochromatorEnergyControl::~BioXASSSRLMonochromatorEnergyControl()
@@ -11,17 +22,62 @@ BioXASSSRLMonochromatorEnergyControl::~BioXASSSRLMonochromatorEnergyControl()
 
 }
 
-bool BioXASSSRLMonochromatorEnergyControl::isConnected() const
+bool BioXASSSRLMonochromatorEnergyControl::canMeasure()
 {
-	bool connected = (
-				AMPVwStatusControl::isConnected() &&
-				hc_->isConnected() &&
-				crystal2D_->isConnected() &&
-				braggAngle_->isConnected() &&
-				angleOffset_->isConnected()
-				);
+	bool result = false;
 
-	return connected;
+	if (isConnected())
+		result = ( bragg_->canMeasure() && region_->canMeasure() );
+
+	return result;
+}
+
+bool BioXASSSRLMonochromatorEnergyControl::canMove()
+{
+	bool result = false;
+
+	if (isConnected())
+		result = ( bragg_->canMove() );
+
+	return result;
+}
+
+bool BioXASSSRLMonochromatorEnergyControl::canStop()
+{
+	bool result = false;
+
+	if (isConnected())
+		result = ( bragg_->canStop() );
+
+	return false;
+}
+
+void BioXASSSRLMonochromatorEnergyControl::setBraggControl(AMControl *newControl)
+{
+	if (bragg_ != newControl) {
+
+		if (bragg_)
+			removeChildControl(bragg_);
+
+		bragg_ = newControl;
+
+		if (bragg_)
+			addChildControl(bragg_);
+	}
+}
+
+void BioXASSSRLMonochromatorEnergyControl::setRegionControl(AMControl *newControl)
+{
+	if (region_ != newControl) {
+
+		if (region_)
+			removeChildControl(region_);
+
+		region_ = newControl;
+
+		if (region_)
+			addChildControl(region_);
+	}
 }
 
 void BioXASSSRLMonochromatorEnergyControl::setEnergyCalibration(double newEnergy)
@@ -31,39 +87,95 @@ void BioXASSSRLMonochromatorEnergyControl::setEnergyCalibration(double newEnergy
 		double newOffset = calibrateEnergy(value(), newEnergy);
 
 		// Set the new angle offset.
-		angleOffset_->move(newOffset);
+		setDeltaTheta(newOffset);
+
+		// Update the control value.
+		updateValue();
 	}
 }
 
-AMAction3* BioXASSSRLMonochromatorEnergyControl::createSetEnergyCalibrationAction(double newEnergy)
+void BioXASSSRLMonochromatorEnergyControl::setDeltaTheta(double newAngle)
 {
-	AMAction3 *action = 0;
-
-	if (isConnected()) {
-		// Calculate needed offset.
-		double newOffset = calibrateEnergy(value(), newEnergy);
-
-		// Set the new angle offset.
-		action = AMActionSupport::buildControlMoveAction(angleOffset_, newOffset, false);
+	if (deltaTheta_ != newAngle) {
+		deltaTheta_ = newAngle;
+		emit deltaThetaChanged(deltaTheta_);
 	}
+}
 
-	return action;
+void BioXASSSRLMonochromatorEnergyControl::updateConnected()
+{
+	bool isConnected = (
+				bragg_ && bragg_->isConnected() &&
+				region_ && region_->isConnected()
+				);
+
+	setConnected(isConnected);
+}
+
+void BioXASSSRLMonochromatorEnergyControl::updateValue()
+{
+	if (isConnected()) {
+		setValue( calculateEnergy(hc_, crystal2D_, deltaTheta_, bragg_->value(), region_->value(), m1AngleOffset_, thetaBraggOffset_));
+	}
+}
+
+void BioXASSSRLMonochromatorEnergyControl::updateIsMoving()
+{
+	if (isConnected()) {
+		setIsMoving( bragg_->isMoving() || region_->isMoving() );
+	}
+}
+
+AMAction3* BioXASSSRLMonochromatorEnergyControl::createMoveAction(double setpoint)
+{
+	return 0;
 }
 
 double BioXASSSRLMonochromatorEnergyControl::calibrateEnergy(double oldEnergy, double newEnergy) const
 {
 	// Gather pre-calibration information.
-	double oldOffset = angleOffset_->value();
-	double hc = hc_->value();
-	double crystal2D = crystal2D_->value();
-	double braggAngle = braggAngle_->value();
+	double oldOffset = deltaTheta_;
+	double hc = hc_;
+	double crystal2D = crystal2D_;
+	double braggPosition = bragg_->value();
 
 	// Calculate changes needed for calibration.
 	double deltaEnergy = newEnergy - oldEnergy;
-	double deltaOffset = hc / (crystal2D * oldEnergy * oldEnergy * cos(braggAngle * M_PI / 180)) * deltaEnergy * 180 / M_PI;
+	double deltaOffset = hc / (crystal2D * oldEnergy * oldEnergy * cos(braggPosition * M_PI / 180)) * deltaEnergy * 180 / M_PI;
 
 	// Calibration results.
 	double newOffset = oldOffset + deltaOffset;
 
 	return newOffset;
+}
+
+double BioXASSSRLMonochromatorEnergyControl::calculateBraggAngleRegionA(double deltaTheta, double braggPosition, double m1AngleOffset, double thetaBraggOffset)
+{
+	return (calculateBraggAngleRegionB(deltaTheta, braggPosition, m1AngleOffset, thetaBraggOffset) - 180);
+}
+
+double BioXASSSRLMonochromatorEnergyControl::calculateBraggAngleRegionB(double deltaTheta, double braggPosition, double m1AngleOffset, double thetaBraggOffset)
+{
+	return (thetaBraggOffset - braggPosition + m1AngleOffset + deltaTheta);
+}
+
+double BioXASSSRLMonochromatorEnergyControl::calculateEnergy(double hc, double crystal2D, double deltaTheta, double braggPosition, double region, double m1AngleOffset, double thetaBraggOffset)
+{
+	double result = 0;
+	double braggAngle = 0;
+	bool validRegion = false;
+
+	if ((int)region == BioXASSSRLMonochromator::Region::A) {
+		braggAngle = calculateBraggAngleRegionA(deltaTheta, braggPosition, m1AngleOffset, thetaBraggOffset);
+		validRegion = true;
+
+	} else if ((int)region == BioXASSSRLMonochromator::Region::B) {
+		braggAngle = calculateBraggAngleRegionB(deltaTheta, braggPosition, m1AngleOffset, thetaBraggOffset);
+		validRegion = true;
+	}
+
+	if (validRegion)
+		result = ( hc / (crystal2D + sin(braggAngle * M_PI / 180)) );
+
+	return result;
 }
--- source/beamline/BioXAS/BioXASSSRLMonochromatorEnergyControl.h
+++ source/beamline/BioXAS/BioXASSSRLMonochromatorEnergyControl.h
@@ -6,64 +6,109 @@
 #include "beamline/AMPVControl.h"
 #include "actions3/AMActionSupport.h"
 
-class BioXASSSRLMonochromatorEnergyControl : public AMPVwStatusControl
+#include "beamline/AMPseudoMotorControl.h"
+
+class BioXASSSRLMonochromatorEnergyControl : public AMPseudoMotorControl
 {
 	Q_OBJECT
+
 public:
-	explicit BioXASSSRLMonochromatorEnergyControl(const QString& name,
-				       const QString& readPVname,
-				       const QString& writePVname,
-				       const QString& movingPVname,
-				       const QString& stopPVname = QString(),
-				       QObject* parent = 0,
-				       double tolerance = AMCONTROL_TOLERANCE_DONT_CARE,
-				       double moveStartTimeoutSeconds = 2.0,
-				       AMAbstractControlStatusChecker* statusChecker = new AMControlStatusCheckerDefault(1),
-				       int stopValue = 1,
-				       const QString &description = "");
+	/// Constructor.
+	explicit BioXASSSRLMonochromatorEnergyControl(const QString &name, QObject *parent = 0);
+	/// Destructor.
 	virtual ~BioXASSSRLMonochromatorEnergyControl();
 
-	/// Returns true if all subcontrols are connected, false otherwise.
-	virtual bool isConnected() const;
+	/// Returns true if the control value is always measureable, provided it is connected. False otherwise.
+	virtual bool shouldMeasure() { return true; }
+	/// Returns true if a control move is always possible, provided it is connected. False otherwise.
+	virtual bool shouldMove() const { return true; }
+	/// Returns true if a control stop is always possible, provided it is connected. False otherwise.
+	virtual bool shouldStop() const { return false; }
+
+	/// Returns true if this control's value can be measured right now. False otherwise.
+	virtual bool canMeasure() const;
+	/// Returns true if this control can move right now. False otherwise.
+	virtual bool canMove() const;
+	/// Returns true if this control can stop right now. False otherwise.
+	virtual bool canStop() const;
 
 	/// Returns the hc constant.
-	double hc() const { return hc_->value(); }
+	double hc() const { return hc_; }
 	/// Returns the crystal 2D spacing.
-	double crystal2D() const { return crystal2D_->value(); }
-	/// Returns the physical bragg angle.
-	double braggAngle() const { return braggAngle_->value(); }
-	/// Returns the bragg angle offset. This value is what's adjusted during energy calibration.
-	double angleOffset() const { return angleOffset_->value(); }
+	double crystal2D() const { return crystal2D_; }
+	/// Returns the delta theta constant (energy calibration offset).
+	double deltaTheta() const { return deltaTheta_; }
+	/// Returns the theta offset to the bragg angle.
+	double thetaBraggOffset() const { return thetaBraggOffset_; }
 
-	/// Returns the hc constant control.
-	virtual AMControl* hcControl() const { return hc_; }
-	/// Returns the crystal 2D control.
-	virtual AMControl* crystal2DControl() const { return crystal2D_; }
-	/// Returns the goniometer bragg angle control.
-	virtual AMControl* braggAngleControl() const { return braggAngle_; }
-	/// Returns the bragg angle offset control. Its value is increased or decreased with energy calibration.
-	virtual AMControl* angleOffsetControl() const { return angleOffset_; }
+	/// Returns the bragg motor control.
+	AMControl* braggControl() const { return bragg_; }
+	/// Returns the region control.
+	AMControl* regionControl() const { return region_; }
 
 	/// Returns a new action that adjusts the angle offset s.t. the mono energy matches the desired energy.
 	AMAction3* createSetEnergyCalibrationAction(double newEnergy);
 
+	/// Returns true if the given value is a valid value for this control. False otherwise.
+	virtual bool validValue(double value) const { Q_UNUSED(value) return true; }
+	/// Returns true if the given value is a valid setpoint for this control. False otherwise.
+	virtual bool validSetpoint(double value) const { return (value > 0); }
+
+signals:
+	/// Notifier that the bragg control has changed.
+	void braggControlChanged(AMControl *newControl);
+	/// Notifier that the region control has changed.
+	void regionControlChanged(AMControl *newControl);
+
 public slots:
-	/// Sets the bragg offset such that the mono energy matches the desired energy.
+	/// Sets the bragg control.
+	void setBraggControl(AMControl *newControl);
+	/// Sets the region control.
+	void setRegionControl(AMControl *newControl);
+
+	/// Sets the delta theta (energy calibration) offset such that the mono energy matches the desired energy.
 	void setEnergyCalibration(double newEnergy);
 
+protected slots:
+	/// Sets the delta theta (energy calibration) offset.
+	void setDeltaTheta(double newAngle);
+
+	/// Updates the connected state.
+	virtual void updateConnected();
+	/// Updates the current value.
+	virtual void updateValue();
+	/// Updates the 'is moving' state.
+	virtual void updateIsMoving();
+
 protected:
+	/// Creates and returns a move action.
+	virtual AMAction3* createMoveAction(double setpoint);
+
 	/// Returns the bragg motor angle offset needed in order for the old energy to match the desired new energy.
 	double calibrateEnergy(double oldEnergy, double newEnergy) const;
 
+	/// Returns the bragg angle calculation result for region A.
+	static double calculateBraggAngleRegionA(double deltaTheta, double braggPosition, double m1AngleOffset, double thetaBraggOffset);
+	/// Returns the bragg angle calculation result for region B.
+	static double calculateBraggAngleRegionB(double deltaTheta, double braggPosition, double m1AngleOffset, double thetaBraggOffset);
+	/// Returns the energy calculation result for the given values. Returns 0 if not in a valid region (can happen during normal operation).
+	static double calculateEnergy(double hc, double crystal2D, double deltaTheta, double braggPosition, double region, double m1AngleOffset, double thetaBraggOffset);
+
 protected:
-	/// The hc constant control.
-	AMReadOnlyPVControl *hc_;
-	/// The crystal 2D control.
-	AMReadOnlyPVControl *crystal2D_;
+	/// The hc constant.
+	double hc_;
+	/// The crystal 2D constant.
+	double crystal2D_;
+	/// The delta theta (energy calibration) offset.
+	double deltaTheta_;
+	/// The m1 angle offset.
+	double m1AngleOffset_;
+	/// The theta offset to the bragg angle.
+	double thetaBraggOffset_;
 	/// The goniometer bragg angle control.
-	AMReadOnlyPVControl *braggAngle_;
-	/// The bragg angle offset control.
-	AMPVControl *angleOffset_;
+	AMControl *bragg_;
+	/// The region control.
+	AMControl *region_;
 };
 
 #endif // BIOXASSSRLMONOCHROMATORENERGYCONTROL_H
