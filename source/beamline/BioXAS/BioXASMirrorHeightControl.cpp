6ed2180a7556abdec552487af79841cce6746b0d
helfrij <jessie.helfrich@gmail.com>
2015-05-26 16:20:19 -0600

    Implemented the calculations for the mirror height control. The calculateHeight() result appears to agree with what the pseudomotor application generates, other calculations are untested.

--- source/beamline/BioXAS/BioXASMirrorHeightControl.cpp
+++ source/beamline/BioXAS/BioXASMirrorHeightControl.cpp
@@ -58,17 +58,42 @@ AMAction3* BioXASMirrorHeightControl::createMoveAction(double setpoint)
 
 double BioXASMirrorHeightControl::calculateUpstreamInboardZ(double height, double upstreamInboardX, double upstreamInboardY, double upstreamOutboardX, double upstreamOutboardY, double upstreamOutboardZ, double downstreamX, double downstreamY, double downstreamZ)
 {
-	return 0;
+	double term1 = ((downstreamZ - upstreamOutboardZ) *(downstreamY - upstreamInboardY));
+	double term2 = ((upstreamInboardX - downstreamX) * (upstreamOutboardY - downstreamY)) + ((upstreamOutboardX - downstreamX) * (downstreamY - upstreamInboardY));
+	double term3 = (downstreamX * (downstreamY - upstreamOutboardY) + downstreamY * (upstreamOutboardX - downstreamX)) / (upstreamOutboardY - downstreamY);
+	double term4 = (height - ((downstreamZ * upstreamOutboardY) - (upstreamOutboardZ * downstreamY)) / (upstreamOutboardY - downstreamY));
+	double term5 = (upstreamOutboardY - downstreamY);
+
+	double result = (term1 + (term2/term3) * (term4)) / (term5);
+
+	return result;
 }
 
 double BioXASMirrorHeightControl::calculateUpstreamOutboardZ(double height, double upstreamInboardX, double upstreamInboardY, double upstreamInboardZ, double upstreamOutboardX, double upstreamOutboardY, double downstreamX, double downstreamY, double downstreamZ)
 {
-	return 0;
+	double term1 = height / (downstreamX * (downstreamY - upstreamOutboardY) + downstreamY * (upstreamOutboardX - downstreamX)) / (upstreamOutboardY - downstreamY);
+	double term2 = ((upstreamInboardX - downstreamX) * (upstreamOutboardY - downstreamY)) + ((upstreamOutboardX - downstreamX) * (downstreamY - upstreamInboardY));
+	double term3 = (upstreamInboardZ - downstreamZ) * (upstreamOutboardY - downstreamY);
+	double term4 = downstreamZ * upstreamOutboardY * ((upstreamInboardX - downstreamX) * (upstreamOutboardY - downstreamY)) + ((upstreamOutboardX - downstreamX) * (downstreamY - upstreamInboardY));
+	double term5 = downstreamZ * downstreamY * upstreamInboardY;
+	double term6 = downstreamY - upstreamInboardY - downstreamY * ((upstreamInboardX - downstreamX) * (upstreamOutboardY - downstreamY)) + ((upstreamOutboardX - downstreamX) * (downstreamY - upstreamInboardY));
+
+	double result = ((term1 * term2) - term3 - term4 + term5) / term6;
+	return result;
 }
 
 double BioXASMirrorHeightControl::calculateDownstreamZ(double height, double upstreamInboardX, double upstreamInboardY, double upstreamInboardZ, double upstreamOutboardX, double upstreamOutboardY, double upstreamOutboardZ, double downstreamX, double downstreamY)
 {
-	return 0;
+	double term1 = height / (downstreamX * (downstreamY - upstreamOutboardY) + downstreamY * (upstreamOutboardX - downstreamX)) / (upstreamOutboardY - downstreamY);
+	double term2 = (upstreamOutboardY - downstreamY) * ((upstreamInboardX - downstreamX) * (upstreamOutboardY - downstreamY)) + ((upstreamOutboardX - downstreamX) * (downstreamY - upstreamInboardY));
+	double term3 = upstreamOutboardZ * downstreamY * ((upstreamInboardX - downstreamX) * (upstreamOutboardY - downstreamY)) + ((upstreamOutboardX - downstreamX) * (downstreamY - upstreamInboardY));
+	double term4 = upstreamInboardZ * pow(upstreamOutboardY - downstreamY, 2);
+	double term5 = upstreamOutboardZ * (downstreamY - upstreamInboardY) * (upstreamOutboardY - downstreamY);
+	double term6 = upstreamOutboardY * ((upstreamInboardX - downstreamX) * (upstreamOutboardY - downstreamY)) + ((upstreamOutboardX - downstreamX) * (downstreamY - upstreamInboardY)) - pow(upstreamOutboardY - downstreamY, 2) - (downstreamY - upstreamInboardY) * (upstreamOutboardY - downstreamY);
+
+	double result = (term1 * term2 + term3 - term4 - term5) / term6;
+
+	return result;
 }
 
 double BioXASMirrorHeightControl::calculateHeight(double upstreamInboardX, double upstreamInboardY, double upstreamInboardZ, double upstreamOutboardX, double upstreamOutboardY, double upstreamOutboardZ, double downstreamX, double downstreamY, double downstreamZ)
