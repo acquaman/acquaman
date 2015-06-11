beadc2106c92a2ed01cbafa1207a354a5c24390b
Iain Workman <iain.workman@googlemail.com>
2015-06-11 09:40:53 -0600

    #1422 Altered documentation of AMGCS2SetSyntaxVersionCommand: - After attempting to create a get syntax version command, it was realized that pretty much any commands issued to a controller set to v1.0 will result in the PI_GCS2_DLL drivers crashing. Made changes to the documentation to highlight this (and abandoned work on the syntax version query command). As such this command should be one of the first things run on initialization to ensure v2.0 snytax

--- source/application/AMPIC887DriverServer/AMGCS2SetSyntaxVersionCommand.h
+++ source/application/AMPIC887DriverServer/AMGCS2SetSyntaxVersionCommand.h
@@ -7,10 +7,13 @@
   * version to GCS2.0.
   *
   * GCS1.0 is only provided on the controller for backwards compatibility use. These
-  * drivers do not support interfacing with GCS1.0. As such this command exists
-  * only to ensure that if, for some reason, the controller is manually set to
-  * syntax version 1.0, we can detect this during initialization with an
-  * AMGCS2SyntaxVersionQueryCommand, and use this command to set it back to 2.0.
+  * drivers do not support interfacing with GCS1.0. A such this command exists only
+  * to ensure that a controller is on the correct syntax level on initialization.
+  *
+  * NOTE: It is not possible to check the syntax version if the syntax version is
+  * set to 1.0. As such the only method to ensure that the controller is set to
+  * syntax version 2.0 without risk of crashing the drivers (which occurs if a call
+  * to qCSV() is made when set to syntax version 1.0) is to run this command.
   */
 class AMGCS2SetSyntaxVersionCommand : public AMGCS2Command
 {
