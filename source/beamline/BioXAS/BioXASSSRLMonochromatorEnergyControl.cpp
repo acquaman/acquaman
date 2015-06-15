51d0884f07183d3a9bb447f44703a7f0d7856c8f
helfrij <jessie.helfrich@gmail.com>
2015-06-15 17:04:36 -0600

    Removed the requirement that the energy control be connected to be measurable, it isn't really necessary.

--- source/beamline/BioXAS/BioXASSSRLMonochromatorEnergyControl.cpp
+++ source/beamline/BioXAS/BioXASSSRLMonochromatorEnergyControl.cpp
@@ -39,10 +39,11 @@ BioXASSSRLMonochromatorEnergyControl::~BioXASSSRLMonochromatorEnergyControl()
 
 bool BioXASSSRLMonochromatorEnergyControl::canMeasure() const
 {
-	bool result = false;
-
-	if (isConnected())
-		result = ( bragg_->canMeasure() && braggSetPosition_->canMeasure() && region_->canMeasure() && m1MirrorPitch_->canMeasure() );
+	bool result = (
+				bragg_ && bragg_->canMeasure() &&
+				region_ && region_->canMeasure() &&
+				m1MirrorPitch_ && m1MirrorPitch_->canMeasure()
+				);
 
 	return result;
 }
