52eb1a8fa93e62b796f909bf30a7bd455236a8d8
helfrij <jessie.helfrich@gmail.com>
2015-06-17 16:50:20 -0600

    Added some mono motors to the list of exposed controls in both Side and Main applications.

--- source/beamline/BioXAS/BioXASMainBeamline.cpp
+++ source/beamline/BioXAS/BioXASMainBeamline.cpp
@@ -248,6 +248,12 @@ void BioXASMainBeamline::setupExposedControls()
 	addExposedControl(mono_->braggMotor()->encoderCalibrationSlopeControl());
 	addExposedControl(mono_->braggMotor()->stepCalibrationSlopeControl());
 	addExposedControl(mono_->braggMotor()->retries());
+	addExposedControl(mono_->verticalMotor());
+	addExposedControl(mono_->lateralMotor());
+	addExposedControl(mono_->crystal1PitchMotor());
+	addExposedControl(mono_->crystal1RollMotor());
+	addExposedControl(mono_->crystal2PitchMotor());
+	addExposedControl(mono_->crystal2RollMotor());
 }
 
 void BioXASMainBeamline::setupExposedDetectors()
