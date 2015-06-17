5fe97f6cf1ec1e817b6dc0c7316fdf2c376c5403
helfrij <jessie.helfrich@gmail.com>
2015-06-17 09:13:43 -0600

    Added a new class to the AMScanViewToolBar, a very basic view to display position information. This will be displayed in the toolbar when the MPlotDataPositionTool is selected.

--- source/ui/dataman/AMScanViewToolBar.cpp
+++ source/ui/dataman/AMScanViewToolBar.cpp
@@ -70,7 +70,94 @@ void AMScanViewToolBar::setPlot(MPlot *newPlot)
 	}
 }
 
+void AMScanViewToolBar::setToolNames(const QStringList &names)
+{
+	toolBox_->clear();
+	toolBox_->addItems(names);
+}
+
 void AMScanViewToolBar::onToolBoxItemActivated(const QString &text)
 {
 	Q_UNUSED(text)
+}
+
+
+
+
+
+
+
+
+
+
+AMScanViewToolBarDataPositionView::AMScanViewToolBarDataPositionView(bool enableValue, QWidget *parent) :
+	QWidget(parent)
+{
+	// Initialize member variables.
+
+	valueEnabled_ = false;
+
+	// Create UI elements.
+
+	QLabel *positionPrompt = new QLabel("Position:");
+	position_ = new QLabel();
+	positionDisplay_ = new QGroupBox();
+
+	QLabel *valuePrompt = new QLabel("Value:");
+	value_ = new QLabel();
+	valueDisplay_ = new QGroupBox();
+
+	// Create and set layouts.
+
+	QHBoxLayout *positionDisplayLayout = new QHBoxLayout();
+	positionDisplayLayout->addWidget(positionPrompt);
+	positionDisplayLayout->addWidget(position_);
+
+	positionDisplay_->setLayout(positionDisplayLayout);
+
+	QHBoxLayout *valueDisplayLayout = new QHBoxLayout();
+	valueDisplayLayout->addWidget(valuePrompt);
+	valueDisplayLayout->addWidget(value_);
+
+	valueDisplay_->setLayout(valueDisplayLayout);
+
+	QHBoxLayout *layout = new QHBoxLayout();
+	layout->addWidget(positionDisplay_);
+	layout->addWidget(valueDisplay_);
+
+	setLayout(layout);
+
+	// Initial settings.
+
+	valueDisplay_->hide();
+
+	// Current settings.
+
+	setValueEnabled(enableValue);
+}
+
+AMScanViewToolBarDataPositionView::~AMScanViewToolBarDataPositionView()
+{
+
+}
+
+void AMScanViewToolBarDataPositionView::setValueEnabled(bool isEnabled)
+{
+	if (valueEnabled_ != isEnabled) {
+		valueEnabled_ = isEnabled;
+		valueDisplay_->setVisible(valueEnabled_);
+		emit valueEnabledChanged(valueEnabled_);
+	}
+}
+
+void AMScanViewToolBarDataPositionView::setPosition(const QString &newPosition)
+{
+	position_->setText(newPosition);
+}
+
+void AMScanViewToolBarDataPositionView::setValue(const QString &newValue)
+{
+	value_->setText(newValue);
+
+	setValueEnabled(newValue.isEmpty());
 }
--- source/ui/dataman/AMScanViewToolBar.h
+++ source/ui/dataman/AMScanViewToolBar.h
@@ -4,6 +4,7 @@
 #include <QWidget>
 #include <QComboBox>
 #include <QLabel>
+#include <QGroupBox>
 #include <QLayout>
 
 class MPlot;
@@ -28,6 +29,8 @@ signals:
 public slots:
 	/// Sets the plot to be edited.
 	void setPlot(MPlot *newPlot);
+	/// Sets the list of available tool names.
+	void setToolNames(const QStringList &names);
 
 protected:
 	/// Handles updating the plot with the selected tool.
@@ -39,6 +42,46 @@ protected:
 
 	/// The combo box displaying the currently active tool, and containing all available tool options.
 	QComboBox *toolBox_;
+};
+
+
+
+class AMScanViewToolBarDataPositionView : public QWidget
+{
+	Q_OBJECT
+
+public:
+	/// Constructor.
+	explicit AMScanViewToolBarDataPositionView(bool enableValue = false, QWidget *parent = 0);
+	/// Destructor.
+	virtual ~AMScanViewToolBarDataPositionView();
+	/// Returns whether the value display is enabled.
+	bool valueEnabled() const { return valueEnabled_; }
+
+signals:
+	/// Notifier that the value display has been en/disabled.
+	void valueEnabledChanged(bool isEnabled);
+
+public slots:
+	/// Sets whether the value is displayed.
+	void setValueEnabled(bool isEnabled);
+	/// Sets the position to be displayed.
+	void setPosition(const QString &newPosition);
+	/// Sets the value to be displayed. Sets enableValue to true if
+	void setValue(const QString &newValue);
+
+protected:
+	/// Bool indicating whether the value should be displayed.
+	bool valueEnabled_;
+
+	/// The label containing the current position.
+	QLabel *position_;
+	/// The box containing all position UI elements.
+	QGroupBox *positionDisplay_;
+	/// The label containing the current value.
+	QLabel *value_;
+	/// The box containing all value UI elements.
+	QGroupBox *valueDisplay_;
 };
 
 #endif // AMSCANVIEWTOOLBAR_H
