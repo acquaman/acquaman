fb58fb85bd1f14b2e5a26a64f176462b2170ab38
Iain Workman <iain.workman@googlemail.com>
2015-06-25 15:16:27 -0600

    #1458 Fixes to the stop command: - Altered the stop command so it now calls stop through a pure ASCII call - Altered the stop command so it waits for 30ms, then clears the error code (doing this any earlier causes the blocking bug from the library to occur

--- source/application/AMPIC887DriverServer/GCS2Commands/AMGCS2StopCommand.h
+++ source/application/AMPIC887DriverServer/GCS2Commands/AMGCS2StopCommand.h
@@ -1,7 +1,7 @@
 #ifndef AMGCS2STOPCOMMAND_H
 #define AMGCS2STOPCOMMAND_H
 #include "AMGCS2Command.h"
-
+#include <QObject>
 /*!
   * A command which represents an instruction to a PI C887.11 Controller to stop
   * all axes immediates and set its error code to 10 (Controller was stopped by
@@ -20,11 +20,17 @@ public:
 	  */
 	virtual ~AMGCS2StopCommand() {}
 
+	/*!
+	  * A string status which reflects whether the controller was stopped.
+	  */
+	virtual QString outputString() const;
 protected:
 	/*!
 	  * Defines the steps taken to issue a stop all command to the controller.
 	  */
 	virtual bool runImplementation();
+
+	QString statusString_;
 };
 
 #endif // AMGCS2STOPCOMMAND_H
