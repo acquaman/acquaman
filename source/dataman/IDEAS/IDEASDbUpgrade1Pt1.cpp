63a0fc6a06aedd6539594d8377456911f64b6c85
Darren Hunter <darren.hunter@usask.ca>
2015-08-10 17:59:37 -0600

    Implemented the upgradeImplementation for the IDEAS db upgrade that will allow it to use the new scan configuration db object.  Not yet tested but it is complete. #1094

--- source/dataman/IDEAS/IDEASDbUpgrade1Pt1.cpp
+++ source/dataman/IDEAS/IDEASDbUpgrade1Pt1.cpp
@@ -24,6 +24,223 @@ bool IDEASDbUpgrade1Pt1::upgradeNecessary() const
 
 bool IDEASDbUpgrade1Pt1::upgradeImplementation()
 {
+	// Getting started.
+	databaseToUpgrade_->startTransaction();
+
+	// Cleanup a bunch of old columns that have no purpose anymore.
+
+	if (databaseToUpgrade_->columnExists("IDEASXASScanConfiguration_table", "startValue")
+			&& !AMDbUpgradeSupport::removeColumn(databaseToUpgrade_, "IDEASXASScanConfiguration_table", "startValue")){
+
+		AMErrorMon::alert(this, IDEASDBUPGRADE1PT1_COULD_NOT_REMOVE_COLUMN, "Could not remove the startValue column in IDEASScanConfiguration_table");
+		databaseToUpgrade_->rollbackTransaction();
+		return false;
+	}
+
+	if (databaseToUpgrade_->columnExists("IDEASXASScanConfiguration_table", "endValue")
+			&& !AMDbUpgradeSupport::removeColumn(databaseToUpgrade_, "IDEASXASScanConfiguration_table", "endValue")){
+
+		AMErrorMon::alert(this, IDEASDBUPGRADE1PT1_COULD_NOT_REMOVE_COLUMN, "Could not remove the endValue column in IDEASScanConfiguration_table");
+		databaseToUpgrade_->rollbackTransaction();
+		return false;
+	}
+
+	if (databaseToUpgrade_->columnExists("IDEASXASScanConfiguration_table", "minEnergy")
+			&& !AMDbUpgradeSupport::removeColumn(databaseToUpgrade_, "IDEASXASScanConfiguration_table", "minEnergy")){
+
+		AMErrorMon::alert(this, IDEASDBUPGRADE1PT1_COULD_NOT_REMOVE_COLUMN, "Could not remove the minEnergy column in IDEASScanConfiguration_table");
+		databaseToUpgrade_->rollbackTransaction();
+		return false;
+	}
+
+	if (databaseToUpgrade_->columnExists("IDEASXASScanConfiguration_table", "maxEnergy")
+			&& !AMDbUpgradeSupport::removeColumn(databaseToUpgrade_, "IDEASXASScanConfiguration_table", "maxEnergy")){
+
+		AMErrorMon::alert(this, IDEASDBUPGRADE1PT1_COULD_NOT_REMOVE_COLUMN, "Could not remove the maxEnergy column in IDEASScanConfiguration_table");
+		databaseToUpgrade_->rollbackTransaction();
+		return false;
+	}
+
+	if (databaseToUpgrade_->columnExists("IDEASXASScanConfiguration_table", "useFixedTime")
+			&& !AMDbUpgradeSupport::removeColumn(databaseToUpgrade_, "IDEASXASScanConfiguration_table", "useFixedTime")){
+
+		AMErrorMon::alert(this, IDEASDBUPGRADE1PT1_COULD_NOT_REMOVE_COLUMN, "Could not remove the useFixedTime column in IDEASScanConfiguration_table");
+		databaseToUpgrade_->rollbackTransaction();
+		return false;
+	}
+
+	if (databaseToUpgrade_->columnExists("IDEASXASScanConfiguration_table", "regions")
+			&& !AMDbUpgradeSupport::removeColumn(databaseToUpgrade_, "IDEASXASScanConfiguration_table", "regions")){
+
+		AMErrorMon::alert(this, IDEASDBUPGRADE1PT1_COULD_NOT_REMOVE_COLUMN, "Could not remove the regions column in IDEASScanConfiguration_table");
+		databaseToUpgrade_->rollbackTransaction();
+		return false;
+	}
+
+	if (databaseToUpgrade_->columnExists("IDEASXASScanConfiguration_table", "I0Channel")
+			&& !AMDbUpgradeSupport::removeColumn(databaseToUpgrade_, "IDEASXASScanConfiguration_table", "I0Channel")){
+
+		AMErrorMon::alert(this, IDEASDBUPGRADE1PT1_COULD_NOT_REMOVE_COLUMN, "Could not remove the I0Channel column in IDEASScanConfiguration_table");
+		databaseToUpgrade_->rollbackTransaction();
+		return false;
+	}
+
+	if (databaseToUpgrade_->columnExists("IDEASXASScanConfiguration_table", "ItChannel")
+			&& !AMDbUpgradeSupport::removeColumn(databaseToUpgrade_, "IDEASXASScanConfiguration_table", "ItChannel")){
+
+		AMErrorMon::alert(this, IDEASDBUPGRADE1PT1_COULD_NOT_REMOVE_COLUMN, "Could not remove the ItChannel column in IDEASScanConfiguration_table");
+		databaseToUpgrade_->rollbackTransaction();
+		return false;
+	}
+
+	if (databaseToUpgrade_->columnExists("IDEASXASScanConfiguration_table", "IrChannel")
+			&& !AMDbUpgradeSupport::removeColumn(databaseToUpgrade_, "IDEASXASScanConfiguration_table", "IrChannel")){
+
+		AMErrorMon::alert(this, IDEASDBUPGRADE1PT1_COULD_NOT_REMOVE_COLUMN, "Could not remove the IrChannel column in IDEASScanConfiguration_table");
+		databaseToUpgrade_->rollbackTransaction();
+		return false;
+	}
+
+	if (databaseToUpgrade_->columnExists("IDEASXASScanConfiguration_table", "isXRFScan")
+			&& !AMDbUpgradeSupport::removeColumn(databaseToUpgrade_, "IDEASXASScanConfiguration_table", "isXRFScan")){
+
+		AMErrorMon::alert(this, IDEASDBUPGRADE1PT1_COULD_NOT_REMOVE_COLUMN, "Could not remove the isXRFScan column in IDEASScanConfiguration_table");
+		databaseToUpgrade_->rollbackTransaction();
+		return false;
+	}
+
+	// Make the new tables.  Only need two for this upgrade.
+
+	QStringList names;
+	names << "AMDbObjectType" << "thumbnailCount" << "thumbnailFirstId" << "name" << "energy" << "fluorescenceDetector";
+	QStringList types;
+	types << "TEXT" << "INTEGER" << "INTEGER" << "TEXT" << "REAL" << "INTEGER";
+
+	if (!databaseToUpgrade_->ensureTable("IDEASScanConfigurationDbObject_table", names, types)){
+
+		AMErrorMon::alert(this, IDEASDBUPGRADE1PT1_COULD_NOT_ADD_TABLE, "Could not add the IDEASScanConfigurationDbObject_table to the database.");
+		databaseToUpgrade_->rollbackTransaction();
+		return false;
+	}
+
+	names = QStringList() << "id1" << "table1" << "id2" << "table2";
+	types = QStringList() << "INTEGER" << "TEXT" << "INTEGER" << "TEXT";
+
+	if (!databaseToUpgrade_->ensureTable("IDEASScanConfigurationDbObject_table_regionsOfInterest", names, types)){
+
+		AMErrorMon::alert(this, IDEASDBUPGRADE1PT1_COULD_NOT_ADD_TABLE, "Could not add the IDEASScanConfigurationDbObject_table_regionsOfInterest to the database.");
+		databaseToUpgrade_->rollbackTransaction();
+		return false;
+	}
+
+	// Add the dbObject column to the XAS scan configuration table.
+
+	if (!databaseToUpgrade_->ensureColumn("IDEASXASScanConfiguration_table", "configurationDbObject", "TEXT")){
+
+		AMErrorMon::alert(this, IDEASDBUPGRADE1PT1_COULD_NOT_CREATE_COLUMN, "Could no create the configurationDbObject column inside of IDEASXASScanConfiguration_table");
+		databaseToUpgrade_->rollbackTransaction();
+		return false;
+	}
+
+	// Fill the data.
+
+	QVariantList ids = databaseToUpgrade_->retrieve("IDEASXASScanConfiguration_table", "id");
+	QVariantList scanNames = databaseToUpgrade_->retrieve("IDEASXASScanConfiguration_table", "name");
+	QVariantList energy = databaseToUpgrade_->retrieve("IDEASXASScanConfiguration_table", "edgeEnergy");
+	QVariantList fluorescenceDetector = databaseToUpgrade_->retrieve("IDEASXASScanConfiguration_table", "fluorescenceDetector");
+
+	for (int i = 0, size = ids.size(); i < size; i++){
+
+		int id = databaseToUpgrade_->insertOrUpdate(0, "IDEASScanConfigurationDbObject_table",
+													QStringList() << "AMDbObjectType" << "thumbnailCount" << "thumbnailFirstId" << "name" << "energy" << "fluorescenceDetector",
+													QVariantList()
+													<< "IDEASScanConfigurationDbObject"
+													<< 0
+													<< QVariant()
+													<< scanNames.at(i)
+													<< energy.at(i)
+													<< fluorescenceDetector.at(i));
+
+		if (id == 0){
+
+			AMErrorMon::alert(this, IDEASDBUPGRADE1PT1_COULD_NOT_ADD_ROW, "Could not insert into IDEASScanConfigurationDbObject_table");
+			databaseToUpgrade_->rollbackTransaction();
+			return false;
+		}
+
+		if (!databaseToUpgrade_->update(ids.at(i).toInt(), "IDEASXASScanConfiguration_table", "configurationDbObject", QString("IDEASScanConfigurationDbObject_table;%1").arg(id))){
+
+			AMErrorMon::alert(this, IDEASDBUPGRADE1PT1_COULD_NOT_UPDATE_COLUMN, "Could not update the configurationDbObject column in IDEASXASScanConfiguration_table");
+			databaseToUpgrade_->rollbackTransaction();
+			return false;
+		}
+	}
+
+	// Rename some columns.
+
+	if (!AMDbUpgradeSupport::changeColumnName(databaseToUpgrade_, "IDEASXASScanConfiguration_table", "isTransScan", "usingTransmission")){
+
+		AMErrorMon::alert(this, IDEASDBUPGRADE1PT1_COULD_NOT_CHANGE_COLUMN_NAME, "Could not rename column isTransScan to usingTransmission in IDEASXASScanConfiguration_table");
+		databaseToUpgrade_->rollbackTransaction();
+		return false;
+	}
+
+	if (!AMDbUpgradeSupport::changeColumnName(databaseToUpgrade_, "IDEASXASScanConfiguration_table", "useRef", "usingReference")){
+
+		AMErrorMon::alert(this, IDEASDBUPGRADE1PT1_COULD_NOT_CHANGE_COLUMN_NAME, "Could not rename column useRef to usingReference in IDEASXASScanConfiguration_table");
+		databaseToUpgrade_->rollbackTransaction();
+		return false;
+	}
+
+	// Database cleanup.
+
+	QStringList dboColumnNames = QStringList() << "AMDbObjectType" << "tableName" << "description" << "version" << "inheritance";
+	QVariantList dboValues = QVariantList() << "IDEASScanConfigurationDbObject" << "IDEASScanConfigurationDbObject_table" << "IDEAS Scan Configuration Database Object" << 1 << "IDEASScanConfigurationDbObject;AMDbObject;QObject";
+	databaseToUpgrade_->ensureColumn("AMDbObjectTypes_table", "inheritance", "TEXT");
+	int newId = databaseToUpgrade_->insertOrUpdate(0, "AMDbObjectTypes_table", dboColumnNames, dboValues);
+
+	if (newId == 0){
+
+		databaseToUpgrade_->rollbackTransaction();
+		AMErrorMon::alert(this, IDEASDBUPGRADE1PT5_COULD_NOT_ADD_NEW_ENTRY_VESPERSSCANCONFIGURATIONDBOBJECT_TO_AMDBOBJECTTYPES_TABLE, "Could not add the new IDEASScanConfigurationDbObject entry to the AMDbObjectTypes table.");
+		return false;
+	}
+
+	// First: allColumns
+	if (databaseToUpgrade_->insertOrUpdate(0, "AMDbObjectTypes_allColumns", QStringList() << "typeId" << "columnName", QVariantList() << newId << "name") == 0
+			|| databaseToUpgrade_->insertOrUpdate(0, "AMDbObjectTypes_allColumns", QStringList() << "typeId" << "columnName", QVariantList() << newId << "energy") == 0
+			|| databaseToUpgrade_->insertOrUpdate(0, "AMDbObjectTypes_allColumns", QStringList() << "typeId" << "columnName", QVariantList() << newId << "fluorescenceDetector") == 0
+			){
+
+		databaseToUpgrade_->rollbackTransaction();
+		AMErrorMon::alert(this, IDEASDBUPGRADE1PT1_COULD_NOT_ADD_NEW_ENTRIES_TO_AMDBOBJECTTYPES_TABLE_ALLCOLUMNS, "Could not add the entries for IDEASScanConfigurationDbObject to the allColumns associated table of AMDbObjectTypes table.");
+		return false;
+	}
+
+	// Second: visibleColumns
+	if (databaseToUpgrade_->insertOrUpdate(0, "AMDbObjectTypes_visibleColumns", QStringList() << "typeId" << "columnName", QVariantList() << newId << "name") == 0
+			|| databaseToUpgrade_->insertOrUpdate(0, "AMDbObjectTypes_visibleColumns", QStringList() << "typeId" << "columnName", QVariantList() << newId << "energy") == 0
+			|| databaseToUpgrade_->insertOrUpdate(0, "AMDbObjectTypes_visibleColumns", QStringList() << "typeId" << "columnName", QVariantList() << newId << "fluorescenceDetector") == 0
+			){
+
+		databaseToUpgrade_->rollbackTransaction();
+		AMErrorMon::alert(this, IDEASDBUPGRADE1PT1_COULD_NOT_ADD_NEW_ENTRIES_TO_AMDBOBJECTTYPES_TABLE_VISIBLECOLUMNS, "Could not add the entries for IDEASScanConfigurationDbObject to the visibleColumns associated table of AMDbObjectTypes table.");
+		return false;
+	}
+
+	// Third: loadColumns
+	if (databaseToUpgrade_->insertOrUpdate(0, "AMDbObjectTypes_loadColumns", QStringList() << "typeId" << "columnName", QVariantList() << newId << "name") == 0
+			|| databaseToUpgrade_->insertOrUpdate(0, "AMDbObjectTypes_loadColumns", QStringList() << "typeId" << "columnName", QVariantList() << newId << "energy") == 0
+			|| databaseToUpgrade_->insertOrUpdate(0, "AMDbObjectTypes_loadColumns", QStringList() << "typeId" << "columnName", QVariantList() << newId << "fluorescenceDetector") == 0
+			){
+
+		databaseToUpgrade_->rollbackTransaction();
+		AMErrorMon::alert(this, IDEASDBUPGRADE1PT1_COULD_NOT_ADD_NEW_ENTRIES_TO_AMDBOBJECTTYPES_TABLE_LOADCOLUMNS, "Could not add the entries for IDEASScanConfigurationDbObject to the loadColumns associated table of AMDbObjectTypes table.");
+		return false;
+	}
+
+	databaseToUpgrade_->commitTransaction();
+
 	return false;
 }
 
