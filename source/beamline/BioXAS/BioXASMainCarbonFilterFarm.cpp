0b6c746c76435880e59c292b3453aeef697505c8
helfrij <jessie.helfrich@gmail.com>
2015-08-10 13:48:46 -0600

    Commented out the body of the constructor for BioXASMainCarbonFilterFarm. This is just to get the application to build, as I think there are improvements to be made in how the carbon filter farm is controlled.

--- source/beamline/BioXAS/BioXASMainCarbonFilterFarm.cpp
+++ source/beamline/BioXAS/BioXASMainCarbonFilterFarm.cpp
@@ -3,38 +3,38 @@
 BioXASMainCarbonFilterFarm::BioXASMainCarbonFilterFarm(QObject *parent) :
 	BioXASCarbonFilterFarm("MainCarbonFilterFarm", parent)
 {
-	// Create components.
+//	// Create components.
 
-	upstreamPosition_ = new AMPVControl(name_+"UpstreamPosition", "SMTR1607-5-I00-01:mm:fbk", "SMTR1607-5-I00-01:mm:sp", "SMTR1607-5-I00-01:stop", this);
-	upstreamStatus_ = new AMReadOnlyPVControl(name_+"UpstreamStatus", "SMTR1607-5-I00-01:inPosition", this);
-	upstreamActuator_ = new BioXASCarbonFilterFarmActuatorControl(upstreamPosition_, upstreamStatus_, this);
+//	upstreamPosition_ = new AMPVControl(name_+"UpstreamPosition", "SMTR1607-5-I00-01:mm:fbk", "SMTR1607-5-I00-01:mm:sp", "SMTR1607-5-I00-01:stop", this);
+//	upstreamStatus_ = new AMReadOnlyPVControl(name_+"UpstreamStatus", "SMTR1607-5-I00-01:inPosition", this);
+//	upstreamActuator_ = new BioXASCarbonFilterFarmActuatorControl(upstreamPosition_, upstreamStatus_, this);
 
-	downstreamPosition_ = new AMPVControl(name_+"DownstreamPosition", "SMTR1607-5-I00-02:mm:fbk", "SMTR1607-5-I00-02:mm:sp", "SMTR1607-5-I00-02:stop", this);
-	downstreamStatus_ = new AMReadOnlyPVControl(name_+"DownstreamStatus", "SMTR1607-5-I00-02:inPosition", this);
-	downstreamActuator_ = new BioXASCarbonFilterFarmActuatorControl(downstreamPosition_, downstreamStatus_, this);
+//	downstreamPosition_ = new AMPVControl(name_+"DownstreamPosition", "SMTR1607-5-I00-02:mm:fbk", "SMTR1607-5-I00-02:mm:sp", "SMTR1607-5-I00-02:stop", this);
+//	downstreamStatus_ = new AMReadOnlyPVControl(name_+"DownstreamStatus", "SMTR1607-5-I00-02:inPosition", this);
+//	downstreamActuator_ = new BioXASCarbonFilterFarmActuatorControl(downstreamPosition_, downstreamStatus_, this);
 
-	filter_ = new BioXASSideCarbonFilterFarmControl(upstreamActuator_, downstreamActuator_, this);
+//	filter_ = new BioXASSideCarbonFilterFarmControl(upstreamActuator_, downstreamActuator_, this);
 
-	// Make connections.
+//	// Make connections.
 
-	connect( filter_, SIGNAL(connected(bool)), this, SLOT(updateConnected()) );
+//	connect( filter_, SIGNAL(connected(bool)), this, SLOT(updateConnected()) );
 
-	// Current settings.
+//	// Current settings.
 
-	upstreamActuator_->setWindowPosition(Window::None, BIOXASSIDECARBONFILTERFARM_UPSTREAM_OUT);
-	upstreamActuator_->setWindowPosition(Window::Bottom, BIOXASSIDECARBONFILTERFARM_UPSTREAM_BOTTOM);
-	upstreamActuator_->setWindowPosition(Window::Top, BIOXASSIDECARBONFILTERFARM_UPSTREAM_TOP);
+//	upstreamActuator_->setWindowPosition(Window::None, BIOXASMAINCARBONFILTERFARM_UPSTREAM_OUT);
+//	upstreamActuator_->setWindowPosition(Window::Bottom, BIOXASMAINCARBONFILTERFARM_UPSTREAM_BOTTOM);
+//	upstreamActuator_->setWindowPosition(Window::Top, BIOXASMAINCARBONFILTERFARM_UPSTREAM_TOP);
 
-	downstreamActuator_->setWindowPosition(Window::None, BIOXASSIDECARBONFILTERFARM_DOWNSTREAM_OUT);
-	downstreamActuator_->setWindowPosition(Window::Bottom, BIOXASSIDECARBONFILTERFARM_DOWNSTREAM_BOTTOM);
-	downstreamActuator_->setWindowPosition(Window::Top, BIOXASSIDECARBONFILTERFARM_DOWNSTREAM_TOP);
+//	downstreamActuator_->setWindowPosition(Window::None, BIOXASMAINCARBONFILTERFARM_DOWNSTREAM_OUT);
+//	downstreamActuator_->setWindowPosition(Window::Bottom, BIOXASMAINCARBONFILTERFARM_DOWNSTREAM_BOTTOM);
+//	downstreamActuator_->setWindowPosition(Window::Top, BIOXASMAINCARBONFILTERFARM_DOWNSTREAM_TOP);
 
-	filter_->setWindowFilter(Actuator::Upstream, Window::Bottom, Filter::Fifty);
-	filter_->setWindowFilter(Actuator::Upstream, Window::Top, Filter::Fifty);
-	filter_->setWindowFilter(Actuator::Downstream, Window::Bottom, Filter::None);
-	filter_->setWindowFilter(Actuator::Downstream, Window::Top, Filter::SevenHundred);
+//	filter_->setWindowFilter(Actuator::Upstream, Window::Bottom, Filter::Fifty);
+//	filter_->setWindowFilter(Actuator::Upstream, Window::Top, Filter::Fifty);
+//	filter_->setWindowFilter(Actuator::Downstream, Window::Bottom, Filter::None);
+//	filter_->setWindowFilter(Actuator::Downstream, Window::Top, Filter::SevenHundred);
 
-	updateConnected();
+//	updateConnected();
 }
 
 BioXASMainCarbonFilterFarm::~BioXASMainCarbonFilterFarm()
