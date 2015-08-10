965444fc8bb1f01392867791679443ef3f8ad098
helfrij <jessie.helfrich@gmail.com>
2015-08-10 11:06:42 -0600

    Added creation of the estimated time label to the constructor of BioXASMainXASScanConfigurationView--the source of a segfault when building BioXASMain, I think.

--- source/ui/BioXAS/BioXASMainXASScanConfigurationView.cpp
+++ source/ui/BioXAS/BioXASMainXASScanConfigurationView.cpp
@@ -35,6 +35,8 @@ BioXASMainXASScanConfigurationView::BioXASMainXASScanConfigurationView(BioXASMai
 	scanName_ = new QLineEdit();
 	scanName_->setText(configuration_->userScanName());
 
+	estimatedTimeLabel_ = new QLabel();
+
 	connect(scanName_, SIGNAL(editingFinished()), this, SLOT(onScanNameEdited()));
 	connect(configuration_, SIGNAL(nameChanged(QString)), scanName_, SLOT(setText(QString)));
     connect(configuration_, SIGNAL(totalTimeChanged(double)), this, SLOT(onEstimatedTimeChanged(double)));
