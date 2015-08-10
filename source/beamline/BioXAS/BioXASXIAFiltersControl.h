ea22d373fe5101d124d2705dcf1580a0c97ae315
helfrij <jessie.helfrich@gmail.com>
2015-08-10 13:59:02 -0600

    Added getters for each individual actuator control in BioXASXIAFiltersControl--they were missing.

--- source/beamline/BioXAS/BioXASXIAFiltersControl.h
+++ source/beamline/BioXAS/BioXASXIAFiltersControl.h
@@ -30,6 +30,15 @@ public:
 	/// Returns true if this control can stop a move right now.
 	virtual bool canStop() const;
 
+	/// Returns the first filter actuator control.
+	AMControl* firstFilterActuatorControl() const { return firstFilterActuatorControl_; }
+	/// Returns the second filter actuator control.
+	AMControl* secondFilterActuatorControl() const { return secondFilterActuatorControl_; }
+	/// Returns the third filter actuator control.
+	AMControl* thirdFilterActuatorControl() const { return thirdFilterActuatorControl_; }
+	/// Returns the fourth filter actuator control.
+	AMControl* fourthFilterActuatorControl() const { return fourthFilterActuatorControl_; }
+
 	/// Returns true if the given value is a valid value for this control, false otherwise.
 	virtual bool validValue(double value) const;
 
