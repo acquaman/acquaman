99da4e6fee72ac6ba460cb92c1ca40da4c0062c4
helfrij <jessie.helfrich@gmail.com>
2015-08-11 11:42:22 -0600

    Uncommented the actuator controls in BioXASMainXIAFilters and changed the pv names to correspond to Main actuator pvs.

--- source/beamline/BioXAS/BioXASMainXIAFilters.cpp
+++ source/beamline/BioXAS/BioXASMainXIAFilters.cpp
@@ -5,16 +5,16 @@ BioXASMainXIAFilters::BioXASMainXIAFilters(QObject *parent) :
 {
 	// Initialize inherited variables.
 
-//	filter1_ = new CLSBiStateControl("XIA Filter 1", "XIA Filter 1", "PFIL1607-6-I22-01:ctl", "PFIL1607-6-I22-01:OprOpen", "PFIL1607-6-I22-01:OprClose", new AMControlStatusCheckerDefault(2), this);
-//	filter2_ = new CLSBiStateControl("XIA Filter 2", "XIA Filter 2", "PFIL1607-6-I22-02:ctl", "PFIL1607-6-I22-02:OprOpen", "PFIL1607-6-I22-02:OprClose", new AMControlStatusCheckerDefault(2), this);
-//	filter3_ = new CLSBiStateControl("XIA Filter 3", "XIA Filter 3", "PFIL1607-6-I22-03:ctl", "PFIL1607-6-I22-03:OprOpen", "PFIL1607-6-I22-03:OprClose", new AMControlStatusCheckerDefault(2), this);
-//	filter4_ = new CLSBiStateControl("XIA Filter 4", "XIA Filter 4", "PFIL1607-6-I22-04:ctl", "PFIL1607-6-I22-04:OprOpen", "PFIL1607-6-I22-04:OprClose", new AMControlStatusCheckerDefault(2), this);
+	filter1_ = new CLSBiStateControl("XIA Filter 1", "XIA Filter 1", "PFIL1607-7-I21-01:ctl", "PFIL1607-7-I21-01:OprOpen", "PFIL1607-7-I21-01:OprClose", new AMControlStatusCheckerDefault(2), this);
+	filter2_ = new CLSBiStateControl("XIA Filter 2", "XIA Filter 2", "PFIL1607-7-I21-02:ctl", "PFIL1607-7-I21-02:OprOpen", "PFIL1607-7-I21-02:OprClose", new AMControlStatusCheckerDefault(2), this);
+	filter3_ = new CLSBiStateControl("XIA Filter 3", "XIA Filter 3", "PFIL1607-7-I21-03:ctl", "PFIL1607-7-I21-03:OprOpen", "PFIL1607-7-I21-03:OprClose", new AMControlStatusCheckerDefault(2), this);
+	filter4_ = new CLSBiStateControl("XIA Filter 4", "XIA Filter 4", "PFIL1607-7-I21-04:ctl", "PFIL1607-7-I21-04:OprOpen", "PFIL1607-7-I21-04:OprClose", new AMControlStatusCheckerDefault(2), this);
 
 	filtersControl_ = new BioXASXIAFiltersControl(name() + "Control", this);
-//	filtersControl_->addFilterActuatorControl(filter1_, 0.25);
-//	filtersControl_->addFilterActuatorControl(filter2_, 0.5);
-//	filtersControl_->addFilterActuatorControl(filter3_, 1.0);
-//	filtersControl_->addFilterActuatorControl(filter4_, 10.0);
+	filtersControl_->addFilterActuatorControl(filter1_, 0.25);
+	filtersControl_->addFilterActuatorControl(filter2_, 0.5);
+	filtersControl_->addFilterActuatorControl(filter3_, 1.0);
+	filtersControl_->addFilterActuatorControl(filter4_, 10.0);
 
 	// Make connections.
 
