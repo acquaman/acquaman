12f53bd55ac403e0dfb8c185cd84bc44703aaa2c
David Muir <david.muir@lightsource.ca>
2015-08-24 16:27:52 -0600

    ADDED: Second camera view, removing tabbed view and optomized widget layout. #1525

--- source/ui/REIXS/REIXSSampleManagementPre2013Widget.cpp
+++ source/ui/REIXS/REIXSSampleManagementPre2013Widget.cpp
@@ -3,6 +3,7 @@
 #include <QGridLayout>
 #include <QUrl>
 #include <QGroupBox>
+#include <QSpacerItem>
 
 #include "ui/AMTopFrame2.h"
 
@@ -12,16 +13,27 @@ REIXSSampleManagementPre2013Widget::REIXSSampleManagementPre2013Widget(QWidget *
 	QWidget(parent)
 {
 #ifdef AM_MOBILITY_VIDEO_ENABLED
-	cameraWidget1_ = new AMBeamlineCameraWidget(this, false);
+
+
+
+
+	cameraWidget1_ = new AMBeamlineCameraWidget(this, true);
 	cameraWidget1_->playSource(sampleCamera1Url);
 	cameraWidget1_->setCrosshairCenterPosition(QPointF(0.75, 0.6));
 	cameraWidget1_->setCrosshairPosition(QPointF(0.75, 0.6));
+	cameraWidget1_->setCrosshairVisible(true);
+	cameraWidget1_->setCrosshairLocked(true);
 	cameraWidget1_->setSizePolicy(QSizePolicy::MinimumExpanding, QSizePolicy::MinimumExpanding);
-	cameraWidget2_ = new AMBeamlineCameraWidget(this, false);
+
+
+	cameraWidget2_ = new AMBeamlineCameraWidget(this, true);
 	cameraWidget2_->playSource(sampleCamera2Url);
 	cameraWidget2_->setCrosshairCenterPosition(QPointF(0.5, 0.7));
 	cameraWidget2_->setCrosshairPosition(QPointF(0.5, 0.7));
+	cameraWidget2_->setCrosshairVisible(true);
+	cameraWidget2_->setCrosshairLocked(true);
 	cameraWidget2_->setSizePolicy(QSizePolicy::MinimumExpanding, QSizePolicy::MinimumExpanding);
+
 #else
 	Q_UNUSED(sampleCameraUrl1);
 	Q_UNUSED(sampleCameraUrl2);
@@ -34,6 +46,8 @@ REIXSSampleManagementPre2013Widget::REIXSSampleManagementPre2013Widget(QWidget *
 
 	manipulatorWidget_ = manipulatorWidget;
 
+	QSpacerItem *bottomSpacer = new QSpacerItem(1,1,QSizePolicy::MinimumExpanding,QSizePolicy::MinimumExpanding);
+
 	connect(plateView_, SIGNAL(newSamplePlateSelected()), this, SLOT(onNewSamplePlateSelected()));
 
 	QVBoxLayout *vl = new QVBoxLayout();
@@ -42,13 +56,22 @@ REIXSSampleManagementPre2013Widget::REIXSSampleManagementPre2013Widget(QWidget *
 	vl->addLayout(gl);
 
 #ifdef AM_MOBILITY_VIDEO_ENABLED
-	gl->addWidget(cameraWidget1_, 0, 0, 1, 1);
-	gl->addWidget(cameraWidget2_, 0, 1, 1, 1);
+	gl->addWidget(cameraWidget1_, 0, 0, 1, 2);
+	gl->addWidget(cameraWidget2_, 0, 2, 1, 2);
 #endif
-	gl->addWidget(plateView_, 0, 2, 2, 1);
-	gl->addWidget(manipulatorWidget_, 1, 0, 1, 2);
+	gl->addWidget(plateView_, 0, 4, 3, 1);
+	gl->addWidget(manipulatorWidget_, 1, 1, 1, 2);
 	gl->setColumnStretch(0,1);
 	gl->setColumnStretch(1,0);
+	gl->setColumnStretch(2,0);
+	gl->setColumnStretch(3,1);
+	gl->setColumnStretch(2,0);
+
+	gl->setRowStretch(0,2);
+	gl->setRowStretch(1,0);
+	gl->setRowStretch(2,1);
+
+	gl->addItem(bottomSpacer,2,1,1,4);
 
 	vl->setContentsMargins(0,0,0,0);
 	gl->setContentsMargins(10, 0, 10, 0);
