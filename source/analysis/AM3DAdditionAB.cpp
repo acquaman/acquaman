bd9b4af77ef8860874f651f2324ec9e137fe19d1
Darren Hunter <darren.hunter@lightsource.ca>
2015-11-04 11:44:55 -0600

    Added the dirty indices caching to AM3DAdditionAB. #1692

--- source/analysis/AM3DAdditionAB.cpp
+++ source/analysis/AM3DAdditionAB.cpp
@@ -127,7 +127,16 @@ bool AM3DAdditionAB::axisValues(int axisNumber, int startIndex, int endIndex, do
 // Connected to be called when the values of the input data source change
 void AM3DAdditionAB::onInputSourceValuesChanged(const AMnDIndex& start, const AMnDIndex& end)
 {
-    cacheUpdateRequired_ = true;
+	cacheUpdateRequired_ = true;
+
+	AMnDIndex scanStart = start;
+	AMnDIndex scanEnd = end;
+	scanStart.setRank(start.rank()-1);
+	scanEnd.setRank(end.rank()-1);
+
+	if (scanStart == scanEnd)
+		dirtyIndices_ << start;
+
 	emitValuesChanged(start, end);
 }
 
@@ -139,6 +148,7 @@ void AM3DAdditionAB::onInputSourceSizeChanged()
     axes_[2].size = sources_.at(0)->size(2);
 
     cacheUpdateRequired_ = true;
+    dirtyIndices_.clear();
     cachedData_ = QVector<double>(size().product());
     emitSizeChanged();
 }
@@ -184,8 +194,9 @@ void AM3DAdditionAB::setInputDataSourcesImplementation(const QList<AMDataSource*
 		axes_[1] = sources_.at(0)->axisInfoAt(1);
 		axes_[2] = sources_.at(0)->axisInfoAt(2);
 
-        cacheUpdateRequired_ = true;
-        cachedData_ = QVector<double>(size().product());
+		cacheUpdateRequired_ = true;
+		dirtyIndices_.clear();
+		cachedData_ = QVector<double>(size().product());
 
 		setDescription(QString("Sum of spectra from %1 maps").arg(sources_.size()));
 
@@ -199,9 +210,9 @@ void AM3DAdditionAB::setInputDataSourcesImplementation(const QList<AMDataSource*
 
 	reviewState();
 
-    emitSizeChanged();
-    emitValuesChanged();
-    emitAxisInfoChanged();
+	emitSizeChanged();
+	emitValuesChanged();
+	emitAxisInfoChanged();
 	emitInfoChanged();
 }
 
@@ -238,25 +249,53 @@ void AM3DAdditionAB::reviewState()
 
 void AM3DAdditionAB::computeCachedValues() const
 {
-    AMnDIndex start = AMnDIndex(0, 0, 0);
-    AMnDIndex end = size()-1;
-    int totalSize = start.totalPointsTo(end);
+	AMnDIndex start = AMnDIndex();
+	AMnDIndex end = AMnDIndex();
 
-    QVector<double> data = QVector<double>(totalSize);
-    sources_.at(0)->values(start, end, data.data());
+	if (dirtyIndices_.isEmpty()){
 
-    // Do the first data source separately to initialize the values.
-    cachedData_ = data;
+		start = AMnDIndex(rank(), AMnDIndex::DoInit);
+		end = size()-1;
+	}
 
-    // Iterate through the rest of the sources.
-    for (int i = 1, count = sources_.size(); i < count; i++){
+	else {
 
-        sources_.at(i)->values(start, end, data.data());
+		start = dirtyIndices_.first();
+		end = dirtyIndices_.last();
+		end[rank()-1] = size(rank()-1);
+	}
 
-        for (int j = 0; j < totalSize; j++)
-            cachedData_[j] += data.at(j);
-    }
+	int totalSize = start.totalPointsTo(end);
+	int flatStartIndex = start.flatIndexInArrayOfSize(size());
+	QVector<double> data = QVector<double>(totalSize);
+	sources_.at(0)->values(start, end, data.data());
 
-    cachedDataRange_ = AMUtility::rangeFinder(cachedData_);
-    cacheUpdateRequired_ = false;
+	// Do the first data source separately to initialize the values.
+	memcpy(cachedData_.data()+flatStartIndex, data.constData(), totalSize*sizeof(double));
+	cachedData_ = data;
+
+	// Iterate through the rest of the sources.
+	for (int i = 1, count = sources_.size(); i < count; i++){
+
+		sources_.at(i)->values(start, end, data.data());
+
+		for (int j = 0; j < totalSize; j++)
+			cachedData_[flatStartIndex+j] += data.at(j);
+	}
+
+	if (dirtyIndices_.isEmpty())
+		cachedDataRange_ = AMUtility::rangeFinder(cachedData_);
+
+	else{
+		AMRange cachedRange = AMUtility::rangeFinder(cachedData_.mid(flatStartIndex, totalSize));
+
+		if (cachedDataRange_.minimum() > cachedRange.minimum())
+			cachedDataRange_.setMinimum(cachedRange.minimum());
+
+		if (cachedDataRange_.maximum() < cachedRange.maximum())
+			cachedDataRange_.setMaximum(cachedRange.maximum());
+	}
+
+	cacheUpdateRequired_ = false;
+	dirtyIndices_.clear();
 }
