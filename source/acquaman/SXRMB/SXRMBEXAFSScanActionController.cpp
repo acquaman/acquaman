f9ac165099564f42eca0e6ef8dd32f42c3976914
Darren Hunter <darren.hunter@lightsource.ca>
2015-11-05 11:31:12 -0600

    Changed all the scan controllers that used the ascii region file loader to use CDF instead. #1701

--- source/acquaman/SXRMB/SXRMBEXAFSScanActionController.cpp
+++ source/acquaman/SXRMB/SXRMBEXAFSScanActionController.cpp
@@ -18,7 +18,7 @@ SXRMBEXAFSScanActionController::SXRMBEXAFSScanActionController(SXRMBEXAFSScanCon
 	configuration_ = configuration;
 
 	scan_ = new AMXASScan();
-	scan_->setFileFormat("amRegionAscii2013");
+	scan_->setFileFormat("amCDFv1");
 	scan_->setScanConfiguration(configuration);
 	scan_->setIndexType("fileSystem");
 	scan_->rawData()->addScanAxis(AMAxisInfo("eV", 0, "Incident Energy", "eV"));
