7e3704d16771eaf04f5464e037a932479517285e
helfrij <jessie.helfrich@gmail.com>
2015-11-23 10:44:43 -0600

    Cleaned up the sample chamber move action view class. It's now pretty empty, but SGM application now builds.

--- source/ui/SGM/SGMSampleChamberVacuumMoveActionView.cpp
+++ source/ui/SGM/SGMSampleChamberVacuumMoveActionView.cpp
@@ -1,89 +1,5 @@
 #include "SGMSampleChamberVacuumMoveActionView.h"
 
-SGMSampleChamberVacuumMoveActionLEDView::SGMSampleChamberVacuumMoveActionLEDView(SGMSampleChamberVacuumMoveAction *action, QWidget *parent) :
-	QWidget(parent)
-{
-	// Initialize class variables.
-
-	action_ = 0;
-
-	// Create and set layout.
-
-	layout_ = new QVBoxLayout();
-
-	setLayout(layout_);
-
-	// Current settings.
-
-	setAction(action);
-}
-
-SGMSampleChamberVacuumMoveActionLEDView::~SGMSampleChamberVacuumMoveActionLEDView()
-{
-
-}
-
-void SGMSampleChamberVacuumMoveActionLEDView::clear()
-{
-	foreach (QLabel *led, leds_) {
-		layout_->removeWidget(led);
-		led->deleteLater();
-	}
-}
-
-void SGMSampleChamberVacuumMoveActionLEDView::refresh()
-{
-	// Clear the view.
-
-	clear();
-
-	// Create an led for each subaction, colored according to the subaction running
-	// status.
-
-	if (action_) {
-
-		for (int i = 0, count = action_->subActionCount(); i < count; i++) {
-			AMAction3 *subAction = action_->subActionAt(i);
-
-			if (subAction) {
-
-				AMAction3::State subActionState = subAction->state();
-				QLabel *led = new QLabel();
-
-				if (subActionState == AMAction3::Succeeded)
-					led->setPixmap(QPixmap(":/22x22/greenLEDOn.png"));
-				else if (subActionState == AMAction3::Failed)
-					led->setPixmap(QPixmap(":/22x22/redLEDOn.png"));
-				else if (subActionState == AMAction3::Running)
-					led->setPixmap(QPixmap(":/22x22/yellowLEDOn.png"));
-				else
-					led->setPixmap(QPixmap(":/22x22/yellowLEDOff.png"));
-
-				leds_.append(led);
-				layout_->addWidget(led);
-			}
-		}
-	}
-}
-
-void SGMSampleChamberVacuumMoveActionLEDView::setAction(SGMSampleChamberVacuumMoveAction *newAction)
-{
-	if (action_ != newAction) {
-
-		if (action_)
-			disconnect( action_, 0, this, 0 );
-
-		action_ = newAction;
-
-		if (action_)
-			connect( action_, SIGNAL(progressChanged(double,double)), this, SLOT(refresh()) );
-
-		refresh();
-
-		emit actionChanged(action_);
-	}
-}
-
 SGMSampleChamberVacuumMoveActionView::SGMSampleChamberVacuumMoveActionView(SGMSampleChamberVacuumMoveAction *action, QWidget *parent) :
 	QWidget(parent)
 {
@@ -93,12 +9,10 @@ SGMSampleChamberVacuumMoveActionView::SGMSampleChamberVacuumMoveActionView(SGMSa
 
 	// Create UI elements.
 
-	ledView_ = new SGMSampleChamberVacuumMoveActionView(0);
 
 	// Create and set layouts.
 
 	QHBoxLayout *layout = new QHBoxLayout();
-	layout->addWidget(ledView_);
 
 	setLayout(layout);
 
@@ -114,7 +28,7 @@ SGMSampleChamberVacuumMoveActionView::~SGMSampleChamberVacuumMoveActionView()
 
 void SGMSampleChamberVacuumMoveActionView::clear()
 {
-	ledView_->setAction(0);
+
 }
 
 void SGMSampleChamberVacuumMoveActionView::refresh()
@@ -125,7 +39,6 @@ void SGMSampleChamberVacuumMoveActionView::refresh()
 
 	// Update view elements.
 
-	ledView_->setAction(action_);
 }
 
 void SGMSampleChamberVacuumMoveActionView::setAction(SGMSampleChamberVacuumMoveAction *newAction)
--- source/ui/SGM/SGMSampleChamberVacuumMoveActionView.h
+++ source/ui/SGM/SGMSampleChamberVacuumMoveActionView.h
@@ -7,42 +7,6 @@
 
 #include "actions3/SGM/SGMSampleChamberVacuumMoveAction.h"
 
-class SGMSampleChamberVacuumMoveActionLEDView : public QWidget
-{
-	Q_OBJECT
-
-public:
-	/// Constructor.
-	explicit SGMSampleChamberVacuumMoveActionLEDView(SGMSampleChamberVacuumMoveAction *action, QWidget *parent = 0);
-	/// Destructor.
-	virtual ~SGMSampleChamberVacuumMoveActionLEDView();
-
-	/// Returns the action being viewed.
-	SGMSampleChamberVacuumMoveAction* action() const { return action_; }
-
-signals:
-	/// Notifier that the action being viewed has changed.
-	void actionChanged(SGMSampleChamberVacuumMoveAction *newAction);
-
-public slots:
-	/// Clears the view.
-	void clear();
-	/// Refreshes the view.
-	void refresh();
-
-	/// Sets the action being viewed.
-	void setAction(SGMSampleChamberVacuumMoveAction *newAction);
-
-protected:
-	/// The action being viewed.
-	SGMSampleChamberVacuumMoveAction *action_;
-
-	/// The list of LEDs for each subaction.
-	QList<QLabel*> leds_;
-	/// The layout.
-	QVBoxLayout *layout_;
-};
-
 class SGMSampleChamberVacuumMoveActionView : public QWidget
 {
 	Q_OBJECT
@@ -72,9 +36,6 @@ public slots:
 protected:
 	/// The action being viewed.
 	SGMSampleChamberVacuumMoveAction *action_;
-
-	/// The LED action view.
-	SGMSampleChamberVacuumMoveActionLEDView *ledView_;
 };
 
 #endif // SGMSAMPLECHAMBERVACUUMMOVEACTIONVIEW_H
