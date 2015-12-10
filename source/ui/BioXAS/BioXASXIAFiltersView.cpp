52188279df72f0765f963bbda5e268352cf94f6f
helfrij <jessie.helfrich@gmail.com>
2015-12-10 14:14:27 -0600

    Modified the XIA filters editor titles to include the thickness for the given filter.

--- source/ui/BioXAS/BioXASXIAFiltersView.cpp
+++ source/ui/BioXAS/BioXASXIAFiltersView.cpp
@@ -11,19 +11,19 @@ BioXASXIAFiltersView::BioXASXIAFiltersView(BioXASXIAFilters *filters, QWidget *p
 	// Create UI elements.
 
 	editor1_ = new AMExtendedControlEditor(0);
-	editor1_->setTitle("Filter 1");
+	editor1_->setTitle("Filter 1 - 0.25mm");
 	editor1_->setNoUnitsBox(true);
 
 	editor2_ = new AMExtendedControlEditor(0);
-	editor2_->setTitle("Filter 2");
+	editor2_->setTitle("Filter 2 - 0.5mm");
 	editor2_->setNoUnitsBox(true);
 
 	editor3_ = new AMExtendedControlEditor(0);
-	editor3_->setTitle("Filter 3");
+	editor3_->setTitle("Filter 3 - 1.0mm");
 	editor3_->setNoUnitsBox(true);
 
 	editor4_ = new AMExtendedControlEditor(0);
-	editor4_->setTitle("Filter 4");
+	editor4_->setTitle("Filter 4 - 10.0mm");
 	editor4_->setNoUnitsBox(true);
 
 	// Create and set layout.
