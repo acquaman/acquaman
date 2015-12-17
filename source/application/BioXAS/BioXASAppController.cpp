d702ad0d31018cfb096d4bc5f54e2470d4177c51
helfrij <jessie.helfrich@gmail.com>
2015-12-17 14:12:01 -0600

    Noticed that the BFF screen was missing from the left-side pane, I must have accidentally deleted its entries in BioXASAppController when fixing merge conflicts in 2f2dd91. Re-added the missing information, the BFF views are restored.

--- source/application/BioXAS/BioXASAppController.cpp
+++ source/application/BioXAS/BioXASAppController.cpp
@@ -231,6 +231,7 @@ void BioXASAppController::setupUserInterface()
 	addComponentView(BioXASBeamline::bioXAS()->standardsWheel(), "Standards Wheel");
 	addComponentView(BioXASBeamline::bioXAS()->cryostatStage(), "Cryostat Stage");
 	addComponentView(BioXASBeamline::bioXAS()->endstationTable(), "Endstation Table");
+	addComponentView(BioXASBeamline::bioXAS()->filterFlipper(), "Filter Flipper");
 
 	addDetectorView(BioXASBeamline::bioXAS()->scaler(), "Scaler");
 	addDetectorView(BioXASBeamline::bioXAS()->ge32ElementDetector(), "Ge 32-el");
@@ -482,6 +483,12 @@ QWidget* BioXASAppController::createComponentView(QObject *component)
 		BioXASCryostatStage *cryostatStage = qobject_cast<BioXASCryostatStage*>(component);
 		if (!componentFound && cryostatStage) {
 			componentView = new AMMotorGroupView(cryostatStage->motorGroup(), AMMotorGroupView::CompactView);
+			componentFound = true;
+		}
+
+		BioXASFilterFlipper *filterFlipper = qobject_cast<BioXASFilterFlipper*>(component);
+		if (!componentFound && filterFlipper) {
+			componentView = new BioXASFilterFlipperView(filterFlipper);
 			componentFound = true;
 		}
 	}
