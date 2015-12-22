495f114e2cd4883ddd0ae66c5c6dfe97083886b6
helfrij <jessie.helfrich@gmail.com>
2015-12-22 10:52:41 -0600

    Modified the status checker arguments for all valves on Side, Main, and Imaging so they match the arguments for FrontEnd (cd47ead). Tested by closing valves on each beamline through beamline master display, and opening them through the acquaman MasterValves control. No further issues.

--- source/beamline/BioXAS/BioXASSideValves.cpp
+++ source/beamline/BioXAS/BioXASSideValves.cpp
@@ -5,11 +5,11 @@
 BioXASSideValves::BioXASSideValves(QObject *parent) :
 	BioXASValves("BioXASSideValves", parent)
 {
-	addValve(new CLSBiStateControl("VVR1607-5-I22-02", "VVR1607-5-I22-02", "VVR1607-5-I22-02:state", "VVR1607-5-I22-02:opr:open", "VVR1607-5-I22-02:opr:close", new AMControlStatusCheckerDefault(4), this), CLSBiStateControl::Open, CLSBiStateControl::Closed);
-	addValve(new CLSBiStateControl("VVR1607-5-I22-03", "VVR1607-5-I22-03", "VVR1607-5-I22-03:state", "VVR1607-5-I22-03:opr:open", "VVR1607-5-I22-03:opr:close", new AMControlStatusCheckerDefault(4), this), CLSBiStateControl::Open, CLSBiStateControl::Closed);
-	addValve(new CLSBiStateControl("VVR1607-5-I22-04", "VVR1607-5-I22-04", "VVR1607-5-I22-04:state", "VVR1607-5-I22-04:opr:open", "VVR1607-5-I22-04:opr:close", new AMControlStatusCheckerDefault(4), this), CLSBiStateControl::Open, CLSBiStateControl::Closed);
-	addValve(new CLSBiStateControl("VVR1607-5-I22-06", "VVR1607-5-I22-06", "VVR1607-5-I22-06:state", "VVR1607-5-I22-06:opr:open", "VVR1607-5-I22-06:opr:close", new AMControlStatusCheckerDefault(4), this), CLSBiStateControl::Open, CLSBiStateControl::Closed);
-	addValve(new CLSBiStateControl("VVR1607-5-I22-07", "VVR1607-5-I22-07", "VVR1607-5-I22-07:state", "VVR1607-5-I22-07:opr:open", "VVR1607-5-I22-07:opr:close", new AMControlStatusCheckerDefault(4), this), CLSBiStateControl::Open, CLSBiStateControl::Closed);
+	addValve(new CLSBiStateControl("VVR1607-5-I22-02", "VVR1607-5-I22-02", "VVR1607-5-I22-02:state", "VVR1607-5-I22-02:opr:open", "VVR1607-5-I22-02:opr:close", new AMControlStatusCheckerDefault(2), this), CLSBiStateControl::Open, CLSBiStateControl::Closed);
+	addValve(new CLSBiStateControl("VVR1607-5-I22-03", "VVR1607-5-I22-03", "VVR1607-5-I22-03:state", "VVR1607-5-I22-03:opr:open", "VVR1607-5-I22-03:opr:close", new AMControlStatusCheckerDefault(2), this), CLSBiStateControl::Open, CLSBiStateControl::Closed);
+	addValve(new CLSBiStateControl("VVR1607-5-I22-04", "VVR1607-5-I22-04", "VVR1607-5-I22-04:state", "VVR1607-5-I22-04:opr:open", "VVR1607-5-I22-04:opr:close", new AMControlStatusCheckerDefault(2), this), CLSBiStateControl::Open, CLSBiStateControl::Closed);
+	addValve(new CLSBiStateControl("VVR1607-5-I22-06", "VVR1607-5-I22-06", "VVR1607-5-I22-06:state", "VVR1607-5-I22-06:opr:open", "VVR1607-5-I22-06:opr:close", new AMControlStatusCheckerDefault(2), this), CLSBiStateControl::Open, CLSBiStateControl::Closed);
+	addValve(new CLSBiStateControl("VVR1607-5-I22-07", "VVR1607-5-I22-07", "VVR1607-5-I22-07:state", "VVR1607-5-I22-07:opr:open", "VVR1607-5-I22-07:opr:close", new AMControlStatusCheckerDefault(2), this), CLSBiStateControl::Open, CLSBiStateControl::Closed);
 }
 
 BioXASSideValves::~BioXASSideValves()
