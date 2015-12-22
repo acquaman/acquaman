cd47eada1748a20fda00bebb6981ae388c8b765f
helfrij <jessie.helfrich@gmail.com>
2015-12-22 10:40:14 -0600

    Encountered some problems with attempting to open closed valves: the FrontEndValves reported that they were moving whenever they were closed (not when open). I changed the argument for the status checker from 4 to 2, and that seemed to solve the problem.

--- source/beamline/BioXAS/BioXASFrontEndValves.cpp
+++ source/beamline/BioXAS/BioXASFrontEndValves.cpp
@@ -7,10 +7,10 @@ BioXASFrontEndValves::BioXASFrontEndValves(QObject *parent) :
 {
 	addValve(new AMReadOnlyPVControl("VVR1407-I00-01", "VVR1407-I00-01:state", this), CLSBiStateControl::Open, CLSBiStateControl::Closed);
 	addValve(new AMReadOnlyPVControl("VVF1407-I00-01", "VVF1407-I00-01:state", this), CLSBiStateControl::Open, CLSBiStateControl::Closed);
-	addValve(new CLSBiStateControl("VVR1607-5-I00-01", "VVR1607-5-I00-01", "VVR1607-5-I00-01:state", "VVR1607-5-I00-01:opr:open", "VVR1607-5-I00-01:opr:close", new AMControlStatusCheckerDefault(4), this), CLSBiStateControl::Open, CLSBiStateControl::Closed);
-	addValve(new CLSBiStateControl("VVR1607-5-I22-01", "VVR1607-5-I22-01", "VVR1607-5-I22-01:state", "VVR1607-5-I22-01:opr:open", "VVR1607-5-I22-01:opr:close", new AMControlStatusCheckerDefault(4), this), CLSBiStateControl::Open, CLSBiStateControl::Closed);
-	addValve(new CLSBiStateControl("VVR1607-5-I21-01", "VVR1607-5-I21-01", "VVR1607-5-I21-01:state", "VVR1607-5-I21-01:opr:open", "VVR1607-5-I21-01:opr:close", new AMControlStatusCheckerDefault(4), this), CLSBiStateControl::Open, CLSBiStateControl::Closed);
-	addValve(new CLSBiStateControl("VVR1607-5-I10-01", "VVR1607-5-I10-01", "VVR1607-5-I10-01:state", "VVR1607-5-I10-01:opr:open", "VVR1607-5-I10-01:opr:close", new AMControlStatusCheckerDefault(4), this), CLSBiStateControl::Open, CLSBiStateControl::Closed);
+	addValve(new CLSBiStateControl("VVR1607-5-I00-01", "VVR1607-5-I00-01", "VVR1607-5-I00-01:state", "VVR1607-5-I00-01:opr:open", "VVR1607-5-I00-01:opr:close", new AMControlStatusCheckerDefault(2), this), CLSBiStateControl::Open, CLSBiStateControl::Closed);
+	addValve(new CLSBiStateControl("VVR1607-5-I22-01", "VVR1607-5-I22-01", "VVR1607-5-I22-01:state", "VVR1607-5-I22-01:opr:open", "VVR1607-5-I22-01:opr:close", new AMControlStatusCheckerDefault(2), this), CLSBiStateControl::Open, CLSBiStateControl::Closed);
+	addValve(new CLSBiStateControl("VVR1607-5-I21-01", "VVR1607-5-I21-01", "VVR1607-5-I21-01:state", "VVR1607-5-I21-01:opr:open", "VVR1607-5-I21-01:opr:close", new AMControlStatusCheckerDefault(2), this), CLSBiStateControl::Open, CLSBiStateControl::Closed);
+	addValve(new CLSBiStateControl("VVR1607-5-I10-01", "VVR1607-5-I10-01", "VVR1607-5-I10-01:state", "VVR1607-5-I10-01:opr:open", "VVR1607-5-I10-01:opr:close", new AMControlStatusCheckerDefault(2), this), CLSBiStateControl::Open, CLSBiStateControl::Closed);
 }
 
 BioXASFrontEndValves::~BioXASFrontEndValves()
