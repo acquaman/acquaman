8f488e47a75024859eb4b7cce34ba0cc75cd9126
helfrij <jessie.helfrich@gmail.com>
2016-01-27 09:31:43 -0600

    Changed the parallel list action used to move the zebra's time (seconds) control to a sequential list action. This ensures that the time units are modified before the time value. #1873

--- source/beamline/BioXAS/BioXASZebraTimeSeconds.cpp
+++ source/beamline/BioXAS/BioXASZebraTimeSeconds.cpp
@@ -211,7 +211,7 @@ AMAction3* BioXASZebraTimeSeconds::createMoveAction(double setpointS)
 	// If the time value and units setpoints are valid, create and return valid action.
 
 	if (validTimeValue(timeValueSetpoint) && validTimeUnits(timeUnitsSetpoint)) {
-		AMListAction3 *moveAction = new AMListAction3(new AMListActionInfo3(QString("Moving %1").arg(name()), QString("Moving %1").arg(name())), AMListAction3::Parallel);
+		AMListAction3 *moveAction = new AMListAction3(new AMListActionInfo3(QString("Moving %1").arg(name()), QString("Moving %1").arg(name())), AMListAction3::Sequential);
 		moveAction->addSubAction(AMActionSupport::buildControlMoveAction(timeUnits_, timeUnitsSetpoint));
 		moveAction->addSubAction(AMActionSupport::buildControlMoveAction(timeValue_, timeValueSetpoint));
 
