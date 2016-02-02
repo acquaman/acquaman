c93906915006d21e50e851fae42e0aab9fae19f7
helfrij <jessie.helfrich@gmail.com>
2016-02-02 10:14:29 -0600

    Added #define statements to BioXASFrontEndValves, to help make the open/closed value definitions clearer. #1671

--- source/beamline/BioXAS/BioXASFrontEndValves.cpp
+++ source/beamline/BioXAS/BioXASFrontEndValves.cpp
@@ -5,8 +5,8 @@
 BioXASFrontEndValves::BioXASFrontEndValves(QObject *parent) :
 	BioXASValves("BioXASFrontEndValves", parent)
 {
-	addValve(new AMReadOnlyPVControl("VVR1407-I00-01", "VVR1407-I00-01:state", this), 1, 4);
-	addValve(new AMReadOnlyPVControl("VVF1407-I00-01", "VVF1407-I00-01:state", this), 1, 4);
+	addValve(new AMReadOnlyPVControl("VVR1407-I00-01", "VVR1407-I00-01:state", this), BIOXASFRONTENDVALVES_VALVE_OPEN, BIOXASFRONTENDVALVES_VALVE_CLOSED);
+	addValve(new AMReadOnlyPVControl("VVF1407-I00-01", "VVF1407-I00-01:state", this), BIOXASFRONTENDVALVES_FASTVALVE_OPEN, BIOXASFRONTENDVALVES_FASTVALVE_CLOSED);
 	addValve(new CLSExclusiveStatesControl("VVR1607-5-I00-01", "VVR1607-5-I00-01:state", "VVR1607-5-I00-01:opr:open", "VVR1607-5-I00-01:opr:close", this), CLSExclusiveStatesControl::Open, CLSExclusiveStatesControl::Closed);
 	addValve(new CLSExclusiveStatesControl("VVR1607-5-I22-01", "VVR1607-5-I22-01:state", "VVR1607-5-I22-01:opr:open", "VVR1607-5-I22-01:opr:close", this), CLSExclusiveStatesControl::Open, CLSExclusiveStatesControl::Closed);
 	addValve(new CLSExclusiveStatesControl("VVR1607-5-I21-01", "VVR1607-5-I21-01:state", "VVR1607-5-I21-01:opr:open", "VVR1607-5-I21-01:opr:close", this), CLSExclusiveStatesControl::Open, CLSExclusiveStatesControl::Closed);
