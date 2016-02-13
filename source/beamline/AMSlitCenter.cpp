619555a3490771266bab222f0d39fe5fc675b9a8
helfrij <jessie.helfrich@gmail.com>
2016-02-13 08:50:02 -0600

    Removed some commented out code. #2005

--- source/beamline/AMSlitCenter.cpp
+++ source/beamline/AMSlitCenter.cpp
@@ -19,26 +19,6 @@ void AMSlitCenter::updateValue()
 		setValue(currentCenter());
 }
 
-//void AMSlitCenter::updateMinimumValue()
-//{
-//	double newValue = -1;
-
-//	if (canMeasure())
-//		newValue = calculateCenter(firstBlade_->minimumValue(), firstBladeOrientation(), secondBlade_->minimumValue(), secondBladeOrientation());
-
-//	setMinimumValue(newValue);
-//}
-
-//void AMSlitCenter::updateMaximumValue()
-//{
-//	double newValue = -1;
-
-//	if (canMeasure())
-//		newValue = calculateCenter(firstBlade_->maximumValue(), firstBladeOrientation(), secondBlade_->maximumValue(), secondBladeOrientation());
-
-//	setMaximumValue(newValue);
-//}
-
 AMAction3* AMSlitCenter::createMoveAction(double setpoint)
 {
 	AMListAction3 *action = new AMListAction3(new AMListActionInfo3("Moving slit center.", "Moving slit center."), AMListAction3::Parallel);
--- source/beamline/AMSlitGap.cpp
+++ source/beamline/AMSlitGap.cpp
@@ -20,26 +20,6 @@ void AMSlitGap::updateValue()
 		setValue(currentGap());
 }
 
-//void AMSlitGap::updateMinimumValue()
-//{
-//	double newValue = -1;
-
-//	if (canMeasure())
-//		newValue = calculateGap(firstBlade_->minimumValue(), firstBladeOrientation(), secondBlade_->minimumValue(), secondBladeOrientation());
-
-//	setMinimumValue(newValue);
-//}
-
-//void AMSlitGap::updateMaximumValue()
-//{
-//	double newValue = -1;
-
-//	if (canMeasure())
-//		newValue = calculateGap(firstBlade_->maximumValue(), firstBladeOrientation(), secondBlade_->maximumValue(), secondBladeOrientation());
-
-//	setMaximumValue(newValue);
-//}
-
 AMAction3* AMSlitGap::createMoveAction(double setpoint)
 {
 	AMListAction3 *action = new AMListAction3(new AMListActionInfo3("Moving slit gap.", "Moving slit gap."), AMListAction3::Parallel);
