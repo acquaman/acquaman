05e9a3c60ddde997ed2d9a3f6ef5e4938c2dca9e
helfrij <jessie.helfrich@gmail.com>
2016-03-03 09:45:29 -0600

    Updated the PV names for the Main inboard detector. #2046

--- source/beamline/BioXAS/BioXASMainInboard32ElementGeDetector.cpp
+++ source/beamline/BioXAS/BioXASMainInboard32ElementGeDetector.cpp
@@ -5,22 +5,22 @@ BioXASMainInboard32ElementGeDetector::BioXASMainInboard32ElementGeDetector(const
 {
 	// Stuff required by AMXRFDetector.
 
-	acquisitionStatusControl_ = new AMReadOnlyPVControl("Status", "DXP1607-I21-01:DetectorState_RBV", this);
+	acquisitionStatusControl_ = new AMReadOnlyPVControl("Status", "PDTR1607-7-I21-02:DetectorState_RBV", this);
 
 	for (int i = 0; i < 32; i++) {
 		if (i != 2 && i != 15 && i != 21 && i != 26) { // channels 3, 16, 22, and 27 (start 1) are disabled for this detector.
-			channelEnableControls_.append(new AMSinglePVControl(QString("Channel Enable %1").arg(i+1), QString("DXP1607-I21-01:C%1_PluginControlVal").arg(i+1), this, 0.1));
-			spectraControls_.append(new AMReadOnlyPVControl(QString("Raw Spectrum %1").arg(i+1), QString("DXP1607-I21-01:ARR%1:ArrayData").arg(i+1), this));
-			thresholdControls_.append(new AMPVControl(QString("Threshold %1").arg(i+1), QString("DXP1607-I21-01:C%1_SCA4_THRESHOLD_RBV").arg(i+1), QString("DXP1607-I21-01:C%1_SCA4_THRESHOLD").arg(i+1), QString(), this, 0.5));
+			channelEnableControls_.append(new AMSinglePVControl(QString("Channel Enable %1").arg(i+1), QString("PDTR1607-7-I21-02:C%1_PluginControlVal").arg(i+1), this, 0.1));
+			spectraControls_.append(new AMReadOnlyPVControl(QString("Raw Spectrum %1").arg(i+1), QString("PDTR1607-7-I21-02:ARR%1:ArrayData").arg(i+1), this));
+			thresholdControls_.append(new AMPVControl(QString("Threshold %1").arg(i+1), QString("PDTR1607-7-I21-02:C%1_SCA4_THRESHOLD_RBV").arg(i+1), QString("PDTR1607-7-I21-02:C%1_SCA4_THRESHOLD").arg(i+1), QString(), this, 0.5));
 		}
 	}
 
-	initializationControl_ = new AMSinglePVControl("Initialization", "DXP1607-I21-01:Acquire", this, 0.1);
-	eraseControl_ = new AMSinglePVControl("EraseControl", "DXP1607-I21-01:ERASE", this, 0.5);
-	updateControl_ = new AMSinglePVControl("UpdateControl", "DXP1607-I21-01:UPDATE", this, 0.5);
-	statusMessageControl_ = new AMReadOnlyPVControl("StatusMessage", "DXP1607-I21-01:StatusMessage_RBV", this);
-	currentFrameCountControl_ = new AMReadOnlyPVControl("CurrentFrame", "DXP1607-I21-01:ArrayCounter_RBV", this);
-	framesPerAcquisitionControl_ = new AMPVControl("FramesPerAcquisition", "DXP1607-I21-01:NumImages_RBV", "DXP1607-I21-01:NumImages", QString(), this, 0.5);
+	initializationControl_ = new AMSinglePVControl("Initialization", "PDTR1607-7-I21-02:Acquire", this, 0.1);
+	eraseControl_ = new AMSinglePVControl("EraseControl", "PDTR1607-7-I21-02:ERASE", this, 0.5);
+	updateControl_ = new AMSinglePVControl("UpdateControl", "PDTR1607-7-I21-02:UPDATE", this, 0.5);
+	statusMessageControl_ = new AMReadOnlyPVControl("StatusMessage", "PDTR1607-7-I21-02:StatusMessage_RBV", this);
+	currentFrameCountControl_ = new AMReadOnlyPVControl("CurrentFrame", "PDTR1607-7-I21-02:ArrayCounter_RBV", this);
+	framesPerAcquisitionControl_ = new AMPVControl("FramesPerAcquisition", "PDTR1607-7-I21-02:NumImages_RBV", "PDTR1607-7-I21-02:NumImages", QString(), this, 0.5);
 
 	makeConnections();
 }
