0f1918d2af76c103eeccda6f845f542417bec6a1
helfrij <jessie.helfrich@gmail.com>
2016-03-09 15:11:34 -0600

    Fixed minor typo in the pressure PV name. #2034

--- source/beamline/BioXAS/BioXASSideCryostat.cpp
+++ source/beamline/BioXAS/BioXASSideCryostat.cpp
@@ -4,8 +4,8 @@
 BioXASSideCryostat::BioXASSideCryostat(const QString &name, QObject *parent) :
 	BioXASCryostat(name, parent)
 {
-	setPressure(new AMReadOnlyPVControl("TC1607-6-01:READ_TEMP_SIG_TEMP:1", "TC1607-6-01:READ_TEMP_SIG_TEMP:1", this));
-	setTemperature(new AMReadOnlyPVControl("TMP1607-6-01:pressure", "TMP1607-6-01:pressure", this));
+	setPressure(new AMReadOnlyPVControl("TMP1607-6-01:Pressure", "TMP1607-6-01:Pressure", this));
+	setTemperature(new AMReadOnlyPVControl("TC1607-6-01:READ_TEMP_SIG_TEMP:1", "TC1607-6-01:READ_TEMP_SIG_TEMP:1", this));
 }
 
 BioXASSideCryostat::~BioXASSideCryostat()
