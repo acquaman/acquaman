44e281022628dec4951811cb237aa68f3b090a9c
helfrij <jessie.helfrich@gmail.com>
2016-03-18 10:01:44 -0600

    Removed QDebug. #2088

--- source/ui/BioXAS/BioXASControlEditor.cpp
+++ source/ui/BioXAS/BioXASControlEditor.cpp
@@ -409,11 +409,9 @@ AMNumber BioXASControlEditor::getCalibratedDoubleValue()
 
 	return result;
 }
-#include <QDebug>
+
 void BioXASControlEditor::onContextMenuRequested(const QPoint &clickPosition)
 {
-	qDebug() << control_->toString();
-
 	// Update the actions to reflect current control settings.
 
 	updateActions();
