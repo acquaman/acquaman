01d3fc6399f0dbcaf0b7d8b59865299141885780
helfrij <jessie.helfrich@gmail.com>
2016-04-25 11:58:49 -0600

    Added calls to setMinimumValue/setMaximumValue for the pseudomotors on M1. Builds, but untested. #2264

--- source/beamline/BioXAS/BioXASSideM1Mirror.cpp
+++ source/beamline/BioXAS/BioXASSideM1Mirror.cpp
@@ -22,11 +22,31 @@ BioXASSideM1Mirror::BioXASSideM1Mirror(QObject *parent) :
 	setUpstreamBenderMotor(new CLSMAXvMotor(QString("SMTR1607-5-I22-06 BENDER (UPSTREAM)"), QString("SMTR1607-5-I22-06"), QString("SMTR1607-5-I22-06 BENDER (UPSTREAM)"), true, 0.3, 2.0, this, QString(":lbs")));
 	setDownstreamBenderMotor(new CLSMAXvMotor(QString("SMTR1607-5-I22-07 BENDER (DOWNSTREAM)"), QString("SMTR1607-5-I22-07"), QString("SMTR1607-5-I22-07 BENDER (DOWNSTREAM)"), true, 0.3, 2.0, this, QString(":lbs")));
 
-	setPitch(new BioXASMirrorPitchControl(name()+"PitchControl", "deg", this));
-	setRoll(new BioXASMirrorRollControl(name()+"RollControl", "deg", this));
-	setHeight(new BioXASMirrorHeightControl(name()+"HeightControl", "mm", this));
-	setLateral(new BioXASMirrorLateralControl(name()+"LateralControl", "mm", this));
-	setYaw(new BioXASMirrorYawControl(name()+"YawControl", "deg", this));
+	BioXASMirrorPitchControl *pitchControl = new BioXASMirrorPitchControl(name()+"PitchControl", "deg", this);
+	pitchControl->setMinimumValue(0.1);
+	pitchControl->setMaximumValue(0.225);
+	setPitch(pitchControl);
+
+	BioXASMirrorRollControl *rollControl = new BioXASMirrorRollControl(name()+"RollControl", "deg", this);
+	rollControl->setMinimumValue(-0.1);
+	rollControl->setMaximumValue(0.1);
+	setRoll(rollControl);
+
+	BioXASMirrorHeightControl *heightControl = new BioXASMirrorHeightControl(name()+"HeightControl", "mm", this);
+	heightControl->setMinimumValue(-22);
+	heightControl->setMaximumValue(2);
+	setHeight(heightControl);
+
+	BioXASMirrorLateralControl *lateralControl = new BioXASMirrorLateralControl(name()+"LateralControl", "mm", this);
+	lateralControl->setMinimumValue(-3);
+	lateralControl->setMaximumValue(3);
+	setLateral(lateralControl);
+
+	BioXASMirrorYawControl *yawControl = new BioXASMirrorYawControl(name()+"YawControl", "deg", this);
+	yawControl->setMinimumValue(-0.05);
+	yawControl->setMaximumValue(0.05);
+	setYaw(yawControl);
+
 	setBend(new BioXASSideM1MirrorBendControl(name()+"BendControl", "m", this));
 
 	setDownstreamBladeCurrent(new AMReadOnlyPVControl(QString("%1%2").arg(name()).arg("DownstreamBladeCurrent"), QString("A1607-5-03:A:fbk"), this));
