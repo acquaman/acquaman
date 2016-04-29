424387c5ba2e29821cefe372577ff347bd217235
helfrij <jessie.helfrich@gmail.com>
2016-04-29 16:35:08 -0600

    Modified AMToolButton to force a style update when the colorState changes. #2282

--- source/ui/AMToolButton.cpp
+++ source/ui/AMToolButton.cpp
@@ -1,4 +1,5 @@
 #include "AMToolButton.h"
+#include <QStyle>
 
 AMToolButton::AMToolButton(QWidget *parent) :
 	QToolButton(parent)
@@ -15,6 +16,14 @@ void AMToolButton::setColorState(AMToolButton::ColorState newState)
 {
 	if (colorState_ != newState) {
 		colorState_ = newState;
+
+		// Sometimes, it seems like the tool buttons don't update
+		// their color properly in response to changes in the
+		// colorState QProperty. We can force an update here.
+
+		style()->unpolish(this);
+		style()->polish(this);
+
 		emit colorStateChanged(colorState_);
 	}
 }
