0ea98e79a4fb4fdb95a7a4cf0ab23c471c35ac0a
helfrij <jessie.helfrich@gmail.com>
2016-05-03 12:00:48 -0600

    Undid changes to AMToolButton, they ended up being unnecessary here. #2285

--- source/ui/AMToolButton.cpp
+++ source/ui/AMToolButton.cpp
@@ -1,5 +1,4 @@
 #include "AMToolButton.h"
-#include <QStyle>
 
 AMToolButton::AMToolButton(QWidget *parent) :
 	QToolButton(parent)
@@ -16,10 +15,6 @@ void AMToolButton::setColorState(AMToolButton::ColorState newState)
 {
 	if (colorState_ != newState) {
 		colorState_ = newState;
-
-		style()->unpolish(this);
-		style()->polish(this);
-
 		emit colorStateChanged(colorState_);
 	}
 }
