85a737562fd7f0aab72a742e26708c11279214d4
xuetaoliu-cls <xuetao.liu@lightsource.ca>
2016-05-24 09:54:03 -0600

    #2342 #2127 none-completed changes for CLSBeamlineStatusButtonBar

---
+++ source/ui/CLS/CLSBeamlineStatusButtonBar.cpp
@@ -0,0 +1,79 @@
+#include "CLSBeamlineStatusButtonBar.h"
+
+#include "beamline/CLS/CLSBeamlineStatus.h"
+
+CLSBeamlineStatusButtonBar::CLSBeamlineStatusButtonBar(CLSBeamlineStatus *beamlineStatus, QWidget *parent) :
+	CLSControlButtonBar(parent)
+{
+	// Initialize class variables.
+
+	beamlineStatus_ = 0;
+
+	// Current settings.
+	setBeamlineStatus(beamlineStatus);
+}
+
+CLSBeamlineStatusButtonBar::~CLSBeamlineStatusButtonBar()
+{
+
+}
+
+void CLSBeamlineStatusButtonBar::refresh()
+{
+	// Clear the controls.
+	clearControls();
+
+	// Add control for each component in the beam status.
+	if (beamlineStatus_) {
+		foreach (AMControl *control, beamlineStatus_->components())
+			addControl(control, beamlineStatus_->componentBeamOnValue(control));
+	}
+}
+
+void CLSBeamlineStatusButtonBar::setBeamlineStatus(CLSBeamlineStatus *newControl)
+{
+	if (beamlineStatus_ != newControl) {
+
+		if (beamlineStatus_)
+			disconnect( beamlineStatus_, 0, this, 0 );
+
+		beamlineStatus_ = newControl;
+
+		if (beamlineStatus_)
+			connect( beamlineStatus_, SIGNAL(componentsChanged()), this, SLOT(refresh()) );
+
+		refresh();
+
+		emit beamStatusChanged(beamlineStatus_);
+	}
+}
+
+QAbstractButton* CLSBeamlineStatusButtonBar::createButton(AMControl *control, double greenValue)
+{
+	QAbstractButton *button = CLSControlButtonBar::createButton(control, greenValue);
+
+	if (button) {
+		QIcon buttonIcon;
+		QString toolTip = "";
+		if (beamlineStatus_->isShutterControl(control)) {
+			buttonIcon = QIcon(":/shutterIcon2.png");
+			toolTip = "Shutters";
+		} else if (beamlineStatus_->isValveControl(control)) {
+			buttonIcon = QIcon(":/valveIcon2.png");
+			toolTip = "Valves";
+		} else if (beamlineStatus_->isMirrorMaskControl(control)) {
+			buttonIcon = QIcon(":/mirror-icon1.png");
+			toolTip = "Mirror Mask";
+		} else if (beamlineStatus_->isMonoMaskControl(control)) {
+			buttonIcon =QIcon(":/mono-icon5.png");
+			toolTip = "Monochromator Mask";
+		}
+
+		if (toolTip.length() > 0) {
+			button->setIcon(buttonIcon);
+			button->setToolTip((toolTip);
+		}
+	}
+
+	return button;
+}
