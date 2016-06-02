19ab3acdb285dc668d712f5c769ddb131454cbc1
helfrij <jessie.helfrich@gmail.com>
2016-06-02 15:35:02 -0600

    Added missing connect statement for the Ocean Optics detector in PGM beamline. #2173

--- source/beamline/PGM/PGMBeamline.cpp
+++ source/beamline/PGM/PGMBeamline.cpp
@@ -135,6 +135,7 @@ void PGMBeamline::setupComponents()
     energy_->enableLimitMonitoring();
 
 	oceanOpticsDetector_ = new PGMOceanOpticsXRFDetector("OceanOpticsDetector", "Ocean Optics XRF Detector", this);
+	connect( oceanOpticsDetector_, SIGNAL(connected(bool)), this, SLOT(onControlConnectionChanged()) );
 
 	exitSlitBranchAPosition_ = new AMPVwStatusControl("Exit Slit (A) Position", "PSL16114I2101:X:mm:fbk", "PSL16114I2101:X:mm", "SMTR16114I2104:state", QString(), this, 0.5, 2.0, new AMControlStatusCheckerStopped(0));
 	exitSlitBranchAGap_ = new AMPVwStatusControl("Exit Slit (A) Gap", "PSL16114I2101:Y:mm:fbk", "PSL16114I2101:Y:mm", "SMTR16114I2105:state", QString(), this, 0.5, 2.0, new AMControlStatusCheckerStopped(0));
