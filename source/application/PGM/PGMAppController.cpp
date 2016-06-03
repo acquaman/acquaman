8340242622db580615bef7051cd780345b5fabc4
Xuetao (Sheldon) Liu <xuetao.liu@lightsource.ca>
2016-06-03 12:20:37 -0600

    change the data folder back to origin

--- source/application/PGM/PGMAppController.cpp
+++ source/application/PGM/PGMAppController.cpp
@@ -116,7 +116,7 @@ bool PGMAppController::setupDataFolder()
 {
 	// Get a destination folder.
 	return AMChooseDataFolderDialog::getDataFolder("/AcquamanLocalData/pgm",  //local directory
-							   "/home/liux0/AcquamanData/PGM",               //remote directory
+							   "/home/pgm",               //remote directory
 						       "users",                   //data directory
 						       QStringList());            //extra data directory
 }
