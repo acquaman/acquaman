35b952809f6da876ede4a31045a796043c53ae6d
helfrij <jessie.helfrich@gmail.com>
2016-06-09 08:44:51 -0600

    Uncommented the line that makes the SSRL mono region control moving view modal. We want this view to be modal so the user can't focus away from the window (and lose it behind the main window) or change other controls while the crystal change is in progress. #1451

--- source/ui/BioXAS/BioXASSSRLMonochromatorRegionControlEditor.cpp
+++ source/ui/BioXAS/BioXASSSRLMonochromatorRegionControlEditor.cpp
@@ -27,7 +27,7 @@ void BioXASSSRLMonochromatorRegionControlEditor::showMovingView()
 {
 	if (control_ && initiatedCurrentMove()) {
 		BioXASSSRLMonochromatorRegionControlMovingView *movingView = new BioXASSSRLMonochromatorRegionControlMovingView(qobject_cast<BioXASSSRLMonochromatorRegionControl*>(control_), this);
-		//movingView->setWindowModality(Qt::WindowModal);
+		movingView->setWindowModality(Qt::WindowModal);
 		movingView->setWindowTitle("Mono crystal change procedure");
 		movingView->setWindowFlags(Qt::Dialog | Qt::CustomizeWindowHint | Qt::WindowTitleHint);
 		movingView->show();
