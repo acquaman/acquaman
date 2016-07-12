b7cad9b384054cdceb3a8d0e5c872ad2bc477798
helfrij <jessie.helfrich@gmail.com>
2016-07-12 14:52:18 -0600

    Attempted to implement a kind of paradigm for setting different selection behaviors in the CLSButtonBar, but I'm not sure it will stick. Seems needlessly complicated. #2346

--- source/ui/CLS/CLSButtonBar.cpp
+++ source/ui/CLS/CLSButtonBar.cpp
@@ -1,16 +1,21 @@
 #include "CLSButtonBar.h"
-
+#include <QVariant>
+#include <QDebug>
 CLSButtonBar::CLSButtonBar(QWidget *parent) :
 	QWidget(parent)
 {
 	// Initialize class variables.
+
+	selectionMode_ = UnselectedUncheckedToSelectedUnchecked;
+	reselectionMode_ = SelectedUncheckedToSelectedUnchecked;
 
 	selectedButton_ = 0;
 
 	buttonsGroup_ = new QButtonGroup(this);
 	buttonsGroup_->setExclusive(false); // Exclusive must be turned off to have 'deselect when clicked' capability.
 
-	connect( buttonsGroup_, SIGNAL(buttonClicked(QAbstractButton*)), this, SLOT(onButtonClicked(QAbstractButton*)) );
+	connect( buttonsGroup_, SIGNAL(buttonClicked(QAbstractButton*)), this, SLOT(setSelectedButton(QAbstractButton*)) );
+	connect( buttonsGroup_, SIGNAL(buttonClicked(QAbstractButton*)), this, SIGNAL(buttonClicked(QAbstractButton*)) );
 
 	// Create and set main layout.
 
@@ -40,6 +45,8 @@ void CLSButtonBar::removeButton(QAbstractButton *button)
 {
 	if (button) {
 
+		qDebug() << "\n\nRemoving button.";
+
 		// Check if the button to remove is the selected button.
 		// If so, set the selected button to 0.
 
@@ -53,7 +60,7 @@ void CLSButtonBar::removeButton(QAbstractButton *button)
 	}
 }
 
-void CLSButtonBar::clearButtons()
+void CLSButtonBar::removeButtons()
 {
 	for (int i = 0, count = buttonsGroup_->buttons().count(); i < count; i++) {
 		QAbstractButton *button = buttonsGroup_->button(i);
@@ -63,37 +70,182 @@ void CLSButtonBar::clearButtons()
 	}
 }
 
+void CLSButtonBar::clearButtons()
+{
+	for (int i = 0, count = buttonsGroup_->buttons().count(); i < count; i++) {
+		QAbstractButton *button = buttonsGroup_->button(i);
+
+		buttonsGroup_->removeButton(button);
+		buttonsLayout_->removeWidget(button);
+
+		button->disconnect();
+		button->deleteLater();
+	}
+}
+#include <QDebug>
 void CLSButtonBar::setSelectedButton(QAbstractButton *button)
 {
+//	qDebug() << "\n\n";
+
+//	// Uncheck any previous selection.
+
+//	if (selectedButton_) {
+
+//		qDebug() << "Unchecking previous selection.";
+
+//		buttonsGroup_->blockSignals(true);
+//		selectedButton_->setChecked(false);
+//		buttonsGroup_->blockSignals(false);
+//	}
+
+//	// Update the selection and check state, according to whether a button is being
+//	// selected or reselected, and the current selection/reselection modes.
+
+//	if (selectedButton_ != button && selectionMode_ == UnselectedUncheckedToSelectedChecked) {
+
+//		// Update selection.
+
+//		selectedButton_ = button;
+
+//		// Make sure new selection is checked.
+
+//		if (selectedButton_) {
+//			buttonsGroup_->blockSignals(true);
+//			selectedButton_->setChecked(true);
+//			buttonsGroup_->blockSignals(false);
+//		}
+
+//		qDebug() << "Selecting new checked button.";
+
+//		emit selectedButtonChanged(selectedButton_);
+
+//	} else if (selectedButton_ != button && selectionMode_ == UnselectedUncheckedToSelectedUnchecked) {
+
+//		// Update selection.
+
+//		selectedButton_ = button;
+
+//		// Make sure new selection is unchecked.
+
+//		if (selectedButton_) {
+//			buttonsGroup_->blockSignals(true);
+//			selectedButton_->setChecked(false);
+//			buttonsGroup_->blockSignals(false);
+//		}
+
+//		qDebug() << "Reselecting unchecked button.";
+
+//		emit selectedButtonChanged(selectedButton_);
+
+//	} else if (selectedButton_ == button && reselectionMode_ == SelectedCheckedToUnselectedUnchecked) {
+
+//		if (selectedButton_) {
+
+//			qDebug() << "Deselecting button.";
+
+//			// Update selection.
+
+//			selectedButton_ = 0;
+//			emit selectedButtonChanged(selectedButton_);
+//		}
+
+//	} else if (selectedButton_ == button && reselectionMode_ == SelectedUncheckedToSelectedUnchecked) {
+
+//		// Nothing to do here.
+//	}
+
+
+//	///////
+
 	if (selectedButton_ != button) {
 
+		// In all cases where we are changing the selected button
+		// (to either a new button or to 0), we want to uncheck the previous selection.
+
 		if (selectedButton_) {
-			selectedButton_->blockSignals(true);
+			buttonsGroup_->blockSignals(true);
 			selectedButton_->setChecked(false);
-			selectedButton_->blockSignals(false);
+			buttonsGroup_->blockSignals(false);
 		}
+
+		// Update the selected button.
 
 		selectedButton_ = button;
 
+		// If we are changing to a new selection, we may want the new selection
+		// to be checked or unchecked, depending on the selection mode.
+
 		if (selectedButton_) {
-			selectedButton_->blockSignals(true);
-			selectedButton_->setChecked(true);
-			selectedButton_->blockSignals(false);
+
+			buttonsGroup_->blockSignals(true);
+
+			if (selectionMode_ == UnselectedUncheckedToSelectedChecked)
+				selectedButton_->setChecked(true);
+
+			else if (selectionMode_ == UnselectedUncheckedToSelectedUnchecked)
+				selectedButton_->setChecked(false);
+
+			buttonsGroup_->blockSignals(false);
 		}
 
+		qDebug() << "\n\nSelected button is valid:" << (selectedButton_ ? "Yes" : "No");
+		qDebug() << "Selected button is checked:" << ((selectedButton_ && selectedButton_->isChecked()) ? "Yes" : "No");
+
 		emit selectedButtonChanged(selectedButton_);
+
+	} else if (selectedButton_) {
+
+		// In these cases, we are selecting an already selected button.
+
+		buttonsGroup_->blockSignals(true);
+
+		if (reselectionMode_ == SelectedUncheckedToSelectedUnchecked) {
+			selectedButton_->setChecked(false);
+
+		} else if (reselectionMode_ == SelectedCheckedToUnselectedUnchecked) {
+
+			// Uncheck previous selection.
+
+			selectedButton_->setChecked(false);
+
+			// Update the selected button.
+
+			selectedButton_ = 0;
+			emit selectedButtonChanged(selectedButton_);
+		}
+
+		buttonsGroup_->blockSignals(false);
+	}
+}
+
+void CLSButtonBar::setSelectionMode(SelectionMode newMode)
+{
+	if (selectionMode_ != newMode) {
+		selectionMode_ = newMode;
+		emit selectionModeChanged(selectionMode_);
+	}
+}
+
+void CLSButtonBar::setReselectionMode(ReselectionMode newMode)
+{
+	if (reselectionMode_ != newMode) {
+		reselectionMode_ = newMode;
+		emit reselectionModeChanged(reselectionMode_);
 	}
 }
 
 void CLSButtonBar::onButtonClicked(QAbstractButton *clickedButton)
 {
-	// If the group's selection is identical to the current
-	// selection, the selected button should be unchecked.
-	// Otherwise, the clicked button should become the new
-	// selection.
+	// This method handles identifying the new button to select.
+	// Whether that button is un/checked is handled in setSelectedButton.
 
-	if (selectedButton_ == clickedButton)
-		setSelectedButton(0);
-	else
-		setSelectedButton(clickedButton);
+//	if (selectedButton_ && selectedButton_ == clickedButton) {
+
+//		if (reselectionMode_ == SelectedCheckedToUnselectedUnchecked)
+//			setSelectedButton(0);
+
+//	} else {
+
+//		setSelectedButton(clickedButton);
+//	}
 }
--- source/ui/CLS/CLSButtonBar.h
+++ source/ui/CLS/CLSButtonBar.h
@@ -8,14 +8,24 @@
 
 ///
 /// The button bar is a widget which contains a group of buttons.
-/// Those buttons can work in exclusive mode or not
 ///
 
 class CLSButtonBar : public QWidget
 {
 	Q_OBJECT
 
+	Q_PROPERTY(SelectionMode selectionMode READ selectionMode WRITE setSelectionMode NOTIFY selectionModeChanged)
+	Q_ENUMS(SelectionMode)
+
+	Q_PROPERTY(ReselectionMode reselectionMode READ reselectionMode WRITE setReselectionMode NOTIFY reselectionModeChanged)
+	Q_ENUMS(ReselectionMode)
+
 public:
+	/// Enumeration of possible selection behaviors (initially unselected).
+	enum SelectionMode { UnselectedUncheckedToSelectedUnchecked = 0, UnselectedUncheckedToSelectedChecked = 1 };
+	/// Enumeration of possible reselection behaviors (initially selected).
+	enum ReselectionMode { SelectedUncheckedToSelectedUnchecked = 0, SelectedCheckedToUnselectedUnchecked = 1 };
+
 	/// Constructor.
 	explicit CLSButtonBar(QWidget *parent = 0);
 	/// Destructor.
@@ -24,27 +34,51 @@ public:
 	/// Returns the selected button.
 	QAbstractButton* selectedButton() const { return selectedButton_; }
 
+	/// Returns the selection mode.
+	SelectionMode selectionMode() const { return selectionMode_; }
+	/// Returns the reselection mode.
+	ReselectionMode reselectionMode() const { return reselectionMode_; }
+
 signals:
 	/// Notifier that the buttons have changed.
 	void buttonsChanged();
 	/// Notifier that the button was selected.
 	void selectedButtonChanged(QAbstractButton *newButton);
+	/// Notifier that a button was clicked.
+	void buttonClicked(QAbstractButton *clickedButton);
+	/// Notifier that the selection mode has changed.
+	void selectionModeChanged(SelectionMode newMode);
+	/// Notifier that the reselection mode has changed.
+	void reselectionModeChanged(ReselectionMode newMode);
 
-protected slots:
+public slots:
 	/// Adds a button.
 	void addButton(QAbstractButton *newButton);
 	/// Removes a button.
 	void removeButton(QAbstractButton *button);
-	/// Clears all buttons.
+	/// Removes all buttons, without deleting them.
+	void removeButtons();
+	/// Removes and deletes all buttons.
 	void clearButtons();
 
 	/// Sets the selected button.
 	void setSelectedButton(QAbstractButton *button);
 
+	/// Sets the selection mode.
+	void setSelectionMode(SelectionMode newMode);
+	/// Sets the reselection mode.
+	void setReselectionMode(ReselectionMode newMode);
+
+protected slots:
 	/// Handles updating the selected button, in response to a button being clicked.
 	void onButtonClicked(QAbstractButton *clickedButton);
 
 protected:
+	/// The selection mode, governs button behavior when clicked after initially unselected.
+	SelectionMode selectionMode_;
+	/// The reselection mode, governs button behavior when clicked after initally selected.
+	ReselectionMode reselectionMode_;
+
 	/// The selected button.
 	QAbstractButton *selectedButton_;
 	/// The buttons group.
