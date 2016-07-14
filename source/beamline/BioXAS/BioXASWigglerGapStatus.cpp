915a8896cd5d4d5ce7303ec9ef4a4a1746df56b2
helfrij <jessie.helfrich@gmail.com>
2016-07-14 16:12:36 -0600

    Minor modifications to the BioXAS wiggler gap status, so that it properly reports as connected. #2346

--- source/beamline/BioXAS/BioXASWigglerGapStatus.cpp
+++ source/beamline/BioXAS/BioXASWigglerGapStatus.cpp
@@ -4,14 +4,44 @@
 BioXASWigglerGapStatus::BioXASWigglerGapStatus(const QString &name, const QString &baseName, QObject *parent) :
 	AMSingleEnumeratedControl(name, "", parent)
 {
-	setBaseControl(new AMReadOnlyPVControl(baseName, baseName, this));
+	gap_ = 0;
 
 	addValueOption(Closed, "Closed", BIOXASWIGGLERGAPSTATUS_GAP_CLOSED_MIN, BIOXASWIGGLERGAPSTATUS_GAP_CLOSED_MAX);
 	addValueOption(Open, "Open", BIOXASWIGGLERGAPSTATUS_GAP_OPEN_MIN, BIOXASWIGGLERGAPSTATUS_GAP_OPEN_MAX);
+
+	// Current settings.
+
+	setGap(new AMReadOnlyPVControl(baseName, baseName, this));
 }
 
 BioXASWigglerGapStatus::~BioXASWigglerGapStatus()
 {
 
+}
+
+bool BioXASWigglerGapStatus::isConnected() const
+{
+	bool connected = (
+				AMSingleEnumeratedControl::isConnected() &&
+				gap_ && gap_->isConnected()
+				);
+
+	return connected;
+}
+
+void BioXASWigglerGapStatus::setGap(AMControl *newControl)
+{
+	if (gap_ != newControl) {
+
+		if (removeChildControl(gap_))
+			setBaseControl(0);
+
+		gap_ = newControl;
+
+		if (addChildControl(gap_))
+			setBaseControl(gap_);
+
+		emit gapChanged(gap_);
+	}
 }
 
--- source/beamline/BioXAS/BioXASWigglerGapStatus.h
+++ source/beamline/BioXAS/BioXASWigglerGapStatus.h
@@ -2,6 +2,7 @@
 #define BIOXASWIGGLERGAPSTATUS_H
 
 #include "beamline/AMSingleEnumeratedControl.h"
+#include "beamline/AMControl.h"
 
 #define BIOXASWIGGLERGAPSTATUS_GAP_CLOSED_MIN 0.0
 #define BIOXASWIGGLERGAPSTATUS_GAP_CLOSED_MAX 11.7
@@ -11,8 +12,6 @@
 class BioXASWigglerGapStatus : public AMSingleEnumeratedControl
 {
 	Q_OBJECT
-
-	Q_ENUMS(Value)
 
 public:
 	/// Enumeration of the possible status values.
@@ -27,6 +26,24 @@ public:
 	virtual bool canMove() const { return false; }
 	/// Returns true if this control can stop a move right now. False otherwise.
 	virtual bool canStop() const { return false; }
+
+	/// Returns true if this control is connected, false otherwise.
+	virtual bool isConnected() const;
+
+	/// Returns the gap control.
+	AMControl* gap() const { return gap_; }
+
+signals:
+	/// Notifier that the gap control has changed.
+	void gapChanged(AMControl *newControl);
+
+public slots:
+	/// Sets the gap control.
+	void setGap(AMControl *newControl);
+
+protected:
+	/// The gap control.
+	AMControl *gap_;
 };
 
 #endif // BIOXASWIGGLERGAPSTATUS_H
