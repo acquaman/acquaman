41830099b61847d9aa3e089a01c53dc1ee35ed3b
Michael Butler <Michael.Butler@lightsource.ca>
2016-08-03 10:23:34 -0600

    Modified:
    Added read and write to AMHDF5Attribute
    
    Created error code definitions.
    
    Started to fill out AMErrorMon calls.

--- source/dataman/HDF5/AMHDF5Attribute.cpp
+++ source/dataman/HDF5/AMHDF5Attribute.cpp
@@ -1,9 +1,12 @@
 #include "AMHDF5Attribute.h"
 
+#include "util/AMErrorMonitor.h"
+
 AMHDF5Attribute::AMHDF5Attribute(const QString &name, QObject *parent) : QObject(parent)
 {
 	name_ = name;
 	id_ = 0;
+	datatype_ = H5T_C_S1;
 }
 
 AMHDF5Attribute::~AMHDF5Attribute()
@@ -20,7 +23,8 @@ bool AMHDF5Attribute::exists() const
 	// Negative implies an error occurred.
 
 	if(status < 0){
-		//Error
+		AMErrorMon::alert(this, AMHDF5ATTRIBUTE_H5A_FAILURE, QString("Exists: HFD5 API encountered an error."));
+		// AMHDF5Error dump
 		return false;
 	}
 
@@ -39,14 +43,15 @@ bool AMHDF5Attribute::isOpen() const
 bool AMHDF5Attribute::create(hid_t locationId, hid_t spaceId)
 {
 	if(isOpen()){
-		// Error
+		AMErrorMon::alert(this, AMHDF5ATTRIBUTE_ATTRIBUTE_ALREADY_OPEN, QString("Create: Attribute already opened."));
 		return false;
 	}
 
-	hid_t status = H5Acreate2(locationId, name_.toLocal8Bit().constData(), H5T_C_S1, spaceId, H5P_DEFAULT, H5P_DEFAULT);
+	hid_t status = H5Acreate2(locationId, name_.toLocal8Bit().constData(), datatype_, spaceId, H5P_DEFAULT, H5P_DEFAULT);
 
 	if(status < 0 ){
-		// Error
+		AMErrorMon::alert(this, AMHDF5ATTRIBUTE_ATTRIBUTE_CREATE_FAILED, QString("Create: HDF5 api failed to create the attribute."));
+		// AMHDF5Error dump
 		return false;
 	}
 
@@ -58,7 +63,7 @@ bool AMHDF5Attribute::create(hid_t locationId, hid_t spaceId)
 bool AMHDF5Attribute::open()
 {
 	if(isOpen()){
-		// Error
+		AMErrorMon::alert(this, AMHDF5ATTRIBUTE_ATTRIBUTE_OPEN, QString("Open: Attribute already opened."));
 		return false;
 	}
 
@@ -77,7 +82,7 @@ bool AMHDF5Attribute::open()
 bool AMHDF5Attribute::close()
 {
 	if(!isOpen()){
-		// Error
+		AMErrorMon::alert(this, AMHDF5ATTRIBUTE_ATTRIBUTE_CLOSED, QString("Close: Attribute already closed."));
 		return false;
 	}
 
@@ -96,11 +101,13 @@ bool AMHDF5Attribute::close()
 bool AMHDF5Attribute::remove()
 {
 	if(!isOpen()){
-		// Error
+		AMErrorMon::alert(this, AMHDF5ATTRIBUTE_ATTRIBUTE_REMOVE_FAILED, QString("Remove: Attribute cannot be removed when open."));
 		return false;
 	}
-	// Rules about handling deletion of attributes may need to be handled at the file level.
-	// Documentation suggests having no attributes open during a deletion.
+
+///	Rules about handling deletion of attributes may need to be handled at the file level.
+///	Documentation suggests having no attributes open during a deletion.
+
 //	hid_t status = H5Adelete(id_, name_.toLocal8Bit().constData());
 
 //	if(status < 0){
@@ -109,5 +116,69 @@ bool AMHDF5Attribute::remove()
 //	}
 
 	// Handle id_ here.
+	return true;
+}
+
+QString AMHDF5Attribute::read() const
+{
+	if(!isOpen()){
+		AMErrorMon::alert(this, AMHDF5ATTRIBUTE_ATTRIBUTE_CLOSED, QString("Read: Attribute not open and cannot be read."));
+		return false;
+	}
+
+	char* buffer = new char[512]();
+
+	herr_t status = H5Aread(id_, datatype_, buffer);
+
+	if(status < 0){
+		// Error
+		return "";
+	}
+
+	// Move data to object to avoid memory leaks.
+	QString returnValue(QString::fromLocal8Bit(buffer));
+	delete(buffer); // Cleanup buffer.
+
+	return returnValue;
+}
+
+bool AMHDF5Attribute::write(const QString &attributeData)
+{
+	if(!isOpen()){
+		AMErrorMon::alert(this, AMHDF5ATTRIBUTE_ATTRIBUTE_CLOSED, QString("Write: Attribute not open and cannot be written."));
+		return false;
+	}
+
+	if(attributeData.isEmpty()){
+		// Error
+		return false;
+	}
+
+	herr_t status = H5Awrite(id_, datatype_, attributeData.toLocal8Bit().constData());
+
+	if(status < 0){
+		// Error
+		return false;
+	}
+
+	return true;
+}
+
+bool AMHDF5Attribute::rename(const QString &newName)
+{
+	if(!isOpen()){
+		AMErrorMon::alert(this, AMHDF5ATTRIBUTE_ATTRIBUTE_CLOSED, QString("Rename: Attribute closed and cannot be renamed."));
+		return false;
+	}
+
+	herr_t status = H5Arename(id_, name_.toLocal8Bit().constData(), newName.toLocal8Bit().constData());
+
+	if(status < 0){
+		// Error
+		return false;
+	}
+
+	name_ = newName;
+
 	return true;
 }
--- source/dataman/HDF5/AMHDF5Attribute.h
+++ source/dataman/HDF5/AMHDF5Attribute.h
@@ -5,6 +5,18 @@
 
 #include <hdf5.h>
 
+#define AMHDF5ATTRIBUTE_H5A_FAILURE 687000
+#define AMHDF5ATTRIBUTE_ATTRIBUTE_OPEN 687001
+#define AMHDF5ATTRIBUTE_ATTRIBUTE_CLOSED 687002
+#define AMHDF5ATTRIBUTE_ATTRIBUTE_CREATE_FAILED 687003
+#define AMHDF5ATTRIBUTE_ATTRIBUTE_OPEN_FAILED 687004
+#define AMHDF5ATTRIBUTE_ATTRIBUTE_CLOSE_FAILED 687005
+#define AMHDF5ATTRIBUTE_ATTRIBUTE_REMOVE_FAILED 687006
+#define AMHDF5ATTRIBUTE_ATTRIBUTE_READ_FAILED 687007
+#define AMHDF5ATTRIBUTE_ATTRIBUTE_READ_FAILED 687008
+#define AMHDF5ATTRIBUTE_ATTRIBUTE_RENAME_NAME_INVALID 687009
+#define AMHDF5ATTRIBUTE_ATTRIBUTE_RENAME_FAILED 687010
+
 /// This class is to handle interfacing with H5A allowing for creating and handling HDF5 attributes.
 /// It takes a reference to a HDF5 object (group, dataset, or committed datatype) and allows  for attaching an attribute.
 /// An attribute is meta data inserted into the header of an object and is accessible without looking at the contents of
@@ -14,31 +26,39 @@ class AMHDF5Attribute : public QObject
 	Q_OBJECT
 public:
 
-	// Constructor for an attribute object.
+	/// Constructor for an attribute object.
 	explicit AMHDF5Attribute(const QString &name, QObject *parent = 0);
-	// Destructor.
+	/// Destructor.
 	virtual ~AMHDF5Attribute();
-	// Checks to see if an attribute exists for the specified object.
+	/// Checks to see if an attribute exists for the specified object.
 	bool exists() const;
-
+	/// Checks if this object instance is associated with an open attribute.
 	bool isOpen() const;
 
 signals:
 
 public slots:
-	// Creates an attribute for the associated object.
+	/// Creates an attribute for the associated object.
 	bool create(hid_t locationId, hid_t spaceId);
-	// Opens an attribute for the assosciated object.
+	/// Opens an attribute for the assosciated object.
 	bool open();
-	// Closes an open attribute for the associated object.
+	/// Closes an open attribute for the associated object.
 	bool close();
-	// Removes an existing attributes from an object.
+	/// Removes an existing attributes from an object.
 	bool remove();
+	/// Reads the attribute into a specified buffer.
+	QString read() const;
+	/// Writes the attribute information.
+	bool write(const QString &attributeData);
+	/// Rename this attribute.
+	bool rename(const QString &newName);
 
 private:
-	// Holds the object id for this object.
+	/// Holds the object id for this object.
 	hid_t id_;
-	// Holds the name for the attribute.
+	/// Keeps reference to the type of data the attribute holds.
+	hid_t datatype_;
+	/// Holds the name for the attribute.
 	QString name_;
 
 };
